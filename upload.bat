ab854918ba9b53dbaea5f92a1448dc0639bc53aa
Viktor Szakats <vszakats@users.noreply.github.com>
2015-09-04 16:28:11 +0200

    fix to prev

--- upload.bat
+++ upload.bat
@@ -3,7 +3,8 @@
 
 @echo off
 
-if not "%APPVEYOR_REPO_BRANCH%" == "master" set _SUF=test
+if     "%APPVEYOR_REPO_BRANCH%" == "master" set _SUF=
+if not "%APPVEYOR_REPO_BRANCH%" == "master" set _SUF=-test
 
 curl -fsS -u "%BINTRAY_USER%:%BINTRAY_APIKEY%" -X PUT "https://api.bintray.com/content/%BINTRAY_USER%/generic/%_NAM%/%_VER%%_SUF%/%_BAS%.zip?override=1&publish=1" --data-binary "@%_BAS%.zip"
 for %%I in ("%_BAS%.zip") do echo %%~nxI: %%~zI bytes %%~tI
