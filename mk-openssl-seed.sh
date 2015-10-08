986d7301fa3bfb0e86bcf5e8047592a03485379b
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-08 14:24:56 +0200

    debug

--- mk-openssl-seed.sh
+++ mk-openssl-seed.sh
@@ -6,6 +6,11 @@ cd "$(dirname "$0")" || exit
 
 ts=$(stat -c %W openssl/CHANGES)
 
+stat -c %w openssl/CHANGES
+stat -c %y openssl/CHANGES
+stat -c %W openssl/CHANGES
+stat -c %Y openssl/CHANGES
+
 sed -e "s/-frandom-seed=__RANDOM_SEED__/-frandom-seed=${ts}/g" -i openssl.diff
 sed -e "s/-frandom-seed=__RANDOM_SEED__/-frandom-seed=${ts}/g" -i mk-openssl.bat
 
