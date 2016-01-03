ad3ae68d3c842aafa30f857898262fa5a0f5f298
Viktor Szakats <vszakats@users.noreply.github.com>
2016-01-03 12:32:49 +0100

    apply set locally [ci skip]

--- ul.sh
+++ ul.sh
@@ -10,10 +10,12 @@ else
    _SUF=
 fi
 
-set +x
-curl -fsS -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" \
-   -X PUT "https://api.bintray.com/content/${BINTRAY_USER}/generic/${_NAM}${_SUF}/${_VER}/${_BAS}${_SUF}.7z?override=1&publish=1" \
-   --data-binary "@${_BAS}${_SUF}.7z"
+(
+   set +x
+   curl -fsS -u "${BINTRAY_USER}:${BINTRAY_APIKEY}" \
+      -X PUT "https://api.bintray.com/content/${BINTRAY_USER}/generic/${_NAM}${_SUF}/${_VER}/${_BAS}${_SUF}.7z?override=1&publish=1" \
+      --data-binary "@${_BAS}${_SUF}.7z"
+)
 
 # <filename>: <size> bytes <YYYY-MM-DD> <HH:MM>
 case "$(uname)" in
@@ -25,9 +27,11 @@ openssl dgst -sha256 "${_BAS}${_SUF}.7z"
 openssl dgst -sha256 "${_BAS}${_SUF}.7z" >> ../hashes.txt
 
 if [ "${APPVEYOR_REPO_BRANCH}" = "master" ] ; then
-   set +x
-   curl -fsS \
-      -X POST 'https://www.virustotal.com/vtapi/v2/file/scan' \
-      --form "apikey=${VIRUSTOTAL_APIKEY}" \
-      --form "file=@${_BAS}${_SUF}.7z"
+   (
+      set +x
+      curl -fsS \
+         -X POST 'https://www.virustotal.com/vtapi/v2/file/scan' \
+         --form "apikey=${VIRUSTOTAL_APIKEY}" \
+         --form "file=@${_BAS}${_SUF}.7z"
+   )
 fi
