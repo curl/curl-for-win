9cdca0882d0e6ce1e7ab854d102376c6b76a8f2c
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-15 02:49:32 +0100

    simplify [ci skip]

--- openssl.sh
+++ openssl.sh
@@ -87,7 +87,7 @@ _CPU="$2"
 
    cp -f -p apps/openssl.exe    "${_DST}/"
    cp -f -p apps/*.dll          "${_DST}/"
-   cp -f -p apps/openssl.cnf    "${_DST}/openssl.cnf"
+   cp -f -p apps/openssl.cnf    "${_DST}/"
    cp -f -p include/openssl/*.h "${_DST}/include/openssl/"
    cp -f -p ms/applink.c        "${_DST}/include/openssl/"
    cp -f -p ./*.a               "${_DST}/lib/"
