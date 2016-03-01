27ba4c387babe8e27ed74a0babee14e6d8f66cff
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-01 17:58:39 +0100

    check success of each critical command. 'set -e' doesn't seem to work. [ci skip]

--- _dl.sh
+++ _dl.sh
@@ -33,26 +33,26 @@ fi
 
 if [ "${_BRANCH#*msysmingw*}" = "${_BRANCH}" ] ; then
    # mingw
-   curl -fsS -o pack.bin -L 'https://downloads.sourceforge.net/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z'
-   openssl dgst -sha256 pack.bin | grep -q ec28b6640ad4f183be7afcd6e9c5eabb24b89729ca3fec7618755555b5d70c19
+   curl -fsS -o pack.bin -L 'https://downloads.sourceforge.net/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.3.0/threads-posix/sjlj/x86_64-5.3.0-release-posix-sjlj-rt_v4-rev0.7z' || exit 1
+   openssl dgst -sha256 pack.bin | grep -q ec28b6640ad4f183be7afcd6e9c5eabb24b89729ca3fec7618755555b5d70c19 || exit 1
    # Will unpack into "./mingw64"
-   7z x -y pack.bin > /dev/null
+   7z x -y pack.bin > /dev/null || exit 1
    rm pack.bin
 fi
 
 # nghttp2
-curl -fsS -o pack.bin -L --proto-redir =https "https://github.com/tatsuhiro-t/nghttp2/releases/download/v${NGHTTP2_VER_}/nghttp2-${NGHTTP2_VER_}.tar.bz2"
-openssl dgst -sha256 pack.bin | grep -q "${NGHTTP2_HASH}"
-tar -xvf pack.bin > /dev/null 2>&1
+curl -fsS -o pack.bin -L --proto-redir =https "https://github.com/tatsuhiro-t/nghttp2/releases/download/v${NGHTTP2_VER_}/nghttp2-${NGHTTP2_VER_}.tar.bz2" || exit 1
+openssl dgst -sha256 pack.bin | grep -q "${NGHTTP2_HASH}" || exit 1
+tar -xvf pack.bin > /dev/null 2>&1 || exit 1
 rm pack.bin
 rm -f -r nghttp2 && mv nghttp2-* nghttp2
 
 # Will increase curl binary sizes by 1MB, so leave this optional.
 if [ "${_BRANCH#*libidn*}" != "${_BRANCH}" ] ; then
    # libidn
-   curl -fsS -o pack.bin -L --proto-redir =https "http://ftp.gnu.org/gnu/libidn/libidn-${LIBIDN_VER_}.tar.gz"
-   openssl dgst -sha256 pack.bin | grep -q "${LIBIDN_HASH}"
-   tar -xvf pack.bin > /dev/null 2>&1
+   curl -fsS -o pack.bin -L --proto-redir =https "http://ftp.gnu.org/gnu/libidn/libidn-${LIBIDN_VER_}.tar.gz" || exit 1
+   openssl dgst -sha256 pack.bin | grep -q "${LIBIDN_HASH}" || exit 1
+   tar -xvf pack.bin > /dev/null 2>&1 || exit 1
    rm pack.bin
    rm -f -r libidn && mv libidn-* libidn
 fi
@@ -61,12 +61,12 @@ if [ "${_BRANCH#*cares*}" != "${_BRANCH}" ] ; then
    # c-ares
    if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ] ; then
       CARES_VER_='1.11.0-dev'
-      curl -fsS -o pack.bin -L --proto-redir =https https://github.com/c-ares/c-ares/archive/e41a39b022f28e46a381af118f258432eef0eca5.tar.gz
+      curl -fsS -o pack.bin -L --proto-redir =https https://github.com/c-ares/c-ares/archive/e41a39b022f28e46a381af118f258432eef0eca5.tar.gz || exit 1
    else
-      curl -fsS -o pack.bin "http://c-ares.haxx.se/download/c-ares-${CARES_VER_}.tar.gz"
-      openssl dgst -sha256 pack.bin | grep -q "${CARES_HASH}"
+      curl -fsS -o pack.bin "http://c-ares.haxx.se/download/c-ares-${CARES_VER_}.tar.gz" || exit 1
+      openssl dgst -sha256 pack.bin | grep -q "${CARES_HASH}" || exit 1
    fi
-   tar -xvf pack.bin > /dev/null 2>&1
+   tar -xvf pack.bin > /dev/null 2>&1 || exit 1
    rm pack.bin
    rm -f -r c-ares && mv c-ares-* c-ares
    [ -f "c-ares${_patsuf}.diff" ] && dos2unix < "c-ares${_patsuf}.diff" | patch -N -p1 -d c-ares
@@ -74,21 +74,21 @@ fi
 
 if [ "${_BRANCH#*libressl*}" != "${_BRANCH}" ] ; then
    # libressl
-   curl -fsS -o pack.bin "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL_VER_}.tar.gz"
-   openssl dgst -sha256 pack.bin | grep -q "${LIBRESSL_HASH}"
-   tar -xvf pack.bin > /dev/null 2>&1
+   curl -fsS -o pack.bin "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL_VER_}.tar.gz" || exit 1
+   openssl dgst -sha256 pack.bin | grep -q "${LIBRESSL_HASH}" || exit 1
+   tar -xvf pack.bin > /dev/null 2>&1 || exit 1
    rm pack.bin
    rm -f -r libressl && mv libressl-* libressl
 else
    # openssl
    if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ] ; then
       OPENSSL_VER_='1.1.0-dev'
-      curl -fsS -o pack.bin -L --proto-redir =https https://github.com/openssl/openssl/archive/master.tar.gz
+      curl -fsS -o pack.bin -L --proto-redir =https https://github.com/openssl/openssl/archive/master.tar.gz || exit 1
    else
-      curl -fsS -o pack.bin "https://www.openssl.org/source/openssl-${OPENSSL_VER_}.tar.gz"
-      openssl dgst -sha256 pack.bin | grep -q "${OPENSSL_HASH}"
+      curl -fsS -o pack.bin "https://www.openssl.org/source/openssl-${OPENSSL_VER_}.tar.gz" || exit 1
+      openssl dgst -sha256 pack.bin | grep -q "${OPENSSL_HASH}" || exit 1
    fi
-   tar -xvf pack.bin > /dev/null 2>&1
+   tar -xvf pack.bin > /dev/null 2>&1 || exit 1
    rm pack.bin
    rm -f -r openssl && mv openssl-* openssl
    [ -f "openssl${_patsuf}.diff" ] && dos2unix < "openssl${_patsuf}.diff" | patch -N -p1 -d openssl
@@ -98,9 +98,9 @@ fi
 # and potential licensing issues.
 if [ "${_BRANCH#*librtmp*}" != "${_BRANCH}" ] ; then
    # librtmp
-   curl -fsS -o pack.bin 'https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20151223.gitfa8646d.orig.tar.gz'
-   openssl dgst -sha256 pack.bin | grep -q "${LIBRTMP_HASH}"
-   tar -xvf pack.bin > /dev/null 2>&1
+   curl -fsS -o pack.bin 'https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20151223.gitfa8646d.orig.tar.gz' || exit 1
+   openssl dgst -sha256 pack.bin | grep -q "${LIBRTMP_HASH}" || exit 1
+   tar -xvf pack.bin > /dev/null 2>&1 || exit 1
    rm pack.bin
    rm -f -r librtmp && mv rtmpdump-* librtmp
 fi
@@ -108,12 +108,12 @@ fi
 # libssh2
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ] ; then
    LIBSSH2_VER_='1.7.1-dev'
-   curl -fsS -o pack.bin -L --proto-redir =https https://github.com/libssh2/libssh2/archive/1fcf849e15ffda99cc30b6d23b8d378f501225a2.tar.gz
+   curl -fsS -o pack.bin -L --proto-redir =https https://github.com/libssh2/libssh2/archive/1fcf849e15ffda99cc30b6d23b8d378f501225a2.tar.gz || exit 1
 else
-   curl -fsS -o pack.bin -L --proto-redir =https "https://libssh2.org/download/libssh2-${LIBSSH2_VER_}.tar.gz"
-   openssl dgst -sha256 pack.bin | grep -q "${LIBSSH2_HASH}"
+   curl -fsS -o pack.bin -L --proto-redir =https "https://libssh2.org/download/libssh2-${LIBSSH2_VER_}.tar.gz" || exit 1
+   openssl dgst -sha256 pack.bin | grep -q "${LIBSSH2_HASH}" || exit 1
 fi
-tar -xvf pack.bin > /dev/null 2>&1
+tar -xvf pack.bin > /dev/null 2>&1 || exit 1
 rm pack.bin
 rm -f -r libssh2 && mv libssh2-* libssh2
 [ -f "libssh2${_patsuf}.diff" ] && dos2unix < "libssh2${_patsuf}.diff" | patch -N -p1 -d libssh2
@@ -121,12 +121,12 @@ rm -f -r libssh2 && mv libssh2-* libssh2
 # curl
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ] ; then
    CURL_VER_='7.47.2-dev'
-   curl -fsS -o pack.bin -L --proto-redir =https https://github.com/curl/curl/archive/71398487e75e47c026d0655d540ade247d18f62c.tar.gz
+   curl -fsS -o pack.bin -L --proto-redir =https https://github.com/curl/curl/archive/71398487e75e47c026d0655d540ade247d18f62c.tar.gz || exit 1
 else
-   curl -fsS -o pack.bin "https://curl.haxx.se/download/curl-${CURL_VER_}.tar.bz2"
-   openssl dgst -sha256 pack.bin | grep -q "${CURL_HASH}"
+   curl -fsS -o pack.bin "https://curl.haxx.se/download/curl-${CURL_VER_}.tar.bz2" || exit 1
+   openssl dgst -sha256 pack.bin | grep -q "${CURL_HASH}" || exit 1
 fi
-tar -xvf pack.bin > /dev/null 2>&1
+tar -xvf pack.bin > /dev/null 2>&1 || exit 1
 rm pack.bin
 rm -f -r curl && mv curl-* curl
 [ -f "curl${_patsuf}.diff" ] && dos2unix < "curl${_patsuf}.diff" | patch -N -p1 -d curl
