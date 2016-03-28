28bd3746e198bfd7ae77034ba2da81a2e3a01ead
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-28 19:43:18 +0200

    try installing pefile manually

--- _dl.sh
+++ _dl.sh
@@ -25,14 +25,25 @@ export CURL_HASH=864e7819210b586d42c674a1fdd577ce75a78b3dda64c63565abe5aefd72c75
 # Quit if any of the lines fail
 set -e
 
-# Install required component
+# Install required dependency for pefile
 python -m pip --disable-pip-version-check install --upgrade pip
-python -m pip install --allow-external https://github.com/erocarrera/pefile/releases/download/v2016.3.4/pefile-2016.3.4.tar.gz || exit 1
+python -m pip install future
 
 alias curl='curl -fsS --connect-timeout 15'
 alias gpg='gpg --keyid-format LONG'
 
 gpg --version | grep gpg
+
+# pefile
+curl -o pack.bin 'https://github.com/erocarrera/pefile/releases/download/v2016.3.4/pefile-2016.3.4.tar.gz' || exit 1
+openssl dgst -sha256 pack.bin | grep -q '51d149b31d0eeb5c97c67ee6a05b529d9fab37557d59d82c1f489560dc0b66f7' || exit 1
+tar -xvf pack.bin > /dev/null 2>&1 || exit 1
+rm pack.bin
+rm -f -r pefile && mv pefile-* pefile
+(
+   cd pefile || exit 1
+   ./setup.py
+)
 
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ] ; then
    _patsuf='.dev'
