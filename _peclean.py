dbbb95bb44040f7f3894923463c4c5ae164c1a42
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-23 00:45:45 +0200

    fix python lint issues [ci skip]

--- _peclean.py
+++ _peclean.py
@@ -7,16 +7,21 @@ import calendar
 import datetime
 import glob
 import os
-import pefile
 import sys
 
+import pefile
+
 if len(sys.argv) > 2:
-   ts = calendar.timegm(datetime.datetime.fromtimestamp(os.path.getmtime(sys.argv[1])).timetuple())
-   for file in glob.glob(sys.argv[2]):
-      print(datetime.datetime.fromtimestamp(ts).isoformat() + ' -> ' + file)
-      pe = pefile.PE(file)
-      pe.FILE_HEADER.TimeDateStamp = ts
-      if hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
-         pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = ts
-      pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
-      pe.write(file)
+    FTIME = calendar.timegm(
+        datetime.datetime.fromtimestamp(
+            os.path.getmtime(sys.argv[1])).timetuple())
+    for fname in glob.glob(sys.argv[2]):
+        print(datetime.datetime.fromtimestamp(FTIME).isoformat() + ' -> ' + fname)
+        pe = pefile.PE(fname)
+        pe.FILE_HEADER.TimeDateStamp = FTIME
+        try:
+            pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = FTIME
+        except AttributeError:
+            pass
+        pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
+        pe.write(fname)
