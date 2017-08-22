c6cbfe0a9481a7ffbb5da02cc5a1df970394fd3f
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-22 16:56:57 +0000

    try tar.lz

--- _dl.sh
+++ _dl.sh
@@ -6,7 +6,7 @@
 export ZLIB_VER_='1.2.11'
 export ZLIB_HASH=629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff
 export LIBIDN2_VER_='2.0.3'
-export LIBIDN2_HASH=4335149ce7a5c615edb781574d38f658672780331064fb17354a10e11a5308cd
+export LIBIDN2_HASH=b52fd3bc9693de16db93aa23a11a56f3cdb3fbf005a360145401ab5d252490f5
 export NGHTTP2_VER_='1.25.0'
 export NGHTTP2_HASH=9553f144e60aa0d7bc5245df1c0f6a867a9fd9dc35ed6f358c6122a844d52f62
 export CARES_VER_='1.13.0'
@@ -20,7 +20,7 @@ export LIBRTMP_HASH=5c032f5c8cc2937eb55a81a94effdfed3b0a0304b6376147b86f951e225e
 export LIBSSH2_VER_='1.8.0'
 export LIBSSH2_HASH=39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4
 export CURL_VER_='7.55.1'
-export CURL_HASH=
+export CURL_HASH=3eafca6e84ecb4af5f35795dee84e643d5428287e88c041122bb8dac18676bb7
 
 # Quit if any of the lines fail
 set -e
@@ -88,8 +88,8 @@ rm -f -r nghttp2 && mv nghttp2-* nghttp2
 if [ "${_BRANCH#*libidn2*}" != "${_BRANCH}" ]; then
   # libidn2
   curl \
-    -o pack.bin "https://ftp.gnu.org/gnu/libidn/libidn2-${LIBIDN2_VER_}.tar.gz" \
-    -o pack.sig "https://ftp.gnu.org/gnu/libidn/libidn2-${LIBIDN2_VER_}.tar.gz.sig" || exit 1
+    -o pack.bin "https://ftp.gnu.org/gnu/libidn/libidn2-${LIBIDN2_VER_}.tar.lz" \
+    -o pack.sig "https://ftp.gnu.org/gnu/libidn/libidn2-${LIBIDN2_VER_}.tar.lz.sig" || exit 1
   curl 'https://ftp.gnu.org/gnu/gnu-keyring.gpg' \
   | gpg -q --import 2> /dev/null
   gpg --verify-options show-primary-uid-only --verify pack.sig pack.bin || exit 1
