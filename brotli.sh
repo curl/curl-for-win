f5d9362056c17b55f0c3fbc40b53109b79bc5231
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-05 21:44:31 +0000

    brotli: fix build regression on Windows hosts [ci skip]

--- brotli.sh
+++ brotli.sh
@@ -36,8 +36,8 @@ _cpu="$2"
 
   cmake . \
     -DCMAKE_SYSTEM_NAME='Windows' \
-    -DCMAKE_C_COMPILER="${_TRIPLET}-gcc" \
-    -DCMAKE_CXX_COMPILER="${_TRIPLET}-g++" \
+    -DCMAKE_C_COMPILER="${_CCPREFIX}gcc" \
+    -DCMAKE_CXX_COMPILER="${_CCPREFIX}g++" \
     -DCMAKE_C_FLAGS="${_CFLAGS}" \
     -DCMAKE_INSTALL_PREFIX='/usr/local' \
     -DCMAKE_INSTALL_LIBDIR='lib'
