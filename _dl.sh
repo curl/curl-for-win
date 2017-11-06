2551c80625a6381dc919397f721d613dcfece9be
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-06 00:34:50 +0000

    fix indentation [ci skip]

--- _dl.sh
+++ _dl.sh
@@ -56,11 +56,11 @@ gpg_recv_keys() {
 gpg --version | grep gpg
 
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ]; then
-   _patsuf='.dev'
+  _patsuf='.dev'
 elif [ "${_BRANCH#*master*}" = "${_BRANCH}" ]; then
-   _patsuf='.test'
+  _patsuf='.test'
 else
-   _patsuf=''
+  _patsuf=''
 fi
 
 if [ "${os}" = 'win' ]; then
@@ -192,8 +192,8 @@ rm -f -r libssh2 && mv libssh2-* libssh2
 
 # curl
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ]; then
-  CURL_VER_='7.56.1-dev'
-  curl -o pack.bin -L --proto-redir =https https://github.com/curl/curl/archive/4440b6ad575385b433dc4b8a28ef80000aa95f7f.tar.gz || exit 1
+  CURL_VER_='7.57.0-dev'
+  curl -o pack.bin -L --proto-redir =https https://github.com/curl/curl/archive/c675c40295045d4988eeb6291c54eb48f138822f.tar.gz || exit 1
 else
   curl \
     -o pack.bin "https://curl.haxx.se/download/curl-${CURL_VER_}.tar.xz" \
