b51ed037b728b534138a319df39cd4107ada34c6
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-13 01:12:09 +0000

    nghttp2-clang9

--- _peclean.py
+++ _peclean.py
@@ -16,8 +16,8 @@ import pefile
 if len(sys.argv) > 2:
     FTIME = calendar.timegm(
         datetime.datetime.fromtimestamp(
-            os.path.getmtime(sys.argv[1])).timetuple())
-    for fname in glob.glob(sys.argv[2]):
+            os.path.getmtime(os.path.normpath(sys.argv[1]))).timetuple())
+    for fname in glob.glob(os.path.normpath(sys.argv[2])):
         print(datetime.datetime.fromtimestamp(FTIME).isoformat() + ' -> ' + fname)
         pe = pefile.PE(fname)
         pe.FILE_HEADER.TimeDateStamp = FTIME
