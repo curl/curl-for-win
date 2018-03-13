4b5df9c7e8327a5ad5a3cec1479c4897bf6bf5ea
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-13 01:35:22 +0000

    peclean: support multiple input filenames [ci skip]

--- _peclean.py
+++ _peclean.py
@@ -16,14 +16,15 @@ import pefile
 if len(sys.argv) > 2:
     FTIME = calendar.timegm(
         datetime.datetime.fromtimestamp(
-            os.path.getmtime(sys.argv[1])).timetuple())
-    for fname in glob.glob(sys.argv[2]):
-        print(datetime.datetime.fromtimestamp(FTIME).isoformat() + ' -> ' + fname)
-        pe = pefile.PE(fname)
-        pe.FILE_HEADER.TimeDateStamp = FTIME
-        try:
-            pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = FTIME
-        except AttributeError:
-            pass
-        pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
-        pe.write(fname)
+            os.path.getmtime(os.path.normpath(sys.argv[1]))).timetuple())
+    for argv in sys.argv[2:]:
+        for fname in glob.glob(argv):
+            print(datetime.datetime.fromtimestamp(FTIME).isoformat() + ' -> ' + fname)
+            pe = pefile.PE(fname)
+            pe.FILE_HEADER.TimeDateStamp = FTIME
+            try:
+                pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = FTIME
+            except AttributeError:
+                pass
+            pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
+            pe.write(fname)
