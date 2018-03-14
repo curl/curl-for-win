93ccdfa0849dd12ec7f10010e02f620ba5028324
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-14 08:48:48 +0000

    openssl: install and pick files from the install directory [ci skip]
    `make install` is required for cmake-based libssh2 builds,
    in order to get openssl detected by cmake.

--- openssl.sh
+++ openssl.sh
@@ -16,6 +16,19 @@ _cpu="$2"
 (
   cd "${_NAM}" || exit 0
 
+  # Detect host OS
+  case "$(uname)" in
+    *_NT*)   os='win';;
+    Linux*)  os='linux';;
+    Darwin*) os='mac';;
+    *BSD)    os='bsd';;
+  esac
+
+  if [ "${os}" = 'win' ]; then
+    # Required on MSYS2 for pod2man and pod2html in 'make install' phase
+    export PATH="${PATH}:/usr/bin/core_perl"
+  fi
+
   readonly _ref='CHANGES'
 
   # Build
@@ -27,8 +40,6 @@ _cpu="$2"
   find . -name '*.pc'  -type f -delete
   find . -name '*.dll' -type f -delete
   find . -name '*.exe' -type f -delete
-
-  engdir='engines'
 
   [ "${_cpu}" = '32' ] && options='mingw'
   [ "${_cpu}" = '64' ] && options='mingw64'
@@ -66,22 +77,28 @@ _cpu="$2"
     '--prefix=/usr/local'
   [ "$(echo "${OPENSSL_VER_}" | cut -c -4)" = '1.1.' ] || make depend
   make
-# make install "DESTDIR=$(pwd)/pkg"
+  # Install it so that it can be detected by CMake
+  make install "DESTDIR=$(pwd)/pkg" > /dev/null # 2>&1
+
+  # DESTDIR= + --prefix=
+  _pkg='pkg/usr/local'
 
   # Make steps for determinism
 
-  "${_CCPREFIX}strip" -p --enable-deterministic-archives -g ./*.a
-  "${_CCPREFIX}strip" -p -s apps/openssl.exe
-  "${_CCPREFIX}strip" -p -s apps/*.dll
+  engdir="${_pkg}/lib/engines-1_1"
+
+  "${_CCPREFIX}strip" -p --enable-deterministic-archives -g ${_pkg}/lib/*.a
+  "${_CCPREFIX}strip" -p -s ${_pkg}/bin/openssl.exe
+  "${_CCPREFIX}strip" -p -s ${_pkg}/bin/*.dll
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
     "${_CCPREFIX}strip" -p -s ${engdir}/*.dll
   fi
 
-  ../_peclean.py "${_ref}" apps/openssl.exe
-  ../_peclean.py "${_ref}" apps/*.dll
+  ../_peclean.py "${_ref}" ${_pkg}/bin/openssl.exe
+  ../_peclean.py "${_ref}" ${_pkg}/bin/*.dll
 
-  ../_sign.sh apps/openssl.exe
-  ../_sign.sh apps/*.dll
+  ../_sign.sh ${_pkg}/bin/openssl.exe
+  ../_sign.sh ${_pkg}/bin/*.dll
 
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
     ../_peclean.py "${_ref}" ${engdir}/*.dll
@@ -89,22 +106,23 @@ _cpu="$2"
     ../_sign.sh ${engdir}/*.dll
   fi
 
-  touch -c -r "${_ref}" apps/openssl.exe
-  touch -c -r "${_ref}" apps/*.dll
-  touch -c -r "${_ref}" include/openssl/*.h
-  touch -c -r "${_ref}" ./*.a
-  touch -c -r "${_ref}" ./*.pc
+  touch -c -r "${_ref}" ${_pkg}/ssl/openssl.cnf
+  touch -c -r "${_ref}" ${_pkg}/bin/openssl.exe
+  touch -c -r "${_ref}" ${_pkg}/bin/*.dll
+  touch -c -r "${_ref}" ${_pkg}/include/openssl/*.h
+  touch -c -r "${_ref}" ${_pkg}/lib/*.a
+  touch -c -r "${_ref}" ${_pkg}/lib/pkgconfig/*.pc
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
     touch -c -r "${_ref}" ${engdir}/*.dll
   fi
 
   # Tests
 
-  "${_CCPREFIX}objdump" -x apps/openssl.exe | grep -E -i "(file format|dll name)"
-  "${_CCPREFIX}objdump" -x apps/*.dll       | grep -E -i "(file format|dll name)"
+  "${_CCPREFIX}objdump" -x ${_pkg}/bin/openssl.exe | grep -E -i "(file format|dll name)"
+  "${_CCPREFIX}objdump" -x ${_pkg}/bin/*.dll       | grep -E -i "(file format|dll name)"
 
-  ${_WINE} apps/openssl.exe version
-  ${_WINE} apps/openssl.exe ciphers
+  ${_WINE} ${_pkg}/bin/openssl.exe version
+  ${_WINE} ${_pkg}/bin/openssl.exe ciphers
 
   # Create package
 
@@ -115,21 +133,21 @@ _cpu="$2"
   mkdir -p "${_DST}/lib/pkgconfig"
 
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
-    mkdir -p "${_DST}/${engdir}"
-    cp -f -p ${engdir}/*.dll  "${_DST}/${engdir}/"
+    mkdir -p "${_DST}/lib/engines"
+    cp -f -p ${engdir}/*.dll "${_DST}/lib/engines/"
   fi
 
-  cp -f -p apps/openssl.cnf    "${_DST}/"
-  cp -f -p apps/openssl.exe    "${_DST}/"
-  cp -f -p apps/*.dll          "${_DST}/"
-  cp -f -p include/openssl/*.h "${_DST}/include/openssl/"
-  cp -f -p ./*.a               "${_DST}/lib/"
-  cp -f -p ./*.pc              "${_DST}/lib/pkgconfig/"
-  cp -f -p CHANGES             "${_DST}/CHANGES.txt"
-  cp -f -p LICENSE             "${_DST}/LICENSE.txt"
-  cp -f -p README              "${_DST}/README.txt"
-  cp -f -p FAQ                 "${_DST}/FAQ.txt"
-  cp -f -p NEWS                "${_DST}/NEWS.txt"
+  cp -f -p ${_pkg}/ssl/openssl.cnf     "${_DST}/"
+  cp -f -p ${_pkg}/bin/openssl.exe     "${_DST}/"
+  cp -f -p ${_pkg}/bin/*.dll           "${_DST}/"
+  cp -f -p ${_pkg}/include/openssl/*.h "${_DST}/include/openssl/"
+  cp -f -p ${_pkg}/lib/*.a             "${_DST}/lib/"
+  cp -f -p ${_pkg}/lib/pkgconfig/*.pc  "${_DST}/lib/pkgconfig/"
+  cp -f -p CHANGES                     "${_DST}/CHANGES.txt"
+  cp -f -p LICENSE                     "${_DST}/LICENSE.txt"
+  cp -f -p README                      "${_DST}/README.txt"
+  cp -f -p FAQ                         "${_DST}/FAQ.txt"
+  cp -f -p NEWS                        "${_DST}/NEWS.txt"
 
   # Luckily, applink is not implemented for 64-bit mingw, omit this file then
   [ "${_cpu}" = '32' ] && cp -f -p ms/applink.c "${_DST}/include/openssl/"
