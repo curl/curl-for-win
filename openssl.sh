a43240ba0a3c2bef88e9fb35f9ba92e7345b6c9e
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-14 17:35:25 +0000

    openssl: make engine handling version agnostic [ci skip]
    Also:
    * put back engines directory to the package root
    * do not rename the engine directory from the
      default, so it'll now be 'engines-1_1' (was: engines)

--- openssl.sh
+++ openssl.sh
@@ -85,7 +85,7 @@ _cpu="$2"
 
   # Make steps for determinism
 
-  engdir="${_pkg}/lib/engines-1_1"
+  engdir="${_pkg}/lib/engines*"
 
   "${_CCPREFIX}strip" -p --enable-deterministic-archives -g ${_pkg}/lib/*.a
   "${_CCPREFIX}strip" -p -s ${_pkg}/bin/openssl.exe
@@ -113,7 +113,7 @@ _cpu="$2"
   touch -c -r "${_ref}" ${_pkg}/lib/*.a
   touch -c -r "${_ref}" ${_pkg}/lib/pkgconfig/*.pc
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
-    touch -c -r "${_ref}" ${engdir}/*.dll
+    touch -c -r "${_ref}" ${engdir}/*
   fi
 
   # Tests
@@ -133,8 +133,7 @@ _cpu="$2"
   mkdir -p "${_DST}/lib/pkgconfig"
 
   if ls ${engdir}/*.dll > /dev/null 2>&1; then
-    mkdir -p "${_DST}/lib/engines"
-    cp -f -p ${engdir}/*.dll "${_DST}/lib/engines/"
+    cp -f -p -r ${engdir} "${_DST}/"
   fi
 
   cp -f -p ${_pkg}/ssl/openssl.cnf     "${_DST}/"
