d996d76eea4f024f7ad148a5c4546a3cbfd5c7df
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-14 10:24:36 +0000

    libssh2: add cmake build (yet inactive) [ci skip]

---
+++ libssh2-cmake.sh
@@ -0,0 +1,191 @@
+#!/bin/sh -ex
+
+# Copyright 2014-2018 Viktor Szakats <https://github.com/vszakats>
+# See LICENSE.md
+
+export _NAM
+export _VER
+export _BAS
+export _DST
+
+_NAM="$(basename "$0")"
+_NAM="$(echo "${_NAM}" | cut -f 1 -d '.' | cut -f 1 -d '-')"
+_VER="$1"
+_cpu="$2"
+
+(
+  cd "${_NAM}" || exit
+
+  # Cross-tasks
+
+  # Detect host OS
+  case "$(uname)" in
+    *_NT*)   os='win';;
+    Linux*)  os='linux';;
+    Darwin*) os='mac';;
+    *BSD)    os='bsd';;
+  esac
+
+  if [ "${os}" = 'win' ]; then
+    opt_gmsys='-GMSYS Makefiles'
+    # Without this option, the value '/usr/local' becomes 'msys64/usr/local'
+    export MSYS2_ARG_CONV_EXCL='-DCMAKE_INSTALL_PREFIX='
+  fi
+
+  # Build
+
+  rm -fr pkg
+
+  find . -name '*.a'   -type f -delete
+  find . -name '*.pc'  -type f -delete
+
+  for pass in 'static' 'shared'; do
+
+    rm -fr CMakeFiles CMakeCache.txt cmake_install.cmake
+
+    find . -name '*.o'   -type f -delete
+    find . -name '*.obj' -type f -delete
+    find . -name '*.lo'  -type f -delete
+    find . -name '*.la'  -type f -delete
+    find . -name '*.lai' -type f -delete
+    find . -name '*.Plo' -type f -delete
+
+    _CFLAGS="-m${_cpu} -fno-ident"
+    [ "${_cpu}" = '32' ] && _CFLAGS="${_CFLAGS} -fno-asynchronous-unwind-tables"
+    _LDFLAGS='-Wl,--nxcompat -Wl,--dynamicbase'
+    [ "${_cpu}" = '64' ] && [ "${_CCVER}" -ge '05' ] && _LDFLAGS="${_LDFLAGS} -Wl,--high-entropy-va -Wl,--image-base,0x152000000"
+    if [ "${_BRANCH#*master*}" = "${_BRANCH}" ]; then
+      _LDFLAGS="${_LDFLAGS} -Wl,-Map,libssh2.map"
+    fi
+
+    options='-DCMAKE_SYSTEM_NAME=Windows'
+    options="${options} -DCMAKE_BUILD_TYPE=Release"
+    [ "${pass}" = 'static' ] && options="${options} -DBUILD_SHARED_LIBS=0"
+    [ "${pass}" = 'shared' ] && options="${options} -DBUILD_SHARED_LIBS=1"
+    options="${options} -DBUILD_EXAMPLES=0"
+    options="${options} -DBUILD_TESTING=0"
+    options="${options} -DENABLE_ZLIB_COMPRESSION=1"
+    options="${options} -DZLIB_INCLUDE_DIR:PATH=$(pwd)/../zlib/pkg/usr/local/include"
+    options="${options} -DZLIB_LIBRARY:FILEPATH=$(pwd)/../zlib/pkg/usr/local/lib/libz.a"
+    options="${options} -DCRYPTO_BACKEND=OpenSSL"
+    options="${options} -DOPENSSL_ROOT_DIR=$(pwd)/../openssl/pkg/usr/local/"
+    options="${options} -DOPENSSL_INCLUDE_DIR=$(pwd)/../openssl/pkg/usr/local/include"
+    options="${options} -DOPENSSL_LIBRARIES=$(pwd)/../openssl/pkg/usr/local/lib"
+    options="${options} -DOPENSSL_CRYPTO_LIBRARY=$(pwd)/../openssl/pkg/usr/local/lib"
+    options="${options} -DCMAKE_RC_COMPILER=${_CCPREFIX}windres"
+    options="${options} -DCMAKE_INSTALL_MESSAGE=NEVER"
+    options="${options} -DCMAKE_INSTALL_PREFIX=/usr/local"
+
+    # https://cmake.org/cmake/help/v3.11/manual/cmake-properties.7.html#properties-on-targets
+    [ "${pass}" = 'shared' ] && [ "${_cpu}" = '64' ] && options="${options} -DCMAKE_RELEASE_POSTFIX=-x64"
+
+    if [ "${CC}" = 'mingw-clang' ]; then
+      unset CC
+
+      [ "${os}" = 'linux' ] && _CFLAGS="-L$(find "/usr/lib/gcc/${_TRIPLET}" -name '*posix' | head -n 1) ${_CFLAGS}"
+
+      # '-DMINGW=1' required to detect OpenSSL
+
+      # shellcheck disable=SC2086
+      cmake . ${options} "${opt_gmsys}" \
+        "-DMINGW=1" \
+        "-DCMAKE_SYSROOT=${_SYSROOT}" \
+        "-DCMAKE_LIBRARY_ARCHITECTURE=${_TRIPLET}" \
+        "-DCMAKE_C_COMPILER_TARGET=${_TRIPLET}" \
+        "-DCMAKE_C_COMPILER=clang" \
+        "-DCMAKE_C_FLAGS=${_CFLAGS}" \
+        "-DCMAKE_EXE_LINKER_FLAGS=-static-libgcc" \
+        "-DCMAKE_SHARED_LINKER_FLAGS=-static-libgcc ${_LDFLAGS}"
+    else
+      unset CC
+
+      # shellcheck disable=SC2086
+      cmake . ${options} "${opt_gmsys}" \
+        "-DCMAKE_C_COMPILER=${_CCPREFIX}gcc" \
+        "-DCMAKE_C_FLAGS=-static-libgcc ${_CFLAGS}" \
+        "-DCMAKE_SHARED_LINKER_FLAGS=${_LDFLAGS}"
+    fi
+
+    make install "DESTDIR=$(pwd)/pkg"  # VERBOSE=1
+  done
+
+  # DESTDIR= + CMAKE_INSTALL_PREFIX
+  _pkg='pkg/usr/local'
+
+  ls -l ${_pkg}/bin/*.dll
+  ls -l ${_pkg}/lib/*.a
+
+  # Stick to the name used by GNU Make builds
+  [ "${_cpu}" = '32' ] && mv -f ${_pkg}/lib/liblibssh2.dll.a     ${_pkg}/lib/libssh2.dll.a
+  [ "${_cpu}" = '64' ] && mv -f ${_pkg}/lib/liblibssh2-x64.dll.a ${_pkg}/lib/libssh2.dll.a
+
+  # curl makefile.m32 assumes a certain layout.
+  # Also make sure to copy the static library only:
+  mkdir -p "${_pkg}/win32/"
+  cp -f -p ${_pkg}/lib/libssh2.a "${_pkg}/win32/"
+
+  # Make steps for determinism
+
+  readonly _ref='NEWS'
+
+  "${_CCPREFIX}strip" -p --enable-deterministic-archives -g ${_pkg}/lib/*.a
+  "${_CCPREFIX}strip" -p -s ${_pkg}/bin/*.dll
+
+  ../_peclean.py "${_ref}" ${_pkg}/bin/*.dll
+
+  ../_sign.sh ${_pkg}/bin/*.dll
+
+  touch -c -r "${_ref}" ${_pkg}/bin/*.dll
+  touch -c -r "${_ref}" ${_pkg}/lib/*.a
+  touch -c -r "${_ref}" ${_pkg}/lib/pkgconfig/*.pc
+
+  if [ "${_BRANCH#*master*}" = "${_BRANCH}" ]; then
+    touch -c -r "${_ref}" ${_pkg}/bin/*.map
+    touch -c -r "${_ref}" ${_pkg}/bin/*.def || true
+  fi
+
+  # Tests
+
+  "${_CCPREFIX}objdump" -x ${_pkg}/bin/*.dll | grep -E -i "(file format|dll name)"
+
+  # Create package
+
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _DST="$(mktemp -d)/${_BAS}"
+
+  mkdir -p "${_DST}/docs"
+  mkdir -p "${_DST}/bin"
+  mkdir -p "${_DST}/lib"
+  mkdir -p "${_DST}/lib/pkgconfig"
+  mkdir -p "${_DST}/include"
+
+  (
+    set +x
+    for file in docs/*; do
+      if [ -f "${file}" ] && [ "${file}" != 'Makefile' ] && echo "${file}" | grep -q -v '\.'; then
+        cp -f -p "${file}" "${_DST}/${file}.txt"
+      fi
+    done
+  )
+  cp -f -p ${_pkg}/include/*.h        "${_DST}/include/"
+  cp -f -p ${_pkg}/bin/*.dll          "${_DST}/bin/"
+  cp -f -p ${_pkg}/lib/*.a            "${_DST}/lib/"
+  cp -f -p ${_pkg}/lib/pkgconfig/*.pc "${_DST}/lib/pkgconfig/"
+  cp -f -p NEWS                       "${_DST}/NEWS.txt"
+  cp -f -p COPYING                    "${_DST}/COPYING.txt"
+  cp -f -p README                     "${_DST}/README.txt"
+  cp -f -p RELEASE-NOTES              "${_DST}/RELEASE-NOTES.txt"
+
+  [ -d ../openssl ]  && cp -f -p ../openssl/LICENSE "${_DST}/LICENSE-openssl.txt"
+
+  if [ "${_BRANCH#*master*}" = "${_BRANCH}" ]; then
+    cp -f -p ${_pkg}/bin/*.map   "${_DST}/bin/"
+    cp -f -p ${_pkg}/bin/*.def   "${_DST}/bin/" || true
+  fi
+
+  unix2dos -q -k "${_DST}"/*.txt
+  unix2dos -q -k "${_DST}"/docs/*.txt
+
+  ../_pack.sh "$(pwd)/${_ref}"
+  ../_ul.sh
+)
