03769bef8be1155dcad967f4ed85412aec3b6479
Viktor Szakats <commit@vszakats.net>
2018-06-11 19:00:10 +0000

    _ci_linux_ubuntu.sh: sync with debian [ci skip]

--- _ci_linux_ubuntu.sh
+++ _ci_linux_ubuntu.sh
@@ -14,8 +14,8 @@ dpkg --add-architecture i386
 apt-get -qq update
 # shellcheck disable=SC2086
 apt-get -qq install \
-  curl git make python3-pip \
-  binutils-mingw-w64 gcc-mingw-w64 ${_optpkg} cmake \
+  curl git gpg python3-pip make cmake \
+  binutils-mingw-w64 gcc-mingw-w64 ${_optpkg} \
   zip time jq dos2unix osslsigncode wine-stable wine64 wine32
 
 ./_build.sh
