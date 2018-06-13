1b71bce42c340e4481eb860cf4a6600a6f92e4f8
Viktor Szakats <commit@vszakats.net>
2018-06-13 10:22:15 +0000

    _ci_win_msys2.sh: formatting [ci skip]

--- _ci_win_msys2.sh
+++ _ci_win_msys2.sh
@@ -3,11 +3,14 @@
 # Copyright 2016-2018 Viktor Szakats <https://vszakats.net/>
 # See LICENSE.md
 
-pacman --noconfirm --ask 20 --noprogressbar -S --needed mc
+pacman --noconfirm --ask 20 --noprogressbar -S --needed \
+  mc
 pacman --noconfirm --ask 20 --noprogressbar -S -yu -u
 pacman --noconfirm --ask 20 --noprogressbar -S -yu -u
 pacman --noconfirm --ask 20 --noprogressbar -S --needed \
-  zip mingw-w64-{i686,x86_64}-{cmake,jq,osslsigncode,python3-pip}
+  mingw-w64-{i686,x86_64}-{cmake,jq,osslsigncode,python3-pip} \
+  zip
+
 [ "${CC}" = 'mingw-clang' ] && \
 pacman --noconfirm --ask 20 --noprogressbar -S --needed \
   mingw-w64-{i686,x86_64}-clang
