bebaa5d8f5c6b60bd65730f437fae07a74001c22
Viktor Szakats <commit@vszakats.net>
2018-08-31 17:04:07 +0000

    debian: install openssl headers

--- _ci_linux_debian.sh
+++ _ci_linux_debian.sh
@@ -16,6 +16,7 @@ apt-get -qq -o=Dpkg::Use-Pty=0 update
 # shellcheck disable=SC2086
 apt-get -qq -o=Dpkg::Use-Pty=0 install \
   curl git gpg python3-pip make cmake \
+  libssl-dev \
   gcc-mingw-w64 ${_optpkg} \
   zip time jq dos2unix osslsigncode wine64 wine32
 
