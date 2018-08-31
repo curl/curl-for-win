66d92e3f47da223c1b838b40f01ba0b4579731cb
Viktor Szakats <commit@vszakats.net>
2018-08-31 20:31:33 +0000

    _sign.sh: bump copyright year [ci skip]

--- _sign.sh
+++ _sign.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2016-2017 Viktor Szakats <https://vszakats.net/>
+# Copyright 2016-2018 Viktor Szakats <https://vszakats.net/>
 # See LICENSE.md
 
 if [ -f "${CODESIGN_KEY}" ] && \
