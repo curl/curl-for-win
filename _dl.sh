bd863a198e17fb8ace578ca7030304acaabb4335
Viktor Szakats <commit@vszakats.net>
2018-09-05 06:32:26 +0000

    curl 7.61.1

--- _dl.sh
+++ _dl.sh
@@ -17,8 +17,8 @@ export OPENSSL_VER_='1.1.0i'
 export OPENSSL_HASH=ebbfc844a8c8cc0ea5dc10b86c9ce97f401837f3fa08c17b2cdadc118253cf99
 export LIBSSH2_VER_='1.8.0'
 export LIBSSH2_HASH=39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4
-export CURL_VER_='7.61.0'
-export CURL_HASH=ef6e55192d04713673b4409ccbcb4cb6cd723137d6e10ca45b0c593a454e1720
+export CURL_VER_='7.61.1'
+export CURL_HASH=3d5913d6a39bd22e68e34dff697fd6e4c3c81563f580c76fca2009315cd81891
 export OSSLSIGNCODE_VER_='1.7.1'
 export OSSLSIGNCODE_HASH=f9a8cdb38b9c309326764ebc937cba1523a3a751a7ab05df3ecc99d18ae466c9
 
@@ -26,7 +26,7 @@ export OSSLSIGNCODE_HASH=f9a8cdb38b9c309326764ebc937cba1523a3a751a7ab05df3ecc99d
 # NOTE: Set _REV to empty after bumping CURL_VER_, and
 #       set it to 1 then increment by 1 each time bumping a dependency
 #       version or pushing a CI rebuild for the master branch.
-export _REV='5'
+export _REV=''
 
 [ -z "${_REV}" ] || _REV="_${_REV}"
 
