5045e6fc927539832989788cf4c3450ea90877af
Viktor Szakats <commit@vszakats.net>
2018-10-26 22:59:47 +0000

    fix to previous commit

--- osslsigncode.sh
+++ osslsigncode.sh
@@ -34,7 +34,7 @@ _VER="$1"
 
   # shellcheck disable=SC2086
   gcc -O3 \
-    osslsigncode.c -o ../osslsigncode-determ \
+    osslsigncode.c -o "$(dirname "$0")/osslsigncode-determ" \
     -DHAVE_SYS_MMAN_H \
     ${options} \
     '-DPACKAGE_BUGREPORT="none"' \
