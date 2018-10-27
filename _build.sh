689b9b39f5f5c61f0e902748e0f6474d30e43373
Viktor Szakats <commit@vszakats.net>
2018-10-27 01:40:25 +0000

    one more signing fix

--- _build.sh
+++ _build.sh
@@ -169,6 +169,8 @@ build_single_target() {
   echo ".gcc-mingw-w64-${_machine} $(${_CCPREFIX}gcc -dumpversion)" >> "${_BLD}"
   echo ".binutils-mingw-w64-${_machine} $(${_CCPREFIX}ar V | grep -o -E '[0-9]+\.[0-9]+[\.][0-9]*')" >> "${_BLD}"
 
+# command -v "$(dirname "$0")/osslsigncode-determ" > /dev/null 2>&1 || unset CODESIGN_KEY
+
   time ./zlib.sh       "${ZLIB_VER_}" "${_cpu}"
   time ./libhsts.sh "${LIBHSTS_VER_}" "${_cpu}"
   time ./brotli.sh   "${BROTLI_VER_}" "${_cpu}"
