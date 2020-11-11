9f786d57081a54d87915755aea078add170d0259
Viktor Szakats <commit@vsz.me>
2020-11-11 03:14:09 +0000

    move upload logic from _build.sh to _ul.sh [ci skip]

--- _build.sh
+++ _build.sh
@@ -59,6 +59,7 @@ rm -f ./*-*-mingw*.*
 rm -f hashes.txt
 rm -f "${_BLD}"
 
+# Download sources
 . ./_dl.sh || exit 1
 
 # Decrypt package signing key
@@ -203,6 +204,7 @@ build_single_target() {
   time ./curl.sh       "${CURL_VER_}" "${_cpu}"
 }
 
+# Build binaries
 if [ -n "$CPU" ]; then
   build_single_target "${CPU}"
 else
@@ -210,55 +212,5 @@ else
   build_single_target 32
 fi
 
-sort "${_BLD}" > "${_BLD}.sorted"
-mv -f "${_BLD}.sorted" "${_BLD}"
-
-# Use the newest package timestamp for supplementary files
-# shellcheck disable=SC2012
-touch -r "$(ls -1 -t ./*-*-mingw*.* | head -1)" hashes.txt "${_BLD}" "${_LOG}"
-
-ls -l ./*-*-mingw*.*
-cat hashes.txt
-cat "${_BLD}"
-
-# Strip '-built-on-*' suffix for the single-file artifact,
-# and also add revision to filenames.
-for f in ./*-*-mingw*.*; do
-  mv -f "${f}" "$(echo "${f}" | sed "s|-win|${_REV}-win|g" | sed 's|-built-on-[^.]*||g')"
-done
-
-sed "s|-win|${_REV}-win|g" hashes.txt | sed 's|-built-on-[^.]*||g' | sort > hashes.txt.all
-touch -r hashes.txt hashes.txt.all
-mv -f hashes.txt.all hashes.txt
-
-# Create an artifact that includes all packages
-_ALL="all-mingw-${CURL_VER_}${_REV}.zip"
-zip --quiet -0 -X --latest-time "${_ALL}" ./*-*-mingw*.* hashes.txt "${_BLD}" "${_LOG}"
-
-openssl dgst -sha256 "${_ALL}" | tee    "${_ALL}.txt"
-openssl dgst -sha512 "${_ALL}" | tee -a "${_ALL}.txt"
-
-./_signpack "${_ALL}"
-
-# Official deploy
-if [ "${_BRANCH#*master*}" != "${_BRANCH}" ] && \
-   [ "${PUBLISH_PROD_FROM}" = "${_OS}" ]; then
-(
-  set +x
-  if [ -f "${DEPLOY_KEY}" ]; then
-    echo "Uploading: '${_ALL}'"
-    rsync \
-      --archive \
-      --rsh "ssh \
-        -i '${DEPLOY_KEY}' \
-        -o BatchMode=yes \
-        -o StrictHostKeyChecking=yes \
-        -o ConnectTimeout=20 \
-        -o ConnectionAttempts=5" \
-      "${_ALL}" \
-      "${_ALL}.asc" \
-      "${_ALL}.txt" \
-      'curl-for-win@silly.haxx.se:.'
-  fi
-)
-fi
+# Upload/deploy binaries
+. ./_ul.sh || exit 1
