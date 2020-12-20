curl/curl-for-win

commit b491109a803743133934c4c56b279b077ea45174

add release revision to package filenames on creation [ci skip]

brotli.sh
@@ -112,7 +112,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"

c-ares.sh
@@ -116,7 +116,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"

curl.sh
@@ -208,7 +208,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   [ -d ../brotli ] || _BAS="${_BAS}-nobrotli"
   _DST="$(mktemp -d)/${_BAS}"
 

libidn2.sh
@@ -89,7 +89,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include"

libssh2.sh
@@ -94,7 +94,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/docs"

libssh2_cmake.sh
@@ -142,7 +142,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/docs"

nghttp2.sh
@@ -99,7 +99,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/nghttp2"

nghttp3.sh
@@ -99,7 +99,7 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/nghttp3"

commit 44b39a9b809c22c965413db827f082b3a96fa24e

restore behaviour where the main archive is revision-less [ci skip]
otherwise the hash will change with each revision bump for every
package, masking their reproducibility.

Regression from: b491109a803743133934c4c56b279b077ea45174

_pkg.sh
@@ -46,7 +46,7 @@ create_pkg() {
     _suf="-test-built-on-${_OS}"
   fi
 
-  _pkg="${_BAS}${_suf}${arch_ext}"
+  _pkg="${_PKG}${_suf}${arch_ext}"
 
   _FLS="$(dirname "$0")/_files"
 

brotli.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -112,7 +113,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"

c-ares.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -116,7 +117,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"

curl.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -207,7 +208,9 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
+  [ -d ../brotli ] || _PKG="${_PKG}-nobrotli"
   [ -d ../brotli ] || _BAS="${_BAS}-nobrotli"
   _DST="$(mktemp -d)/${_BAS}"
 

libidn2.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -89,7 +90,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include"

libssh2.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -94,7 +95,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/docs"

libssh2_cmake.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -142,7 +143,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/docs"

nghttp2.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -99,7 +100,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/nghttp2"

nghttp3.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -99,7 +100,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/nghttp3"

ngtcp2.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -86,7 +87,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/ngtcp2"

openssl.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -177,7 +178,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}/include/openssl"

zlib.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -117,7 +118,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"

zstd.sh
@@ -5,6 +5,7 @@
 
 export _NAM
 export _VER
+export _PKG
 export _BAS
 export _DST
 
@@ -142,7 +143,8 @@ _cpu="$2"
 
   # Create package
 
-  _BAS="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _PKG="${_NAM}-${_VER}${_REV}-win${_cpu}-mingw"
+  _BAS="${_NAM}-${_VER}-win${_cpu}-mingw"
   _DST="$(mktemp -d)/${_BAS}"
 
   mkdir -p "${_DST}"