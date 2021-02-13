d7f7ed60fce82e7665f96d0992e2d5e36b9c0f92
Viktor Szakats <commit@vsz.me>
2021-02-13 20:39:17 +0000

    cleanup [ci skip]

--- _ci-win-msys2.sh
+++ _ci-win-msys2.sh
@@ -6,11 +6,11 @@
 pacman --noconfirm --ask 20 --noprogressbar --sync --refresh --sysupgrade --sysupgrade
 pacman --noconfirm --ask 20 --noprogressbar --sync --refresh --sysupgrade --sysupgrade
 pacman --noconfirm --ask 20 --noprogressbar --sync --needed \
-  mingw-w64-{i686,x86_64}-{cmake,jq,python3-pip,rsync} \
+  mingw-w64-{x86_64,i686}-{cmake,jq,python3-pip,rsync} \
   zip zstd
 
 [ "${CC}" = 'mingw-clang' ] && \
 pacman --noconfirm --ask 20 --noprogressbar --sync --needed \
-  mingw-w64-{i686,x86_64}-clang
+  mingw-w64-{x86_64,i686}-clang
 
 ./_build.sh
