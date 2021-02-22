21035dd13e27e2b18016d0cf5213bec0a3594497
Viktor Szakats <commit@vsz.me>
2021-02-22 19:08:19 +0000

    _ci-mac-homebrew.sh: disable analytics, update cask command-line [ci skip]

--- _ci-mac-homebrew.sh
+++ _ci-mac-homebrew.sh
@@ -6,11 +6,12 @@
 [ "${CC}" = 'mingw-clang' ] && _optpkg='llvm'
 
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ANALYTICS=1
 time brew update >/dev/null
 time brew upgrade python
 time brew install xz zstd gnu-tar mingw-w64 ${_optpkg} \
                   jq dos2unix gnu-sed openssl@1.1
-time brew cask install wine-stable
+time brew install --cask wine-stable
 time wineboot --init
 
 ./_build.sh
