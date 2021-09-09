curl/curl-for-win

commit 5a0e8858e89e7612ff7bf8784a0b901f3710e037

openssl.sh: simplify/robustify include/lib folder processing [ci skip]

openssl.sh
@@ -170,15 +170,10 @@ _VER="$1"
   touch -c -r "${_ref}" "${_pks}"/*.cnf*
   touch -c -r "${_ref}" "${_pkg}"/bin/openssl.exe
   touch -c -r "${_ref}" "${_pkg}"/bin/*.dll
-  touch -c -r "${_ref}" "${_pkg}"/include/openssl/*.h
   touch -c -r "${_ref}" "${_pkg}${_lib}"/*.a
   touch -c -r "${_ref}" "${_pkg}${_lib}"/pkgconfig/*.pc
-  if ls "${_pkg}${_lib}"/ossl-modules/*.dll >/dev/null 2>&1; then
-    touch -c -r "${_ref}" "${_pkg}${_lib}"/ossl-modules/*
-  fi
-  if ls "${_pkg}${_lib}"/engines*/*.dll >/dev/null 2>&1; then
-    touch -c -r "${_ref}" "${_pkg}${_lib}"/engines*/*
-  fi
+  find "${_pkg}"/include/openssl -exec touch -c -r "${_ref}" '{}' \;
+  find "${_pkg}${_lib}" -exec touch -c -r "${_ref}" '{}' \;
 
   # Tests
 
@@ -196,7 +191,7 @@ _VER="$1"
 
   mkdir -p "${_DST}"
 
-  cp -f -p -r "${_pkg}"/include "${_DST}/"
+  cp -f -p -r "${_pkg}"/include/openssl "${_DST}/"
 
   if [ -f 'CHANGES.md' ]; then
     # OpenSSL 3.x
@@ -234,12 +229,6 @@ _VER="$1"
     cp -f -p README      "${_DST}/README.txt"
     cp -f -p FAQ         "${_DST}/FAQ.txt"
     cp -f -p NEWS        "${_DST}/NEWS.txt"
-  fi
-
-  # Luckily, applink is not implemented for 64-bit mingw, omit this file then
-  if [ "${_CPU}" = 'x86' ] && [ -r ms/applink.c ]; then
-    touch -c -r "${_ref}" ms/applink.c
-    cp -f -p ms/applink.c "${_DST}/include/openssl/"
   fi
 
   ../_pkg.sh "$(pwd)/${_ref}"

commit 442d5cf6693f316dea9bde843565f96a69cfff48

openssl.sh: fix to distribute applink.c
Regression from 5a0e8858e89e7612ff7bf8784a0b901f3710e037.

_dl.sh
@@ -7,7 +7,7 @@
 # NOTE: Set _REV to empty after bumping CURL_VER_, and
 #       set it to 1 then increment by 1 each time bumping a dependency
 #       version or pushing a CI rebuild for the main branch.
-export _REV='5'
+export _REV='6'
 
 export CURL_VER_='7.78.0'
 export CURL_HASH=be42766d5664a739c3974ee3dfbbcbe978a4ccb1fe628bb1d9b59ac79e445fb5

openssl.sh
@@ -190,5 +190,10 @@ _VER="$1"
   cp -f -p FAQ.md      "${_DST}/"
   cp -f -p NEWS.md     "${_DST}/"
 
+  if [ "${_CPU}" = 'x86' ] && [ -r ms/applink.c ]; then
+    touch -c -r "${_ref}" ms/applink.c
+    cp -f -p ms/applink.c "${_DST}/include/openssl/"
+  fi
+
   ../_pkg.sh "$(pwd)/${_ref}"
 )