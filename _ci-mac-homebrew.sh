52199f95c38b40a5d35ac7dddb98304e260d3032
Viktor Szakats <commit@vsz.me>
2021-10-10 16:43:49 +0000

    _ci-mac-homebrew.sh: use default openssl version (= 3.x) [ci skip]

--- _ci-mac-homebrew.sh
+++ _ci-mac-homebrew.sh
@@ -9,7 +9,7 @@ export HOMEBREW_NO_ANALYTICS=1
 time brew update >/dev/null
 time brew upgrade python
 time brew install xz zstd gnu-tar mingw-w64 ${_optpkg} gettext \
-                  jq dos2unix openssl@3 osslsigncode openssh
+                  jq dos2unix openssl osslsigncode openssh
 time brew install --cask wine-stable
 time wineboot --init
 
