dfd4316824ff9344fd5574e5af91a3e9102845c2
Viktor Szakats <commit@vsz.me>
2022-05-23 09:18:08 +0000

    libressl: add the actual CMake script [ci skip]
    At the moment this method seems to leak private information into the
    binaries, so it's not suitable for public releases. Reported upstream.
    
    Ref: https://github.com/libressl-portable/portable/issues/761

---
+++ libressl-cmake.sh
@@ -0,0 +1,149 @@
+#!/bin/sh
+
+# Copyright 2014-present Viktor Szakats. See LICENSE.md
+
+# shellcheck disable=SC3040
+set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
+
+# NOTE: LibreSSL uses __FILE__ (and __LINE__) in its source code with no
+#       option to disable it. It means that personal or confidential
+#       information leaks into the built objects at compile time, making this
+#       information visible in object files. It also makes it problematic
+#       to create reproducible binaries. In practice this is a problem when
+#       using CMake, because it will convert source filenames into absolute
+#       ones before passing them to the compiler. autotools builds pass the
+#       path relative to the source root, so it is fine.
+
+export _NAM
+export _VER
+export _OUT
+export _BAS
+export _DST
+
+_NAM="$(basename "$0")"
+_NAM="$(echo "${_NAM}" | cut -f 1 -d '.')"
+_VER="$1"
+
+(
+  cd "${_NAM}" || exit 0
+
+  # Cross-tasks
+
+  if [ "${_OS}" = 'win' ]; then
+    opt_gmsys='-GMSYS Makefiles'
+    # Without this option, the value '/usr/local' becomes 'msys64/usr/local'
+    export MSYS2_ARG_CONV_EXCL='-DCMAKE_INSTALL_PREFIX='
+  else
+    opt_gmsys=''
+  fi
+
+  # Build
+
+  rm -r -f pkg CMakeFiles CMakeCache.txt CTestTestfile.cmake cmake_install.cmake
+
+  find . -name '*.o'   -delete
+  find . -name '*.obj' -delete
+  find . -name '*.a'   -delete
+  find . -name '*.lo'  -delete
+  find . -name '*.la'  -delete
+  find . -name '*.lai' -delete
+  find . -name '*.Plo' -delete
+  find . -name '*.pc'  -delete
+
+  _CFLAGS="${_OPTM} -fno-ident"
+  [ "${_CPU}" = 'x86' ] && _CFLAGS="${_CFLAGS} -fno-asynchronous-unwind-tables"
+
+  options=''
+  options="${options} -DCMAKE_SYSTEM_NAME=Windows"
+  options="${options} -DCMAKE_BUILD_TYPE=Release"
+  # A bizarre fix that became required around year 2021 to not fail instantly
+  # on macOS when using clang. Likely not the correct/complete fix.
+  [ "${_OS}" = 'mac' ] && options="${options} -DCMAKE_AR=${_SYSROOT}/bin/${_CCPREFIX}ar"
+  options="${options} -DLIBRESSL_TESTS=OFF"
+  options="${options} -DLIBRESSL_APPS=OFF"
+  options="${options} -DCMAKE_RC_COMPILER=${_CCPREFIX}windres"
+  options="${options} -DCMAKE_RC_FLAGS=-DGCC_WINDRES"
+  options="${options} -DCMAKE_INSTALL_MESSAGE=NEVER"
+
+  if [ "${CC}" = 'mingw-clang' ]; then
+    unset CC
+
+    [ "${_OS}" = 'linux' ] && _CFLAGS="-L$(find "/usr/lib/gcc/${_TRIPLET}" -name '*posix' | head -n 1) ${_CFLAGS}"
+
+  # _CFLAGS="${_CFLAGS} -Xclang -cfguard"
+
+    # shellcheck disable=SC2086
+    cmake . ${options} ${opt_gmsys} \
+      "-DCMAKE_SYSROOT=${_SYSROOT}" \
+      "-DCMAKE_LIBRARY_ARCHITECTURE=${_TRIPLET}" \
+      "-DCMAKE_C_COMPILER_TARGET=${_TRIPLET}" \
+      "-DCMAKE_C_COMPILER=clang${_CCSUFFIX}" \
+      "-DCMAKE_C_FLAGS=${_CFLAGS}"
+  else
+    unset CC
+
+    # shellcheck disable=SC2086
+    cmake . ${options} ${opt_gmsys} \
+      "-DCMAKE_C_COMPILER=${_CCPREFIX}gcc" \
+      "-DCMAKE_C_FLAGS=-static-libgcc ${_CFLAGS}"
+  fi
+
+  make --jobs 2 install "DESTDIR=$(pwd)/pkg"
+
+  # DESTDIR= + CMAKE_INSTALL_PREFIX
+  _pkg='pkg/usr/local'
+
+  # Build fixups for CMake
+
+  # CMake creates static libs with version numbers in them,
+  # e.g. libcrypto-49.a. Strangely the .pc files do not have
+  # them.
+  # Strip those to make them findable by other projects.
+  for l in libcrypto libssl libtls; do
+    mv "${_pkg}/lib/${l}"*.a "${_pkg}/lib/${l}.a"
+  done
+
+  # List files created
+
+  find "${_pkg}" | grep -a -v -F '/share/' | sort
+
+  # Make steps for determinism
+
+  readonly _ref='ChangeLog'
+
+  "${_CCPREFIX}strip" --preserve-dates --strip-debug --enable-deterministic-archives ${_pkg}/lib/*.a
+
+  touch -c -r "${_ref}" ${_pkg}/lib/*.a
+  touch -c -r "${_ref}" ${_pkg}/lib/pkgconfig/*.pc
+  touch -c -r "${_ref}" ${_pkg}/include/openssl/*.h
+  touch -c -r "${_ref}" ${_pkg}/include/*.h
+
+  # Tests
+
+  for bin in \
+    ${_pkg}/bin/openssl.exe \
+  ; do
+    file "${bin}"
+    # Produce 'openssl version -a'-like output without executing the build:
+    strings "${bin}" | grep -a -E '^(LibreSSL [0-9]|built on: |compiler: |platform: |[A-Z]+DIR: )' || true
+  done
+
+  # Create package
+
+  _OUT="${_NAM}-${_VER}${_REVSUFFIX}${_PKGSUFFIX}"
+  _BAS="${_NAM}-${_VER}${_PKGSUFFIX}"
+  _DST="$(mktemp -d)/${_BAS}"
+
+  mkdir -p "${_DST}/include/openssl"
+  mkdir -p "${_DST}/lib/pkgconfig"
+
+  cp -f -p ${_pkg}/lib/*.a             "${_DST}/lib"
+  cp -f -p ${_pkg}/lib/pkgconfig/*.pc  "${_DST}/lib/pkgconfig/"
+  cp -f -p ${_pkg}/include/openssl/*.h "${_DST}/include/openssl/"
+  cp -f -p ${_pkg}/include/*.h         "${_DST}/include/"
+  cp -f -p ChangeLog                   "${_DST}/ChangeLog.txt"
+  cp -f -p COPYING                     "${_DST}/COPYING.txt"
+  cp -f -p README.md                   "${_DST}/"
+
+  ../_pkg.sh "$(pwd)/${_ref}"
+)
