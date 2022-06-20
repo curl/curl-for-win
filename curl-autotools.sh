e824343a5005b8a502d9139329592320d812d74e
Viktor Szakats <commit@vsz.me>
2022-06-20 11:04:43 +0000

    curl-autotools.sh: static builds now work. shared broken. [ci skip]
    - disable building/running tests also in non-cross-builds
    - fix to specify LD for libtool. (even though we use lld in ucrt
      builds, but lld is not known by libtool, so we mis-specify GNU
      ld, being the closest.
    - add extremely ugly hack that makes libtool find system and
      certain other implibs with a .lib extension.
    - add hack to find system implibs when trying to link DLL.
    - split building shared and static binaries. static mode does
      work now, but shared is seriously broken. It is possible
      that a single pass may be enough to build libcurl.a, libcurl.dll
      and a static curl.exe, but for now, linking libcurl.dll bumps
      into a series of issues, e.g. passing an assortment of wrong
      arguments to the linker, hacks necessary to pass pre-linking
      libtool checks, and then generating a corrupt (in multiple ways,
      e.g. lines chopped at 'n' characters, and a broken PATH line)
      "ltwrapper", that it insist on creating as a runner for curl.exe
      built agains libcurl.dll (or something like this).
    - enable some more linker options
    - this is a mess. most of it is probably wrong, including the
      premisses and the implemented hacks. it's a nightmare. and slow.

--- curl-autotools.sh
+++ curl-autotools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# [AUTOTOOLS BROKEN, INCOMPLETE]
+# [SHARED BUILD (DLL) BROKEN]
 
 # Copyright 2014-present Viktor Szakats. See LICENSE.md
 
@@ -30,13 +30,24 @@ _VER="$1"
   find . -name '*.dll' -delete
   find . -name '*.def' -delete
 
-  # Skip building tests
-# sed -i.bak 's| tests||g' ./Makefile.am
+  # Skip building tests in non-cross-build cases
+  sed -i.bak 's| tests packages| packages|g' ./Makefile.am
 
   if [ ! -f 'Makefile' ]; then
     autoreconf --force --install
     cp -f -p Makefile.dist Makefile
   fi
+
+  # For 'shared' builds, create fake .libs to pass libtool's test for implibs.
+  for fn in advapi32 crypt32 wldap32 ws2_32 normaliz ucrt z; do
+    if [ "${fn}" = 'ucrt' ] || \
+       [ "${fn}" = 'z' ]; then
+      fnt=advapi32  # Any implib will do here.
+    else
+      fnt="${fn}"
+    fi
+    ln -s -f "${_SYSROOT}/${_TRIPLET}/lib/lib${fnt}.a" "../lib${fn}.lib"
+  done
 
   # Generate .def file for libcurl by parsing curl headers. Useful to export
   # the libcurl function meant to be exported.
@@ -55,62 +66,64 @@ _VER="$1"
       | sed -E 's|^ *\*? *([a-z_]+) *\(.+$|\1|g'
   } | grep -a -v '^$' | sort | tee -a libcurl.def
 
-  export LDFLAGS="${_OPTM}"
-  # Use -DCURL_STATICLIB when compiling libcurl. This option prevents
-  # marking public libcurl functions as 'exported'. Useful to avoid the
-  # chance of libcurl functions getting exported from final binaries when
-  # linked against static libcurl lib.
-  export CFLAGS='-fno-ident -O3'
-  export CPPFLAGS='-DCURL_STATICLIB -DHAVE_ATOMIC -DHAVE_IOCTLSOCKET_FIONBIO -DHAVE_SOCKET'
-  export LIBS=''
-  ldonly=''
+  for pass in static; do  # FIXME: 'shared' broken.
 
-  # configure: error: --enable-unix-sockets is not available on this platform!
-  # due to non-portable verification method.
-  CPPFLAGS="${CPPFLAGS} -DUSE_UNIX_SOCKETS"
+    export LDFLAGS="${_OPTM}"
+    # Use -DCURL_STATICLIB when compiling libcurl. This option prevents
+    # marking public libcurl functions as 'exported'. Useful to avoid the
+    # chance of libcurl functions getting exported from final binaries when
+    # linked against static libcurl lib.
+    export CFLAGS='-fno-ident -O3'
+    export CPPFLAGS='-DCURL_STATICLIB -DHAVE_ATOMIC -DHAVE_IOCTLSOCKET_FIONBIO -DHAVE_SOCKET'
+    export LIBS=''
+    ldonly=''
 
-  uselld=0
-  if [ "${_CRT}" = 'ucrt' ]; then
+    # configure: error: --enable-unix-sockets is not available on this platform!
+    # due to non-portable verification method.
+    CPPFLAGS="${CPPFLAGS} -DUSE_UNIX_SOCKETS"
+
+    uselld=0
+    if [ "${_CRT}" = 'ucrt' ]; then
+      if [ "${_CC}" = 'clang' ]; then
+        ldonly="${ldonly} -fuse-ld=lld -s"
+        uselld=1
+      else
+        ldonly="${ldonly} -specs=${_GCCSPECS}"
+      fi
+      CPPFLAGS="${CPPFLAGS} -D_UCRT"
+      LIBS="${LIBS} -lucrt"
+    fi
+
     if [ "${_CC}" = 'clang' ]; then
-      ldonly="${ldonly} -fuse-ld=lld -s"
-      uselld=1
+      export CC='clang'
+      if [ "${_OS}" != 'win' ]; then
+        options="${options} --target=${_TRIPLET} --with-sysroot=${_SYSROOT}"
+        LDFLAGS="${LDFLAGS} -target ${_TRIPLET} --sysroot ${_SYSROOT}"
+        [ "${_OS}" = 'linux' ] && ldonly="${ldonly} -L$(find "/usr/lib/gcc/${_TRIPLET}" -name '*posix' | head -n 1)"
+      fi
+      export AR="${_CCPREFIX}ar"
+      export LD="${_CCPREFIX}ld"
+      export NM="${_CCPREFIX}nm"
+      export RANLIB="${_CCPREFIX}ranlib"
     else
-      ldonly="${ldonly} -specs=${_GCCSPECS}"
+      export CC="${_CCPREFIX}gcc -static-libgcc"
     fi
-    CPPFLAGS="${CPPFLAGS} -D_UCRT"
-    LIBS="${LIBS} -lucrt"
-  fi
 
-  if [ "${_CC}" = 'clang' ]; then
-    export CC='clang'
-    if [ "${_OS}" != 'win' ]; then
-      options="${options} --target=${_TRIPLET} --with-sysroot=${_SYSROOT}"
-      LDFLAGS="${LDFLAGS} -target ${_TRIPLET} --sysroot ${_SYSROOT}"
-      [ "${_OS}" = 'linux' ] && ldonly="${ldonly} -L$(find "/usr/lib/gcc/${_TRIPLET}" -name '*posix' | head -n 1)"
-    fi
-    export AR="${_CCPREFIX}ar"
-    export NM="${_CCPREFIX}nm"
-    export RANLIB="${_CCPREFIX}ranlib"
-  else
-    export CC="${_CCPREFIX}gcc -static-libgcc"
-  fi
+    CFLAGS="${LDFLAGS} ${CFLAGS}"
+    LDFLAGS="${LDFLAGS}${ldonly}"
+    [ "${_CPU}" = 'x86' ] && CFLAGS="${CFLAGS} -fno-asynchronous-unwind-tables"
 
-  CFLAGS="${LDFLAGS} ${CFLAGS}"
-  LDFLAGS="${LDFLAGS}${ldonly}"
-  [ "${_CPU}" = 'x86' ] && CFLAGS="${CFLAGS} -fno-asynchronous-unwind-tables"
-
-  if false; then
-    # TODO: Logic below is yet to be migrated to autotools
-
-    export LDFLAGS='-static-libgcc -Wl,--nxcompat -Wl,--dynamicbase'
-    export CURL_LDFLAG_EXTRAS_EXE
-    export CURL_LDFLAG_EXTRAS_DLL
+    LDFLAGS="${LDFLAGS} -Wl,--nxcompat -Wl,--dynamicbase"
     if [ "${_CPU}" = 'x86' ]; then
-      CURL_LDFLAG_EXTRAS_EXE='-Wl,--pic-executable,-e,_mainCRTStartup'
-      CURL_LDFLAG_EXTRAS_DLL=''
+      if [ "${pass}" = 'static' ]; then
+        LDFLAGS="${LDFLAGS} -Wl,--pic-executable,-e,_mainCRTStartup"
+      fi
     else
-      CURL_LDFLAG_EXTRAS_EXE='-Wl,--pic-executable,-e,mainCRTStartup'
-      CURL_LDFLAG_EXTRAS_DLL='-Wl,--image-base,0x150000000'
+      if [ "${pass}" = 'static' ]; then
+        LDFLAGS="${LDFLAGS} -Wl,--pic-executable,-e,mainCRTStartup"
+      else
+        LDFLAGS="${LDFLAGS} -Wl,--image-base,0x150000000"
+      fi
       LDFLAGS="${LDFLAGS} -Wl,--high-entropy-va"
     fi
 
@@ -120,183 +133,203 @@ _VER="$1"
   # LDFLAGS="${LDFLAGS} -municode"
 
     if [ "${_BRANCH#*main*}" = "${_BRANCH}" ]; then
-      CURL_LDFLAG_EXTRAS_EXE="${CURL_LDFLAG_EXTRAS_EXE} -Wl,-Map,curl.map"
-      CURL_LDFLAG_EXTRAS_DLL="${CURL_LDFLAG_EXTRAS_DLL} -Wl,-Map,libcurl.map"
+      if [ "${pass}" = 'static' ]; then
+        LDFLAGS="${LDFLAGS} -Wl,-Map,curl.map"
+      else
+        LDFLAGS="${LDFLAGS} -Wl,-Map,libcurl.map"
+      fi
     fi
 
-    CURL_LDFLAG_EXTRAS_DLL="${CURL_LDFLAG_EXTRAS_DLL} libcurl.def"
+    if [ "${pass}" = 'shared' ]; then
+      # FIXME: This breaks autotools pre-checks. Our exports will always be
+      # missing for test snippets. How to pass this to the actual linking
+      # command? We likely have to solve this differently.
+      #LDFLAGS="${LDFLAGS} libcurl.def"
+      :
+    fi
 
+    # FIXME:
     [ "${_CPU}" = 'x64' ] && export CURL_DLL_SUFFIX="-${_CPU}"
     [ "${_CPU}" = 'a64' ] && export CURL_DLL_SUFFIX="-${_CPU}"
-    export CURL_DLL_A_SUFFIX=.dll
-  fi
 
-  if [ ! "${_BRANCH#*pico*}" = "${_BRANCH}" ] || \
-     [ ! "${_BRANCH#*nano*}" = "${_BRANCH}" ]; then
-    options="${options} --disable-alt-svc"
-  else
-    options="${options} --enable-alt-svc"
-  fi
-
-  if [ ! "${_BRANCH#*pico*}" = "${_BRANCH}" ]; then
-    options="${options} --disable-crypto-auth"
-    options="${options} --disable-dict --disable-file --disable-gopher --disable-mqtt --disable-rtsp --disable-smb --disable-telnet --disable-tftp"
-    options="${options} --disable-ftp"
-    options="${options} --disable-imap --disable-pop3 --disable-smtp"
-    options="${options} --disable-ldap --disable-ldaps --with-ldap-lib=wldap32"
-  else
-    options="${options} --enable-crypto-auth"
-    options="${options} --enable-dict --enable-file --enable-gopher --enable-mqtt --enable-rtsp --enable-smb --enable-telnet --enable-tftp"
-    if [ "${_BRANCH#*noftp*}" = "${_BRANCH}" ]; then
-      options="${options} --enable-ftp"
+    if [ ! "${_BRANCH#*pico*}" = "${_BRANCH}" ] || \
+       [ ! "${_BRANCH#*nano*}" = "${_BRANCH}" ]; then
+      options="${options} --disable-alt-svc"
     else
+      options="${options} --enable-alt-svc"
+    fi
+
+    if [ ! "${_BRANCH#*pico*}" = "${_BRANCH}" ]; then
+      options="${options} --disable-crypto-auth"
+      options="${options} --disable-dict --disable-file --disable-gopher --disable-mqtt --disable-rtsp --disable-smb --disable-telnet --disable-tftp"
       options="${options} --disable-ftp"
-    fi
-    options="${options} --enable-imap --enable-pop3 --enable-smtp"
-    options="${options} --enable-ldap --enable-ldaps --with-ldap-lib=wldap32"
-  fi
-
-  # NOTE: root path with spaces breaks all values with '$(pwd)'. But,
-  #       autotools breaks on spaces anyway, so let us leave it like that.
-
-  if [ -d ../zlib ]; then
-    options="${options} --with-zlib=$(pwd)/../zlib/pkg/usr/local"
-    # These seem to work better than --with-libz-prefix=:
-    #CFLAGS="${CFLAGS} -I$(pwd)/../zlib/pkg/usr/local/include"
-    #LDFLAGS="${LDFLAGS} -L$(pwd)/../zlib/pkg/usr/local/lib"
-  else
-    options="${options} --without-zlib"
-  fi
-
-  if [ -d ../brotli ] && [ "${_BRANCH#*nobrotli*}" = "${_BRANCH}" ]; then
-    options="${options} --with-brotli=$(pwd)/../brotli/pkg/usr/local"
-    LDFLAGS="${LDFLAGS} -L$(pwd)/../brotli/pkg/usr/local/lib"
-    LIBS="${LIBS} -lbrotlicommon"
-  else
-    options="${options} --without-brotli"
-  fi
-
-  options="${options} --without-zstd"
-
-  options="${options} --with-schannel"
-  if [ -d ../libressl ]; then
-    options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../libressl/pkg/usr/local"
-    options="${options} --enable-tls-srp"
-  elif [ -d ../openssl-quic ]; then
-    options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../openssl-quic/pkg/usr/local"
-    options="${options} --enable-tls-srp"
-    # Workaround for 3.x deprecation warnings
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-    LIBS="${LIBS} -lbcrypt"
-  elif [ -d ../openssl ]; then
-    options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../openssl/pkg/usr/local"
-    options="${options} --enable-tls-srp"
-    # Workaround for 3.x deprecation warnings
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-  else
-    options="${options} --with-default-ssl-backend=schannel"
-    options="${options} --disable-tls-srp"
-  fi
-
-  options="${options} --without-gnutls --without-mbedtls --without-wolfssl --without-bearssl --without-rustls --without-nss --without-hyper"
-
-  if [ -d ../libssh2 ]; then
-    options="${options} --with-libssh2=$(pwd)/../libssh2/pkg/usr/local"
-  else
-    options="${options} --without-libssh2"
-  fi
-
-  options="${options} --without-libssh --without-wolfssh"
-  options="${options} --without-librtmp"
-
-  if [ -d ../libidn2 ]; then
-    options="${options} --with-libidn2=$(pwd)/../libidn2/pkg/usr/local"
-  elif [ "${_BRANCH#*pico*}" = "${_BRANCH}" ]; then
-    options="${options} --without-libidn2"  # Prevent autotools picking up a non-cross copy
-    options="${options} --with-winidn"
-  fi
-
-  if [ -d ../libgsasl ]; then
-    options="${options} --with-libgsasl=$(pwd)/../libgsasl/pkg/usr/local"
-    CFLAGS="${CFLAGS} -I$(pwd)/../libgsasl/pkg/usr/local/include"
-    LDFLAGS="${LDFLAGS} -L$(pwd)/../libgsasl/pkg/usr/local/lib"
-  else
-    options="${options} --without-libgsasl"
-  fi
-
-  options="${options} --without-libpsl"
-
-  if [ -d ../nghttp2 ]; then
-    options="${options} --with-nghttp2=$(pwd)/../nghttp2/pkg/usr/local"
-    CPPFLAGS="${CPPFLAGS} -DNGHTTP2_STATICLIB"
-  else
-    options="${options} --without-nghttp2"
-  fi
-  if [ "${_BRANCH#*noh3*}" = "${_BRANCH}" ]; then
-    if [ -d ../nghttp3 ]; then
-      # Detection insists on having a pkg-config, so force feed everything manually.
-      # This lib will not appear enabled in the configure summary.
-      options="${options} --with-nghttp3=yes"
-      CPPFLAGS="${CPPFLAGS} -DNGHTTP3_STATICLIB -DUSE_NGHTTP3"
-      CFLAGS="${CFLAGS} -I$(pwd)/../nghttp3/pkg/usr/local/include"
-      LDFLAGS="${LDFLAGS} -L$(pwd)/../nghttp3/pkg/usr/local/lib"
-      LIBS="${LIBS} -lnghttp3"
+      options="${options} --disable-imap --disable-pop3 --disable-smtp"
+      options="${options} --disable-ldap --disable-ldaps --with-ldap-lib=wldap32"
     else
-      options="${options} --without-nghttp3"
+      options="${options} --enable-crypto-auth"
+      options="${options} --enable-dict --enable-file --enable-gopher --enable-mqtt --enable-rtsp --enable-smb --enable-telnet --enable-tftp"
+      if [ "${_BRANCH#*noftp*}" = "${_BRANCH}" ]; then
+        options="${options} --enable-ftp"
+      else
+        options="${options} --disable-ftp"
+      fi
+      options="${options} --enable-imap --enable-pop3 --enable-smtp"
+      options="${options} --enable-ldap --enable-ldaps --with-ldap-lib=wldap32"
     fi
-    if [ -d ../ngtcp2 ]; then
-      # Detection insists on having a pkg-config, so force feed everything manually
-      # This lib will not appear enabled in the configure summary.
-      options="${options} --with-ngtcp2=yes"
-      CPPFLAGS="${CPPFLAGS} -DNGTCP2_STATICLIB -DUSE_NGTCP2"
-      CFLAGS="${CFLAGS} -I$(pwd)/../ngtcp2/pkg/usr/local/include"
-      LDFLAGS="${LDFLAGS} -L$(pwd)/../ngtcp2/pkg/usr/local/lib"
-      LIBS="${LIBS} -lngtcp2 -lngtcp2_crypto_openssl"
+
+    # NOTE: root path with spaces breaks all values with '$(pwd)'. But,
+    #       autotools breaks on spaces anyway, so let us leave it like that.
+
+    if [ -d ../zlib ]; then
+      options="${options} --with-zlib=$(pwd)/../zlib/pkg/usr/local"
     else
-      options="${options} --without-ngtcp2"
+      options="${options} --without-zlib"
     fi
-  fi
 
-  options="${options} --without-quiche --without-msh3"
+    if [ -d ../brotli ] && [ "${_BRANCH#*nobrotli*}" = "${_BRANCH}" ]; then
+      options="${options} --with-brotli=$(pwd)/../brotli/pkg/usr/local"
+      LDFLAGS="${LDFLAGS} -L$(pwd)/../brotli/pkg/usr/local/lib"
+      LIBS="${LIBS} -lbrotlicommon"
+    else
+      options="${options} --without-brotli"
+    fi
 
-  # shellcheck disable=SC2086
-  ./configure ${options} \
-    --disable-dependency-tracking \
-    --disable-silent-rules \
-    --disable-debug \
-    --disable-pthreads \
-    --enable-optimize \
-    --enable-symbol-hiding \
-    --enable-static \
-    --enable-shared \
-    --enable-headers-api \
-    --disable-ares \
-    --enable-http \
-    --enable-proxy \
-    --enable-manual \
-    --enable-libcurl-option \
-    --enable-ipv6 \
-    --disable-openssl-auto-load-config \
-    --enable-verbose \
-    --enable-sspi \
-    --enable-ntlm \
-    --enable-cookies \
-    --enable-http-auth \
-    --enable-doh \
-    --enable-mime \
-    --enable-dateparse \
-    --enable-netrc \
-    --enable-progress-meter \
-    --enable-dnsshuffle \
-    --enable-get-easy-options \
-    --enable-hsts \
-    --without-ca-path \
-    --without-ca-bundle \
-    --with-ca-fallback \
-    --prefix=/usr/local \
-    --silent
-  make --jobs 2 clean >/dev/null
-  make --jobs 2 install "DESTDIR=$(pwd)/pkg" # >/dev/null # V=1
+    options="${options} --without-zstd"
+
+    options="${options} --with-schannel"
+    if [ -d ../libressl ]; then
+      options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../libressl/pkg/usr/local"
+      options="${options} --enable-tls-srp"
+    elif [ -d ../openssl-quic ]; then
+      options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../openssl-quic/pkg/usr/local"
+      options="${options} --enable-tls-srp"
+      # Workaround for 3.x deprecation warnings
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+      LIBS="${LIBS} -lbcrypt"
+    elif [ -d ../openssl ]; then
+      options="${options} --with-default-ssl-backend=openssl --with-openssl=$(pwd)/../openssl/pkg/usr/local"
+      options="${options} --enable-tls-srp"
+      # Workaround for 3.x deprecation warnings
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+    else
+      options="${options} --with-default-ssl-backend=schannel"
+      options="${options} --disable-tls-srp"
+    fi
+
+    options="${options} --without-gnutls --without-mbedtls --without-wolfssl --without-bearssl --without-rustls --without-nss --without-hyper"
+
+    if [ -d ../libssh2 ]; then
+      options="${options} --with-libssh2=$(pwd)/../libssh2/pkg/usr/local"
+    else
+      options="${options} --without-libssh2"
+    fi
+
+    options="${options} --without-libssh --without-wolfssh"
+    options="${options} --without-librtmp"
+
+    if [ -d ../libidn2 ]; then
+      options="${options} --with-libidn2=$(pwd)/../libidn2/pkg/usr/local"
+    elif [ "${_BRANCH#*pico*}" = "${_BRANCH}" ]; then
+      options="${options} --without-libidn2"  # Prevent autotools picking up a non-cross copy
+      options="${options} --with-winidn"
+    fi
+
+    if [ -d ../libgsasl ]; then
+      options="${options} --with-libgsasl=$(pwd)/../libgsasl/pkg/usr/local"
+      CFLAGS="${CFLAGS} -I$(pwd)/../libgsasl/pkg/usr/local/include"
+      LDFLAGS="${LDFLAGS} -L$(pwd)/../libgsasl/pkg/usr/local/lib"
+    else
+      options="${options} --without-libgsasl"
+    fi
+
+    options="${options} --without-libpsl"
+
+    if [ -d ../nghttp2 ]; then
+      options="${options} --with-nghttp2=$(pwd)/../nghttp2/pkg/usr/local"
+      CPPFLAGS="${CPPFLAGS} -DNGHTTP2_STATICLIB"
+    else
+      options="${options} --without-nghttp2"
+    fi
+    if [ "${_BRANCH#*noh3*}" = "${_BRANCH}" ]; then
+      if [ -d ../nghttp3 ]; then
+        # Detection insists on having a pkg-config, so force feed everything manually.
+        # This lib will not appear enabled in the configure summary.
+        options="${options} --with-nghttp3=yes"
+        CPPFLAGS="${CPPFLAGS} -DNGHTTP3_STATICLIB -DUSE_NGHTTP3"
+        CFLAGS="${CFLAGS} -I$(pwd)/../nghttp3/pkg/usr/local/include"
+        LDFLAGS="${LDFLAGS} -L$(pwd)/../nghttp3/pkg/usr/local/lib"
+        LIBS="${LIBS} -lnghttp3"
+      else
+        options="${options} --without-nghttp3"
+      fi
+      if [ -d ../ngtcp2 ]; then
+        # Detection insists on having a pkg-config, so force feed everything manually.
+        # This lib will not appear enabled in the configure summary.
+        options="${options} --with-ngtcp2=yes"
+        CPPFLAGS="${CPPFLAGS} -DNGTCP2_STATICLIB -DUSE_NGTCP2"
+        CFLAGS="${CFLAGS} -I$(pwd)/../ngtcp2/pkg/usr/local/include"
+        LDFLAGS="${LDFLAGS} -L$(pwd)/../ngtcp2/pkg/usr/local/lib"
+        LIBS="${LIBS} -lngtcp2 -lngtcp2_crypto_openssl"
+      else
+        options="${options} --without-ngtcp2"
+      fi
+    fi
+
+    options="${options} --without-quiche --without-msh3"
+
+    if [ "${pass}" = 'static' ]; then
+      options="${options} --enable-static"
+      options="${options} --disable-shared"
+    else
+      options="${options} --disable-static"
+      options="${options} --enable-shared"
+
+      # Ugly hack to make libtool find what is wants when doing checks
+      # before attempting to link a DLL. We put implibs here with all the
+      # names it needs. This complements the .lib names symlinked to .a files.
+      LDFLAGS="${LDFLAGS} -L$(pwd)/.."
+
+      # FIXME: Not sure why this was necessary to find Windows implibs.
+      LDFLAGS="${LDFLAGS} -L${_SYSROOT}/${_TRIPLET}/lib"
+    fi
+
+    # shellcheck disable=SC2086
+    ./configure ${options} \
+      --disable-dependency-tracking \
+      --disable-silent-rules \
+      --disable-debug \
+      --disable-pthreads \
+      --enable-optimize \
+      --enable-symbol-hiding \
+      --enable-headers-api \
+      --disable-ares \
+      --enable-http \
+      --enable-proxy \
+      --enable-manual \
+      --enable-libcurl-option \
+      --enable-ipv6 \
+      --disable-openssl-auto-load-config \
+      --enable-verbose \
+      --enable-sspi \
+      --enable-ntlm \
+      --enable-cookies \
+      --enable-http-auth \
+      --enable-doh \
+      --enable-mime \
+      --enable-dateparse \
+      --enable-netrc \
+      --enable-progress-meter \
+      --enable-dnsshuffle \
+      --enable-get-easy-options \
+      --enable-hsts \
+      --without-ca-path \
+      --without-ca-bundle \
+      --with-ca-fallback \
+      --prefix=/usr/local \
+      --silent
+    make --jobs 2 clean >/dev/null
+    make --jobs 2 install "DESTDIR=$(pwd)/pkg" # >/dev/null # V=1
+  done
 
   # DESTDIR= + --prefix=
   _pkg='pkg/usr/local'
