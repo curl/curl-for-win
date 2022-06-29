4f3ca6740491ebe9500a697a4a24688122eb0ab6
Viktor Szakats <commit@vsz.me>
2022-06-29 10:00:25 +0000

    _pkg.sh: reduce VirusTotal timeout [ci skip]
    VirusTotal uploads are consistently timeouting since 14(+) days ago.
    Reduce timeouts to 80 seconds, saving 160 seconds of idle wait per build.
    
    VirusTotal uploads work as expected from a local connection, so I'm not
    sure what is going on. If the failures continue, I plan to drop
    VirusTotal uploads.

--- _pkg.sh
+++ _pkg.sh
@@ -104,7 +104,7 @@ create_pkg() {
       | sed -n -E 's,.+= ([0-9a-fA-F]{64}),\1,p')"
     # https://developers.virustotal.com/v3.0/reference
     out="$(curl --disable --user-agent '' --fail --silent --show-error \
-      --connect-timeout 15 --max-time 60 --retry 3 \
+      --connect-timeout 10 --max-time 60 --retry 1 \
       --request POST 'https://www.virustotal.com/api/v3/files' \
       --header @/dev/stdin \
       --form "file=@${_pkg}" <<EOF || true
@@ -114,7 +114,7 @@ EOF
     if [ -n "${out}" ]; then
       id="$(echo "${out}" | jq --raw-output '.data.id')"
       out="$(curl --disable --user-agent '' --fail --silent --show-error \
-        --connect-timeout 15 --max-time 20 --retry 3 \
+        --connect-timeout 10 --max-time 20 --retry 1 \
         "https://www.virustotal.com/api/v3/analyses/${id}" \
         --header @/dev/stdin <<EOF || true
 x-apikey: ${VIRUSTOTAL_APIKEY}
