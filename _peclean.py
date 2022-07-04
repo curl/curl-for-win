538d6acc69b8a0bf0efccd6e2e9b3812bea77acb
Viktor Szakats <commit@vsz.me>
2022-07-04 23:45:05 +0000

    _peclean.py: rename a variable [ci skip]

--- _peclean.py
+++ _peclean.py
@@ -13,16 +13,16 @@ import sys
 import pefile
 
 if len(sys.argv) > 2:
-    FTIME = calendar.timegm(
+    ts = calendar.timegm(
         datetime.datetime.fromtimestamp(
             os.path.getmtime(os.path.normpath(sys.argv[1]))).timetuple())
     for argv in sys.argv[2:]:
         for fname in glob.glob(argv):
-            print(datetime.datetime.fromtimestamp(FTIME).isoformat() + ' -> ' + fname)
+            print(datetime.datetime.fromtimestamp(ts).isoformat() + ' -> ' + fname)
             pe = pefile.PE(fname)
-            pe.FILE_HEADER.TimeDateStamp = FTIME
+            pe.FILE_HEADER.TimeDateStamp = ts
             try:
-                pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = FTIME
+                pe.DIRECTORY_ENTRY_EXPORT.struct.TimeDateStamp = ts
             except AttributeError:
                 pass
             pe.OPTIONAL_HEADER.CheckSum = pe.generate_checksum()
