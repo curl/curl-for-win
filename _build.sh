curl/curl-for-win

commit c01eea11ab5e20837be9cb7cfedeb5f56e1b47c0

pthread workaround: limit to ucrt, add self-contained example [ci skip]
Ref: https://github.com/niXman/mingw-builds/issues/498#issuecomment-1186065810

_build.sh
@@ -69,7 +69,7 @@ set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o p
 #   - https://github.com/mstorsjo/llvm-mingw
 #   - https://github.com/llvm/llvm-project
 #   - https://salsa.debian.org/pkg-llvm-team
-#   - https://git.code.sf.net/p/mingw-w64/mingw-w64
+#   - https://git.code.sf.net/p/mingw-w64/mingw-w64 / https://github.com/mirror/mingw-w64
 #   - https://sourceware.org/git/binutils-gdb.git
 #   - https://github.com/netwide-assembler/nasm
 
@@ -289,7 +289,7 @@ build_single_target() {
     use_llvm_mingw=1
   # llvm-mingw is required for x64 (to avoid pthread link bug with BoringSSL),
   # but for consistency, use it for all targets when building with BoringSSL.
-  elif [ "${_BRANCH#*boringssl*}" != "${_BRANCH}" ]; then
+  elif [ "${_BRANCH#*boringssl*}" != "${_BRANCH}" ] && [ "${_CRT}" = 'ucrt' ]; then
     use_llvm_mingw=1
   elif [ "${_CPU}" = 'a64' ]; then
     use_llvm_mingw=1

curl-autotools.sh
@@ -169,7 +169,7 @@ fi
       CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
       options="${options} --with-openssl=${_TOP}/boringssl/${_PP}"
       options="${options} --disable-tls-srp"
-      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ]; then  # FIXME
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
         LDFLAGS="${LDFLAGS} -Wl,-Bdynamic,-lpthread,-Bstatic"
       else
         LDFLAGS="${LDFLAGS} -Wl,-Bstatic,-lpthread,-Bdynamic"

curl-cmake.sh
@@ -156,7 +156,7 @@ _VER="$1"
       options="${options} -DCURL_USE_OPENSSL=ON"
       options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/boringssl/${_PP}"
       options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/boringssl/${_PP}/include"
-      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ]; then  # FIXME
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
         LIBS="${LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
       else
         LIBS="${LIBS} -Wl,-Bstatic -lpthread -Wl,-Bdynamic"

curl-m32.sh
@@ -134,15 +134,40 @@ _VER="$1"
   elif [ -d ../boringssl ]; then
     CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
     export OPENSSL_PATH="../../boringssl/${_PP}"
-    if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ]; then  # FIXME
-      # Non-production workaround:
+    if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+      # Non-production workaround for:
+      # mingw-w64 x64 winpthread static lib incompatible with UCRT.
+      # ```c
+      # /*
+      #    clang
+      #    $ /usr/local/opt/llvm/bin/clang -fuse-ld=lld \
+      #        -target x86_64-w64-mingw32 --sysroot /usr/local/opt/mingw-w64/toolchain-x86_64 \
+      #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
+      #
+      #    gcc
+      #    $ x86_64-w64-mingw32-gcc -dumpspecs | sed 's/-lmsvcrt/-lucrt/g' > gcc-specs-ucrt
+      #    $ x86_64-w64-mingw32-gcc -specs=gcc-specs-ucrt \
+      #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
+      #
+      #    ``` clang ->
+      #    ld.lld: error: undefined symbol: _setjmp
+      #    >>> referenced by ../src/thread.c:1518
+      #    >>>               libpthread.a(libwinpthread_la-thread.o):(pthread_create_wrapper)
+      #    clang-14: error: linker command failed with exit code 1 (use -v to see invocation)
+      #    ```
+      #    ``` gcc ->
+      #    /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/bin/x86_64-w64-mingw32-ld: /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/lib/gcc/x86_64-w64-mingw32/12.1.0/../../../../x86_64-w64-mingw32/lib/../lib/libpthread.a(libwinpthread_la-thread.o): in function `pthread_create_wrapper':
+      #    /private/tmp/mingw-w64-20220506-7546-1q81cpu/mingw-w64-v10.0.0/mingw-w64-libraries/winpthreads/build-x86_64/../src/thread.c:1518: undefined reference to `_setjmp'
+      #    collect2: error: ld returned 1 exit status
+      #    ```
+      #  */
+      # #include <pthread.h>
+      # int main(void) {
+      #   pthread_rwlock_t lock;
+      #   pthread_rwlock_init(&lock, NULL);
+      #   return 0;
+      # }
       # ```
-      # ld.lld: error: undefined symbol: _setjmp
-      # >>> referenced by ../src/thread.c:1518
-      # >>>               libwinpthread.a(libwinpthread_la-thread.o):(pthread_create_wrapper)
-      # clang-14: error: linker command failed with exit code 1 (use -v to see invocation)
-      # ```
-      # This seems to be an issue with winpthread static lib being incompatible with UCRT.
       # Ref: https://github.com/niXman/mingw-builds/issues/498
       OPENSSL_LIBS="${OPENSSL_LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
     else

commit fce656d3e044f5c4f3b609851c887d27f583a9d1

_build.sh: export _CRT, now used in curl-*.sh [ci skip]
Regression since c01eea11ab5e20837be9cb7cfedeb5f56e1b47c0

_build.sh
@@ -152,7 +152,7 @@ fi
 export _CC='clang'
 [ ! "${_BRANCH#*gcc*}" = "${_BRANCH}" ] && _CC='gcc'
 
-_CRT='ucrt'
+export _CRT='ucrt'
 [ ! "${_BRANCH#*msvcrt*}" = "${_BRANCH}" ] && _CRT='msvcrt'
 
 if [ -z "${CW_MAP:-}" ]; then