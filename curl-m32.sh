curl/curl-for-win

commit 51f56255e19dc8151985e62d6262a975ffc96842

openssl: merge common codepaths [ci skip]

curl-m32.sh
@@ -128,62 +128,59 @@ _VER="$1"
     export ZSTD_LIBS='-Wl,-Bstatic -lzstd -Wl,-Bdynamic'
   fi
 
-  export OPENSSL_LIBS=''
-  if [ "${_OPENSSL}" = 'libressl' ]; then
-    export OPENSSL_PATH="../../${_OPENSSL}/${_PP}"
-  elif [ "${_OPENSSL}" = 'boringssl' ]; then
-    CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
-    export OPENSSL_PATH="../../${_OPENSSL}/${_PP}"
-    if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
-      # Non-production workaround for:
-      # mingw-w64 x64 winpthread static lib incompatible with UCRT.
-      # ```c
-      # /*
-      #    clang
-      #    $ /usr/local/opt/llvm/bin/clang -fuse-ld=lld \
-      #        -target x86_64-w64-mingw32 --sysroot /usr/local/opt/mingw-w64/toolchain-x86_64 \
-      #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
-      #
-      #    gcc
-      #    $ x86_64-w64-mingw32-gcc -dumpspecs | sed 's/-lmsvcrt/-lucrt/g' > gcc-specs-ucrt
-      #    $ x86_64-w64-mingw32-gcc -specs=gcc-specs-ucrt \
-      #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
-      #
-      #    ``` clang ->
-      #    ld.lld: error: undefined symbol: _setjmp
-      #    >>> referenced by ../src/thread.c:1518
-      #    >>>               libpthread.a(libwinpthread_la-thread.o):(pthread_create_wrapper)
-      #    clang-14: error: linker command failed with exit code 1 (use -v to see invocation)
-      #    ```
-      #    ``` gcc ->
-      #    /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/bin/x86_64-w64-mingw32-ld: /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/lib/gcc/x86_64-w64-mingw32/12.1.0/../../../../x86_64-w64-mingw32/lib/../lib/libpthread.a(libwinpthread_la-thread.o): in function `pthread_create_wrapper':
-      #    /private/tmp/mingw-w64-20220506-7546-1q81cpu/mingw-w64-v10.0.0/mingw-w64-libraries/winpthreads/build-x86_64/../src/thread.c:1518: undefined reference to `_setjmp'
-      #    collect2: error: ld returned 1 exit status
-      #    ```
-      #  */
-      # #include <pthread.h>
-      # int main(void) {
-      #   pthread_rwlock_t lock;
-      #   pthread_rwlock_init(&lock, NULL);
-      #   return 0;
-      # }
-      # ```
-      # Ref: https://github.com/niXman/mingw-builds/issues/498
-      OPENSSL_LIBS="${OPENSSL_LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
-    else
-      OPENSSL_LIBS="${OPENSSL_LIBS} -Wl,-Bstatic -lpthread -Wl,-Bdynamic"
-    fi
-  elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-    export OPENSSL_PATH="../../${_OPENSSL}/${_PP}"
-    # Workaround for 3.x deprecation warnings
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-  fi
   if [ -n "${OPENSSL_PATH:-}" ]; then
     options="${options}-ssl"
+    export OPENSSL_PATH="../../${_OPENSSL}/${_PP}"
     export OPENSSL_INCLUDE="${OPENSSL_PATH}/include"
     export OPENSSL_LIBPATH="${OPENSSL_PATH}/lib"
-    OPENSSL_LIBS="${OPENSSL_LIBS} -lssl -lcrypto -lbcrypt"
+    export OPENSSL_LIBS='-lssl -lcrypto -lbcrypt'
     CPPFLAGS="${CPPFLAGS} -DCURL_DISABLE_OPENSSL_AUTO_LOAD_CONFIG"
+
+    if [ "${_OPENSSL}" = 'boringssl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+        # Non-production workaround for:
+        # mingw-w64 x64 winpthread static lib incompatible with UCRT.
+        # ```c
+        # /*
+        #    clang
+        #    $ /usr/local/opt/llvm/bin/clang -fuse-ld=lld \
+        #        -target x86_64-w64-mingw32 --sysroot /usr/local/opt/mingw-w64/toolchain-x86_64 \
+        #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
+        #
+        #    gcc
+        #    $ x86_64-w64-mingw32-gcc -dumpspecs | sed 's/-lmsvcrt/-lucrt/g' > gcc-specs-ucrt
+        #    $ x86_64-w64-mingw32-gcc -specs=gcc-specs-ucrt \
+        #        test.c -D_UCRT -Wl,-Bstatic -lpthread -Wl,-Bdynamic -lucrt
+        #
+        #    ``` clang ->
+        #    ld.lld: error: undefined symbol: _setjmp
+        #    >>> referenced by ../src/thread.c:1518
+        #    >>>               libpthread.a(libwinpthread_la-thread.o):(pthread_create_wrapper)
+        #    clang-14: error: linker command failed with exit code 1 (use -v to see invocation)
+        #    ```
+        #    ``` gcc ->
+        #    /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/bin/x86_64-w64-mingw32-ld: /usr/local/Cellar/mingw-w64/10.0.0_1/toolchain-x86_64/lib/gcc/x86_64-w64-mingw32/12.1.0/../../../../x86_64-w64-mingw32/lib/../lib/libpthread.a(libwinpthread_la-thread.o): in function `pthread_create_wrapper':
+        #    /private/tmp/mingw-w64-20220506-7546-1q81cpu/mingw-w64-v10.0.0/mingw-w64-libraries/winpthreads/build-x86_64/../src/thread.c:1518: undefined reference to `_setjmp'
+        #    collect2: error: ld returned 1 exit status
+        #    ```
+        #  */
+        # #include <pthread.h>
+        # int main(void) {
+        #   pthread_rwlock_t lock;
+        #   pthread_rwlock_init(&lock, NULL);
+        #   return 0;
+        # }
+        # ```
+        # Ref: https://github.com/niXman/mingw-builds/issues/498
+        OPENSSL_LIBS="${OPENSSL_LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
+      else
+        OPENSSL_LIBS="${OPENSSL_LIBS} -Wl,-Bstatic -lpthread -Wl,-Bdynamic"
+      fi
+    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+      # Workaround for 3.x deprecation warnings
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+    fi
   fi
 
   if [ -d ../mbedtls ]; then

curl-autotools.sh
@@ -161,29 +161,26 @@ fi
     options="${options} --with-schannel"
     CPPFLAGS="${CPPFLAGS} -DHAS_ALPN"
 
-    if [ "${_OPENSSL}" = 'libressl' ]; then
+    if [ -n "${_OPENSSL}" ]; then
       options="${options} --with-openssl=${_TOP}/${_OPENSSL}/${_PP}"
-      options="${options} --enable-tls-srp"
-      LIBS="${LIBS} -lbcrypt"
-    elif [ "${_OPENSSL}" = 'boringssl' ]; then
-      CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
-      options="${options} --with-openssl=${_TOP}/${_OPENSSL}/${_PP}"
-      options="${options} --disable-tls-srp"
-      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
-        LDFLAGS="${LDFLAGS} -Wl,-Bdynamic,-lpthread,-Bstatic"
-      else
-        LDFLAGS="${LDFLAGS} -Wl,-Bstatic,-lpthread,-Bdynamic"
+      options="${options} --disable-openssl-auto-load-config"
+      if [ "${_OPENSSL}" = 'libressl' ]; then
+        options="${options} --enable-tls-srp"
+        LIBS="${LIBS} -lbcrypt"
+      elif [ "${_OPENSSL}" = 'boringssl' ]; then
+        CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
+        options="${options} --disable-tls-srp"
+        if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+          LDFLAGS="${LDFLAGS} -Wl,-Bdynamic,-lpthread,-Bstatic"
+        else
+          LDFLAGS="${LDFLAGS} -Wl,-Bstatic,-lpthread,-Bdynamic"
+        fi
+      elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+        options="${options} --enable-tls-srp"
+        LIBS="${LIBS} -lbcrypt"
       fi
-    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-      options="${options} --with-openssl=${_TOP}/${_OPENSSL}/${_PP}"
-      options="${options} --enable-tls-srp"
-      LIBS="${LIBS} -lbcrypt"
     else
       options="${options} --disable-tls-srp"
-    fi
-
-    if [ -n "${_OPENSSL}" ]; then
-      options="${options} --disable-openssl-auto-load-config"
     fi
 
     if [ -d ../mbedtls ]; then

curl-cmake.sh
@@ -144,33 +144,27 @@ _VER="$1"
       options="${options} -DCURL_ZSTD=OFF"
     fi
 
-    if [ "${_OPENSSL}" = 'libressl' ]; then
+    if [ -n "${_OPENSSL}" ]; then
       options="${options} -DCURL_USE_OPENSSL=ON"
       options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
       options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-      CPPFLAGS="${CPPFLAGS} -DHAVE_OPENSSL_SRP -DUSE_TLS_SRP"
-      LIBS="${LIBS} -lbcrypt"
-    elif [ "${_OPENSSL}" = 'boringssl' ]; then
-      CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
-      options="${options} -DCURL_USE_OPENSSL=ON"
-      options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-      options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
-        LIBS="${LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
-      else
-        LIBS="${LIBS} -Wl,-Bstatic -lpthread -Wl,-Bdynamic"
+      options="${options} -DCURL_DISABLE_OPENSSL_AUTO_LOAD_CONFIG=ON"
+      if [ "${_OPENSSL}" = 'libressl' ]; then
+        CPPFLAGS="${CPPFLAGS} -DHAVE_OPENSSL_SRP -DUSE_TLS_SRP"
+        LIBS="${LIBS} -lbcrypt"
+      elif [ "${_OPENSSL}" = 'boringssl' ]; then
+        CPPFLAGS="${CPPFLAGS} -DCURL_BORINGSSL_VERSION=\\\"$(printf '%.8s' "${BORINGSSL_VER_}")\\\""
+        if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+          LIBS="${LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
+        else
+          LIBS="${LIBS} -Wl,-Bstatic -lpthread -Wl,-Bdynamic"
+        fi
+      elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+        CPPFLAGS="${CPPFLAGS} -DHAVE_OPENSSL_SRP -DUSE_TLS_SRP"
+        LIBS="${LIBS} -lbcrypt"
       fi
-    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-      options="${options} -DCURL_USE_OPENSSL=ON"
-      options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-      options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-      CPPFLAGS="${CPPFLAGS} -DHAVE_OPENSSL_SRP -DUSE_TLS_SRP"
-      LIBS="${LIBS} -lbcrypt"
     else
       options="${options} -DCURL_USE_OPENSSL=OFF"
-    fi
-    if [ -n "${_OPENSSL}" ]; then
-      options="${options} -DCURL_DISABLE_OPENSSL_AUTO_LOAD_CONFIG=ON"
     fi
 
     if [ -d ../mbedtls ]; then

libssh.sh
@@ -28,34 +28,32 @@ _VER="$1"
     options="${options} -DZLIB_INCLUDE_DIR=${_TOP}/zlib/${_PP}/include"
   fi
 
-  if [ "${_OPENSSL}" = 'libressl' ]; then
+  if [ -n "${_OPENSSL}" ]; then
     options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
     options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    LIBS="${LIBS} -lbcrypt"
-    LIBS="${LIBS} -lws2_32"  # to detect EVP_aes_128_*
-  elif [ "${_OPENSSL}" = 'boringssl' ]; then
+    if [ "${_OPENSSL}" = 'libressl' ]; then
+      LIBS="${LIBS} -lbcrypt"
+      LIBS="${LIBS} -lws2_32"  # to detect EVP_aes_128_*
+    elif [ "${_OPENSSL}" = 'boringssl' ]; then
 
-    # FIXME (upstream):
-    # - It collides with wincrypt.h macros. Workaround:
-    CPPFLAGS="${CPPFLAGS} -DNOCRYPT -D__WINCRYPT_H__"
-    # - Wants to compile libcrypto_compat.c and assumes pre-OpenSSL 1.1
-    #   non-opaque structures. Workaround:
-    echo > src/libcrypto-compat.c
-    # - Detects HAVE_OPENSSL_CRYPTO_CTR128_ENCRYPT, and assumes it means
-    #   openssl/modes.h also exists, but with BoringSSL, it does not. Workaround:
-    [ -d include/openssl ] || mkdir -p include/openssl
-    touch include/openssl/modes.h
+      # FIXME (upstream):
+      # - It collides with wincrypt.h macros. Workaround:
+      CPPFLAGS="${CPPFLAGS} -DNOCRYPT -D__WINCRYPT_H__"
+      # - Wants to compile libcrypto_compat.c and assumes pre-OpenSSL 1.1
+      #   non-opaque structures. Workaround:
+      echo > src/libcrypto-compat.c
+      # - Detects HAVE_OPENSSL_CRYPTO_CTR128_ENCRYPT, and assumes it means
+      #   openssl/modes.h also exists, but with BoringSSL, it does not. Workaround:
+      [ -d include/openssl ] || mkdir -p include/openssl
+      touch include/openssl/modes.h
 
-    options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-    options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    CPPFLAGS="${CPPFLAGS} -DWIN32_LEAN_AND_MEAN"
-    LIBS="${LIBS} -lpthread"  # to detect EVP_aes_128_*
-  elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-    options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-    options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-    LIBS="${LIBS} -lbcrypt"
-    LIBS="${LIBS} -lws2_32"  # to detect EVP_aes_128_*
+      CPPFLAGS="${CPPFLAGS} -DWIN32_LEAN_AND_MEAN"
+      LIBS="${LIBS} -lpthread"  # to detect EVP_aes_128_*
+    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+      LIBS="${LIBS} -lbcrypt"
+      LIBS="${LIBS} -lws2_32"  # to detect EVP_aes_128_*
+    fi
   elif [ -d ../mbedtls ]; then
     if false; then
       # Compile errors as of mbedTLS 3.2.1 + libssh 0.9.6

libssh2-cmake.sh
@@ -25,25 +25,21 @@ _VER="$1"
     options="${options} -DZLIB_INCLUDE_DIR=${_TOP}/zlib/${_PP}/include"
   fi
 
-  if [ "${_OPENSSL}" = 'libressl' ]; then
+  if [ -n "${_OPENSSL}"  ]; then
     options="${options} -DCRYPTO_BACKEND=OpenSSL"
     options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
     options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    CPPFLAGS="${CPPFLAGS} -DNOCRYPT"
-    LIBS="${LIBS} -lbcrypt"
-    LIBS="${LIBS} -lws2_32"  # to detect HAVE_EVP_AES_128_CTR
-  elif [ "${_OPENSSL}" = 'boringssl' ]; then
-    options="${options} -DCRYPTO_BACKEND=OpenSSL"
-    options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-    options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    LIBS="${LIBS} -lpthread"  # to detect HAVE_EVP_AES_128_CTR
-  elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-    options="${options} -DCRYPTO_BACKEND=OpenSSL"
-    options="${options} -DOPENSSL_ROOT_DIR=${_TOP}/${_OPENSSL}/${_PP}"
-    options="${options} -DOPENSSL_INCLUDE_DIR=${_TOP}/${_OPENSSL}/${_PP}/include"
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-    LIBS="${LIBS} -lbcrypt"
-    LIBS="${LIBS} -lws2_32"  # to detect HAVE_EVP_AES_128_CTR
+    if [ "${_OPENSSL}" = 'libressl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DNOCRYPT"
+      LIBS="${LIBS} -lbcrypt"
+      LIBS="${LIBS} -lws2_32"  # to detect HAVE_EVP_AES_128_CTR
+    elif [ "${_OPENSSL}" = 'boringssl' ]; then
+      LIBS="${LIBS} -lpthread"  # to detect HAVE_EVP_AES_128_CTR
+    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+      LIBS="${LIBS} -lbcrypt"
+      LIBS="${LIBS} -lws2_32"  # to detect HAVE_EVP_AES_128_CTR
+    fi
   elif [ -d ../mbedtls ]; then
     if false; then
       # Compile errors as of mbedTLS 3.2.1 + libssh 1.10.0

libssh2.sh
@@ -36,17 +36,17 @@ _VER="$1"
     LDFLAGS="${LDFLAGS} -L${_TOP}/zlib/${_PP}/lib"
   fi
 
-  if [ "${_OPENSSL}" = 'libressl' ]; then
+  if [ -n "${_OPENSSL}" ]; then
     options="${options} --with-crypto=openssl --with-libssl-prefix=${_TOP}/${_OPENSSL}/${_PP}"
-    CPPFLAGS="${CPPFLAGS} -DNOCRYPT"
-    LIBS="${LIBS} -lbcrypt"
-  elif [ "${_OPENSSL}" = 'boringssl' ]; then
-    options="${options} --with-crypto=openssl --with-libssl-prefix=${_TOP}/${_OPENSSL}/${_PP}"
-    LIBS="${LIBS} -lpthread"
-  elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
-    options="${options} --with-crypto=openssl --with-libssl-prefix=${_TOP}/${_OPENSSL}/${_PP}"
-    CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
-    LIBS="${LIBS} -lbcrypt"
+    if [ "${_OPENSSL}" = 'libressl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DNOCRYPT"
+      LIBS="${LIBS} -lbcrypt"
+    elif [ "${_OPENSSL}" = 'boringssl' ]; then
+      LIBS="${LIBS} -lpthread"
+    elif [ "${_OPENSSL}" = 'openssl-quic' ] || [ "${_OPENSSL}" = 'openssl' ]; then
+      CPPFLAGS="${CPPFLAGS} -DOPENSSL_SUPPRESS_DEPRECATED"
+      LIBS="${LIBS} -lbcrypt"
+    fi
   elif [ -d ../mbedtls ]; then
     if false; then
       # Compile errors as of mbedTLS 3.2.1 + libssh 1.10.0

commit 8951c63f5e9f27073d5d141985ff119c7e6c6490

curl-m32.sh: fix typo [ci skip]
Regression since 51f56255e19dc8151985e62d6262a975ffc96842

curl-m32.sh
@@ -128,7 +128,7 @@ _VER="$1"
     export ZSTD_LIBS='-Wl,-Bstatic -lzstd -Wl,-Bdynamic'
   fi
 
-  if [ -n "${OPENSSL_PATH:-}" ]; then
+  if [ -n "${_OPENSSL}" ]; then
     options="${options}-ssl"
     export OPENSSL_PATH="../../${_OPENSSL}/${_PP}"
     export OPENSSL_INCLUDE="${OPENSSL_PATH}/include"