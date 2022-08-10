6d905b1dced88846af1dabad72493489915dcd09
Viktor Szakats <commit@vsz.me>
2022-08-10 16:53:56 +0000

    _build.sh: wolfssl uses autotools [ci skip]

--- _build.sh
+++ _build.sh
@@ -95,7 +95,7 @@ set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o p
 #   nghttp2       cmake
 #   nghttp3       cmake
 #   ngtcp2        cmake
-#   wolfssl       cmake
+#   wolfssl       autotools
 #   mbedtls       cmake
 #   openssl       proprietary
 #   boringssl     cmake
