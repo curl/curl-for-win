curl/curl-for-win

commit db02347fd2a01e1ce81b2cbaf84da4a491e7ffef

bumper.sh: add support for tagged releases [ci skip]
such as openssl-quic, nghttp3, ngtcp2.

Also:
- rename openssl_quic to openssl-quic internally
- disable xtrace for the uni package hashing loop to avoid log noise

_dl.sh
@@ -47,17 +47,20 @@ cat <<EOF
   {
     "name": "nghttp3",
     "url": "https://github.com/ngtcp2/nghttp3/releases/download/v{ver}/nghttp3-{ver}.tar.xz",
-    "redir": "redir"
+    "redir": "redir",
+    "tag": ".+"
   },
   {
     "name": "ngtcp2",
     "url": "https://github.com/ngtcp2/ngtcp2/releases/download/v{ver}/ngtcp2-{ver}.tar.xz",
-    "redir": "redir"
+    "redir": "redir",
+    "tag": ".+"
   },
   {
-    "name": "openssl_quic",
+    "name": "openssl-quic",
     "url": "https://github.com/quictls/openssl/archive/refs/heads/openssl-{ver}+quic.tar.gz",
-    "redir": "redir"
+    "redir": "redir",
+    "tag": "openssl-\\\\d+\\\\.\\\\d+\\\\.\\\\d+\\\\+quic$"
   },
   {
     "name": "openssl",
@@ -147,11 +150,19 @@ check_update() {
   newver=''
   if [[ "${url}" =~ ^https://github.com/([a-zA-Z0-9-]+/[a-zA-Z0-9-]+)/ ]]; then
     slug="${BASH_REMATCH[1]}"
-    # heavily rate-limited
-    newver="$(my_curl --user-agent ' ' "https://api.github.com/repos/${slug}/releases/latest" | \
-      jq --raw-output '.tag_name' | sed 's|^v||')"
-    if [[ "${newver}" =~ ^[0-9]+\.[0-9]+$ ]]; then
-      newver="${newver}.0"
+    if [ -n "$5" ]; then
+      newver="$(curl --disable --user-agent ' ' --silent --fail --show-error \
+          "https://api.github.com/repos/${slug}/git/refs/heads" \
+        | jq --raw-output '.[].ref' \
+        | grep -a -E "$5" \
+        | grep -a -E -o '\d+\.\d+\.\d' | sort | tail -1)"
+    else
+      # heavily rate-limited
+      newver="$(my_curl --user-agent ' ' "https://api.github.com/repos/${slug}/releases/latest" | \
+        jq --raw-output '.tag_name' | sed 's|^v||')"
+      if [[ "${newver}" =~ ^[0-9]+\.[0-9]+$ ]]; then
+        newver="${newver}.0"
+      fi
     fi
   else
     mask="${pkg}[._-]v?([0-9]+(\.[0-9]+)+)\.t"
@@ -241,11 +252,13 @@ bump() {
 
   while read -r pkg; do
     if [[ "${pkg}" =~ ^([A-Z0-9_]+)_VER_=(.+)$ ]]; then
-      name="${BASH_REMATCH[1],,}"
+      nameenv="${BASH_REMATCH[1]}"
+      name="${nameenv,,}"
+      name="${name//_/-}"
       ourver="${BASH_REMATCH[2]}"
       ourvern="$(printf '%s' "${ourver}" | to8digit)"
 
-      hashenv="${name^^}_HASH"
+      hashenv="${nameenv}_HASH"
       eval hash="\${${hashenv}:-}"
 
       jp="$(dependencies_json | jq \
@@ -257,11 +270,12 @@ bump() {
         url="$( printf '%s' "${jp}" | jq --raw-output '.url')"
         desc="$(printf '%s' "${jp}" | jq --raw-output '.descending')"
         pin="$( printf '%s' "${jp}" | jq --raw-output '.pinned')"
+        tag="$( printf '%s' "${jp}" | jq --raw-output '.tag' | sed 's|null||')"
 
         if [ "${pin}" = 'true' ]; then
           >&2 echo "! ${name}: Version pinned. Skipping."
         else
-          newver="$(check_update "${name}" "${ourvern}" "${url}" "${desc}")"
+          newver="$(check_update "${name}" "${ourvern}" "${url}" "${desc}" "${tag}")"
           if [ -n "${newver}" ]; then
             >&2 echo "! ${name}: New version found: |${newver}|"
 
@@ -300,7 +314,7 @@ bump() {
         newhash="${hash}"
       fi
 
-      echo "export ${name^^}_VER_='${newver}'"
+      echo "export ${nameenv^^}_VER_='${newver}'"
       [ -n "${hash}" ] && echo "export ${hashenv}=${newhash}"
     fi
   done <<< "$(env | grep -a -E '^[A-Z0-9_]+_VER_' | \
@@ -447,8 +461,8 @@ if [ "${_BRANCH#*nano*}" = "${_BRANCH}" ] && \
    [ "${_BRANCH#*mini*}" = "${_BRANCH}" ] && \
    [ "${_BRANCH#*schannel*}" = "${_BRANCH}" ]; then
   if [ "${_BRANCH#*quic*}" != "${_BRANCH}" ]; then
-    live_dl openssl_quic "${OPENSSL_QUIC_VER_}"
-    live_xt openssl_quic "${OPENSSL_QUIC_HASH}"
+    live_dl openssl-quic "${OPENSSL_QUIC_VER_}"
+    live_xt openssl-quic "${OPENSSL_QUIC_HASH}"
   elif [ "${_BRANCH#*libressl*}" = "${_BRANCH}" ]; then
     if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ]; then
       OPENSSL_VER_='3.0.0-beta2'

_build.sh
@@ -290,7 +290,7 @@ build_single_target() {
   time ./nghttp3.sh       "${NGHTTP3_VER_}"
   time ./libressl.sh     "${LIBRESSL_VER_}"
   time ./openssl.sh       "${OPENSSL_VER_}"
-  time ./openssl.sh  "${OPENSSL_QUIC_VER_}" openssl_quic
+  time ./openssl.sh  "${OPENSSL_QUIC_VER_}" openssl-quic
   time ./ngtcp2.sh         "${NGTCP2_VER_}"
   time ./libssh2.sh       "${LIBSSH2_VER_}"
   time ./curl.sh             "${CURL_VER_}"
@@ -308,6 +308,7 @@ build_single_target() {
 
   (
     cd "${_DST}"
+    set +x
     _fn='BUILD-HASHES.txt'
     {
       find . -type f | grep -a -E '/(bin|include|lib)/' | sort | while read -r f; do

_bumper.sh
@@ -18,17 +18,6 @@ fi
 
 ./_dl.sh bump
 
-# Find out the latest tag for OpenSSL QUIC:
-# (standard bumper logic fails because these are branches, not releases)
-
-latest_tag="$(curl --disable --user-agent ' ' --silent --fail --show-error \
-    'https://api.github.com/repos/quictls/openssl/git/refs/heads' \
-  | jq '.[].ref' \
-  | grep -E '/openssl-\d+\.\d+\.\d+\+quic\"' \
-  | grep -E -o '\d+\.\d+\.\d' | sort | tail -1)"
-
-echo "\n  OpenSSL QUIC: ${latest_tag}"
-
 # Find out the latest docker image release:
 
 name='debian'

_pkg.sh
@@ -13,8 +13,6 @@ fi
 
 _cdo="$(pwd)"
 
-_NAMX="$(printf '%s' "${_NAM}" | tr '_' '-')"
-
 if [ "${_NAM}" != "${_UNIPKG}" ]; then
 
   find "${_DST}" -depth -type d -exec touch -c -r "$1" '{}' +
@@ -37,7 +35,7 @@ if [ "${_NAM}" != "${_UNIPKG}" ]; then
       cp -f -p "${_DST}"/*.* "${unipkg}"
       rsync --archive --update "${_DST}/docs" "${unipkg}"
     else
-      _NAM_DEP="${unipkg}/dep/${_NAMX}"
+      _NAM_DEP="${unipkg}/dep/${_NAM}"
       mkdir -p "${_NAM_DEP}"
       cp -f -p "${_DST}"/*.* "${_NAM_DEP}"
     fi
@@ -143,7 +141,7 @@ EOF
 }
 
 if [ "${_NAM}" != "${_UNIPKG}" ]; then
-  ver="${_NAMX} ${_VER}"
+  ver="${_NAM} ${_VER}"
   echo "${ver}" >> "${_UNIMFT}"
   if ! grep -q -a -F "${ver}" -- "${_BLD}"; then
     echo "${ver}" >> "${_BLD}"

curl.sh
@@ -92,8 +92,8 @@ _VER="$1"
 
   if [ -d ../libressl ]; then
     export OPENSSL_PATH=../../libressl/pkg/usr/local
-  elif [ -d ../openssl_quic ]; then
-    export OPENSSL_PATH=../../openssl_quic/pkg/usr/local
+  elif [ -d ../openssl-quic ]; then
+    export OPENSSL_PATH=../../openssl-quic/pkg/usr/local
     # Workaround for 3.x deprecation warnings
     CURL_CFLAG_EXTRAS="${CURL_CFLAG_EXTRAS} -DOPENSSL_SUPPRESS_DEPRECATED"
   elif [ -d ../openssl ]; then
@@ -169,7 +169,7 @@ _VER="$1"
 
   # Download CA bundle
   # CAVEAT: Build-time download. It can break reproducibility.
-  if [ -d ../libressl ] || [ -d ../openssl ] || [ -d ../openssl_quic ]; then
+  if [ -d ../libressl ] || [ -d ../openssl ] || [ -d ../openssl-quic ]; then
     [ -f '../ca-bundle.crt' ] || \
       curl --disable --user-agent '' --fail --silent --show-error \
         --remote-time --xattr \
@@ -245,7 +245,7 @@ _VER="$1"
   cp -f -p README                   "${_DST}/README.txt"
   cp -f -p RELEASE-NOTES            "${_DST}/RELEASE-NOTES.txt"
 
-  if [ -d ../libressl ] || [ -d ../openssl ] || [ -d ../openssl_quic ]; then
+  if [ -d ../libressl ] || [ -d ../openssl ] || [ -d ../openssl-quic ]; then
     cp -f -p scripts/mk-ca-bundle.pl "${_DST}/"
     cp -f -p ../ca-bundle.crt        "${_DST}/bin/curl-ca-bundle.crt"
   fi

libssh2.sh
@@ -62,8 +62,8 @@ _VER="$1"
   if [ -d ../libressl ]; then
     options="${options} --with-crypto=openssl --with-libssl-prefix=$(pwd)/../libressl/pkg/usr/local"
     CFLAGS="${CFLAGS} -DHAVE_EVP_AES_128_CTR=1 -DNOCRYPT"
-  elif [ -d ../openssl_quic ]; then
-    options="${options} --with-crypto=openssl --with-libssl-prefix=$(pwd)/../openssl_quic/pkg/usr/local"
+  elif [ -d ../openssl-quic ]; then
+    options="${options} --with-crypto=openssl --with-libssl-prefix=$(pwd)/../openssl-quic/pkg/usr/local"
     CFLAGS="${CFLAGS} -DHAVE_EVP_AES_128_CTR=1 -DOPENSSL_SUPPRESS_DEPRECATED"
   elif [ -d ../openssl ]; then
     options="${options} --with-crypto=openssl --with-libssl-prefix=$(pwd)/../openssl/pkg/usr/local"

ngtcp2.sh
@@ -52,11 +52,11 @@ _VER="$1"
   [ "${_OS}" = 'mac' ] && options="${options} -DCMAKE_AR=${_SYSROOT}/bin/${_CCPREFIX}ar"
   options="${options} -DENABLE_STATIC_LIB=1"
   options="${options} -DENABLE_SHARED_LIB=0"
-  if [ -d ../openssl_quic ]; then
+  if [ -d ../openssl-quic ]; then
     options="${options} -DENABLE_OPENSSL=1"
     options="${options} -DHAVE_SSL_IS_QUIC=1"  # Detection fails with a long list of unfixable errors, so force it.
-    options="${options} -DOPENSSL_ROOT_DIR=../openssl_quic/pkg/usr/local"
-    options="${options} -DOPENSSL_INCLUDE_DIR=../openssl_quic/pkg/usr/local/include"
+    options="${options} -DOPENSSL_ROOT_DIR=../openssl-quic/pkg/usr/local"
+    options="${options} -DOPENSSL_INCLUDE_DIR=../openssl-quic/pkg/usr/local/include"
   fi
   if [ -d ../nghttp3 ]; then
     options="${options} -DLIBNGHTTP3_LIBRARY=../nghttp3/pkg/usr/local/lib"
@@ -99,7 +99,7 @@ _VER="$1"
 
   # Rename static libs so they get found by dependents
 
-  if [ -d ../openssl_quic ]; then
+  if [ -d ../openssl-quic ]; then
     mv -f ${_pkg}/lib/libngtcp2_crypto_openssl_static.a ${_pkg}/lib/libngtcp2_crypto_openssl.a
   fi
   mv -f ${_pkg}/lib/libngtcp2_static.a ${_pkg}/lib/libngtcp2.a

commit 9019050036ef57824d05e614b65877b8c4da919d

_dl.sh: fix bump detection for ngtcp2/nghttp3 [ci skip]
Regression since db02347fd2a01e1ce81b2cbaf84da4a491e7ffef

_dl.sh
@@ -188,7 +188,7 @@ EOF
 }
 
 my_curl() {
-  # >&2 echo "|$@|"
+  # >&2 echo "my_curl|$@|"
   curl --disable --user-agent '' --fail --silent --show-error --globoff \
     --connect-timeout 15 --max-time 60 --retry 3 --max-redirs 10 "$@"
 }
@@ -216,6 +216,7 @@ gpg_recv_key() {
 
 # replace {ver}/{vermm} macros with the version number
 expandver() {
+  # >&2 echo "expandver|$@|"
   sed \
     -e "s/{ver}/$1/g" \
     -e "s/{vermm}/$(echo "$1" | cut -d . -f -2)/g"
@@ -249,7 +250,12 @@ check_update() {
         | grep -a -o -E "$9")"
     # heavily rate-limited
     elif [ -n "$5" ]; then
-      ref="$(my_curl --user-agent ' ' "https://api.github.com/repos/${slug}/git/refs/heads" \
+      if [[ "${url}" = *'/refs/heads/'* ]]; then
+        heads_or_tags='heads'
+      else
+        heads_or_tags='tags'
+      fi
+      ref="$(my_curl --user-agent ' ' "https://api.github.com/repos/${slug}/git/refs/${heads_or_tags}" \
         | jq --raw-output '.[].ref' \
         | grep -a -E "$5" | sort | tail -1)"
       newver="$(printf '%s' "${ref}" | grep -a -E -o '\d+\.\d+\.\d')"