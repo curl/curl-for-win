curl/curl-for-win

commit 090c1f95f7d063e99a2eca0a352f27bf56036de7

_dl.sh: try fixing it differently

_build.sh
@@ -94,7 +94,7 @@ rm -f hashes.txt
 rm -f "${_BLD}"
 
 # Download sources
-. ./_dl.sh || exit 1
+./_dl.sh || exit 1
 
 # Decrypt package signing key
 SIGN_PKG_KEY='sign-pkg.gpg.asc'

_dl.sh
@@ -107,7 +107,7 @@ check_update() {
   ourvern="${2:-000000}"
   url="$3"
   unset newver
-  if [[ "${url}" =~ ^https:\/\/github.com\/([a-zA-Z0-9-]+\/[a-zA-Z0-9-]+)\/ ]]; then
+  if [[ "${url}" =~ ^https://github.com/([a-zA-Z0-9-]+/[a-zA-Z0-9-]+)/ ]]; then
     slug="${BASH_REMATCH[1]}"
     # heavily rate-limited
     newver="$(my_curl --user-agent ' ' "https://api.github.com/repos/${slug}/releases/latest" | \

commit 44d99dad14dd65ee5b3c7cb14fee9987acf9014c

_build.sh: fix dev branch regression [ci skip]
So that the CURL_VER_ and other overrides in dl.sh actually work
when doing a dev build.

Regression from 090c1f95f7d063e99a2eca0a352f27bf56036de7

[UNTESTED]

_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Copyright (C) Viktor Szakats. See LICENSE.md
 # SPDX-License-Identifier: MIT
@@ -242,9 +242,7 @@ touch hashes.txt "${_BLD}" "${_URLS}"
 export _REVSUFFIX="${_REV}"; [ -z "${_REVSUFFIX}" ] || _REVSUFFIX="_${_REVSUFFIX}"
 
 # Download sources
-# FIXME: Since 090c1f95f7d063e99a2eca0a352f27bf56036de7, versions/hashes
-#        remain unchanged after overriding them in ./_dl.sh for 'dev' builds.
-./_dl.sh
+. ./_dl.sh
 
 # Find and setup llvm-mingw downloaded above.
 if [ -z "${CW_LLVM_MINGW_PATH:-}" ] && \