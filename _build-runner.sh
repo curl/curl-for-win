8760e399a3e8c3a56d38d8298f10c8920fb28e70
Viktor Szakats <commit@vsz.me>
2023-02-25 13:24:13 +0000

    _build-runner.sh: standalone runner [ci skip]
    The idea is to help launching a curl-for-win build from a curl CI
    session, or outside CI, on a local machine. It supports cross-builds
    from Debian, macOS, or MSYS2 (untested).
    
    I've been using a similar script for local tests for a long time.
    
    Builds support llvm/clang (default) or gcc, for curl it supports
    GNU make, CMake and Makefile.mk (default). For CPU, it supports
    64-bit, 32-bit and ARM 64-bit.
    
    It supports predefined configurations, with options to enable c-ares,
    wolfssl, wolfssh, libssh, mbedtls, libressl, boringssl, libpsl,
    libidn2. You can select mainline openssl (e.g. 3.1.0 beta) by disabling
    HTTP/3. See the list under `CW_CONFIG` inside `./_build.sh`.
    
    The default config is x64-only, using latest curl and libssh2 master
    branches, with c-ares and libpsl over the official curl Windows
    binaries. You can customize curl/libssh2 branches via `CURL_REV_` and
    `LIBSSH2_REV_`.
    
    These builds use a local (automatically downloaded) copy of
    `llvm-mingw`, and trivial set of apt/brew/msys2 packages. Plus go/nasm
    for boringssl builds.
    
    The build skips code signing, packages signing and package uploads.
    
    The script downloads and builds everything necessary according
    to the versions listed in `./_versions.sh`. I used to bump these to
    their latest versions using `./_bump.sh`.
    
    Consider this a WIP and EXPERIMENTAL.
    
    Ref: https://github.com/curl/curl/issues/10603

---
+++ _build-runner.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Copyright (C) Viktor Szakats. See LICENSE.md
+# SPDX-License-Identifier: MIT
+
+# WORK-IN-PROGRESS / EXPERIMENTAL
+
+# This script makes a local curl (and dependencies) build.
+# Start it in a curl-for-win repo sandbox.
+# Output is generated in the same directory.
+
+# shellcheck disable=SC3040
+set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
+
+cd "$(dirname "$0")"
+
+# Customize these
+export CW_CONFIG='dev-x64-big-cares'
+#export CURL_REV_='master'
+#CURL_SCRIPT='curl-cmake'  # curl-gnumake (default) or curl-cmake or curl-autotools
+
+# Install necessary packages
+if [ ! -f .cw-initialized ]; then
+  case "$(uname)" in
+    *_NT*)
+      pacman --noconfirm --ask 20 --noprogressbar --sync --needed \
+        mingw-w64-x86_64-{cmake,jq,python-pefile,gettext} \
+        zip
+      [[ "${CW_CONFIG}" = *'boringssl'* ]] && \
+      pacman --noconfirm --ask 20 --noprogressbar --sync --needed \
+        mingw-w64-x86_64-{go,nasm}
+      ;;
+    Linux*)
+      apt-get --quiet 2 --option Dpkg::Use-Pty=0 install \
+        curl git gpg python3-pefile make cmake \
+        autoconf automake autopoint libtool \
+        zip time jq dos2unix
+      [[ "${CW_CONFIG}" = *'boringssl'* ]] && \
+      apt-get --quiet 2 --option Dpkg::Use-Pty=0 install \
+        golang nasm
+      ;;
+    Darwin*)
+      brew install \
+        xz gnu-tar gettext jq dos2unix
+      [[ "${CW_CONFIG}" = *'boringssl'* ]] && \
+      brew install \
+        go nasm
+      ;;
+  esac
+  touch .cw-initialized
+fi
+
+# Not much to customize here
+export CW_LLVM_MINGW_DL=1
+export CW_LLVM_MINGW_ONLY=1
+export CW_NOTIME=1
+export CW_MAP=1
+export CW_JOBS=2
+
+export VIRUSTOTAL_APIKEY=
+export SIGN_CODE_GPG_PASS=
+export SIGN_CODE_KEY_PASS=
+export SIGN_PKG_KEY_ID=
+export SIGN_PKG_GPG_PASS=
+export SIGN_PKG_KEY_PASS=
+export DEPLOY_GPG_PASS=
+export DEPLOY_KEY_PASS=
+
+# Run
+ln -s -f "${CURL_SCRIPT}" curl
+
+./_build.sh 2>&1 | stdbuf -i0 -o0 -e0 tee "log-$(date '+%s').txt"
