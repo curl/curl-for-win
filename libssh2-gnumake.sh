curl/curl-for-win

commit 892dff07295621f09f48c8dd7c6fb9647390681d

libssh2: prepare for upstream patch set [ci skip]
Ref: https://github.com/libssh2/libssh2/pull/808

libssh2-gnumake.sh
@@ -16,12 +16,19 @@ _VER="$1"
 (
   cd "${_NAM}" || exit 0
 
-  export ARCH
-  [ "${_CPU}" = 'x64' ] && ARCH='w64'
-  [ "${_CPU}" = 'x86' ] && ARCH='w32'
-  # FIXME: ARM64 support missing from upstream.
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    [ "${_CPU}" = 'x64' ] && ARCH='w64'
+    [ "${_CPU}" = 'x86' ] && ARCH='w32'
+    # ARM64 support missing from upstream.
+  else
+    export ARCH='custom'
+  fi
 
-  CPPFLAGS='-DHAVE_STRTOLL -DHAVE_DECL_SECUREZEROMEMORY=1 -DLIBSSH2_CLEAR_MEMORY'
+  CPPFLAGS='-DHAVE_STRTOLL'
+
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    CPPFLAGS="${CPPFLAGS} -DHAVE_DECL_SECUREZEROMEMORY=1 -DLIBSSH2_CLEAR_MEMORY"
+  fi
 
   if [ -n "${_ZLIB}" ]; then
     export ZLIB_PATH="../../${_ZLIB}/${_PP}/include"
@@ -46,12 +53,16 @@ _VER="$1"
     export WITH_WINCNG=1
   fi
 
-  export CROSSPREFIX="${_BINUTILS_PREFIX}"  # for windres
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    export CROSSPREFIX="${_BINUTILS_PREFIX}"  # for windres
+  fi
   export LIBSSH2_CC="${_CC_GLOBAL}"
+  export LIBSSH2_RC="${RC}"
   export LIBSSH2_AR="${AR}"
   export LIBSSH2_RANLIB="${RANLIB}"
   export LIBSSH2_CFLAG_EXTRAS="${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${CPPFLAGS}"
   export LIBSSH2_LDFLAG_EXTRAS="${_LDFLAGS_GLOBAL}"
+  export LIBSSH2_RCFLAG_EXTRAS="${_RCFLAGS_GLOBAL}"
   export LIBSSH2_DLL_A_SUFFIX='.dll'
 
   if [ "${CW_DEV_INCREMENTAL:-}" != '1' ]; then

_build.sh
@@ -522,7 +522,9 @@ build_single_target() {
 
   export _LD
   _BINUTILS_PREFIX="${_CCPREFIX}"
-  export _BINUTILS_PREFIX  # Exclusively for libssh2-gnumake.sh
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    export _BINUTILS_PREFIX  # Exclusively for libssh2-gnumake.sh
+  fi
   if [ "${_CC}" = 'llvm' ]; then
     _CC_GLOBAL="clang${CW_CCSUFFIX} --target=${_TRIPLET}"
     _CONFIGURE_GLOBAL="${_CONFIGURE_GLOBAL} --target=${_TRIPLET}"

libssh2-cmake.sh
@@ -16,10 +16,14 @@ _VER="$1"
 
   rm -r -f "${_PKGDIR}" "${_BLDDIR}"
 
-  CPPFLAGS='-DHAVE_DECL_SECUREZEROMEMORY=1 -D_FILE_OFFSET_BITS=64'
+  CPPFLAGS='-D_FILE_OFFSET_BITS=64'
   LDFLAGS=''
   LIBS=''
   options=''
+
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    CPPFLAGS="${CPPFLAGS} -DHAVE_DECL_SECUREZEROMEMORY=1"
+  fi
 
   if [ -n "${_ZLIB}" ]; then
     options="${options} -DENABLE_ZLIB_COMPRESSION=ON"

libssh2.sh
@@ -29,7 +29,9 @@ _VER="$1"
   export LDFLAGS="${_LDFLAGS_GLOBAL}"
   export LIBS="${_LIBS_GLOBAL}"
 
-  CPPFLAGS="${CPPFLAGS} -DHAVE_DECL_SECUREZEROMEMORY=1 -DLIBSSH2_CLEAR_MEMORY"
+  if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
+    CPPFLAGS="${CPPFLAGS} -DHAVE_DECL_SECUREZEROMEMORY=1 -DLIBSSH2_CLEAR_MEMORY"
+  fi
 
   # NOTE: root path with spaces breaks all values with '${_TOP}'. But,
   #       autotools breaks on spaces anyway, so let us leave it like that.

commit 2e75d5b3771f8aea018ebe99a7e2efce1e73e1c7

libssh2-gnumake.sh: refix to export ARCH with 1.10.0 [ci skip]
Regression from 892dff07295621f09f48c8dd7c6fb9647390681d

libssh2-gnumake.sh
@@ -17,8 +17,8 @@ _VER="$1"
   cd "${_NAM}" || exit 0
 
   if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
-    [ "${_CPU}" = 'x64' ] && ARCH='w64'
-    [ "${_CPU}" = 'x86' ] && ARCH='w32'
+    [ "${_CPU}" = 'x64' ] && export ARCH='w64'
+    [ "${_CPU}" = 'x86' ] && export ARCH='w32'
     # ARM64 support missing from upstream.
   else
     export ARCH='custom'