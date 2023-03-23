curl/curl-for-win

commit d1b0a350ba7abf90a8e9320f074d26ebfb618e20

_build.sh: let it run outside a git sandbox [ci skip]

_build.sh
@@ -142,16 +142,16 @@ elif [ -n "${CI_JOB_ID:-}" ]; then
 else
   _SLUG="curl/${_SELF}"
   _LOGURL=''
-  _COMMIT="$(git rev-parse --verify HEAD)"
-  _COMMIT_SHORT="$(git rev-parse --short=8 HEAD)"
+  _COMMIT="$(git rev-parse --verify HEAD || true)"
+  _COMMIT_SHORT="$(git rev-parse --short=8 HEAD || true)"
 fi
 echo "${_LOGURL}" | tee "${_LOG}"
 
 export _BRANCH="${APPVEYOR_REPO_BRANCH:-}${CI_COMMIT_REF_NAME:-}${GITHUB_REF:-}${CW_CONFIG:-}"
-[ -n "${_BRANCH}" ] || _BRANCH="$(git symbolic-ref --short --quiet HEAD)"
+[ -n "${_BRANCH}" ] || _BRANCH="$(git symbolic-ref --short --quiet HEAD || true)"
 [ -n "${_BRANCH}" ] || _BRANCH='main'
 if command -v git >/dev/null 2>&1; then
-  _URL_BASE="$(git ls-remote --get-url | sed 's/\.git$//')"
+  _URL_BASE="$(git ls-remote --get-url || true | sed 's/\.git$//')"
   _URL_FULL="${_URL_BASE}/tree/${_COMMIT}"
   _TAR="${_URL_BASE}/archive/${_COMMIT}.tar.gz"
 else

commit f153a3b0ba5abc405c08e8f5cbd77e6d6f38f387

_build.sh: fix broken URL for curl-for-win source archive [ci skip]
(We don't use this URL for anything yet.)

Regression from d1b0a350ba7abf90a8e9320f074d26ebfb618e20

_build.sh
@@ -153,7 +153,7 @@ export _BRANCH="${APPVEYOR_REPO_BRANCH:-}${CI_COMMIT_REF_NAME:-}${GITHUB_REF:-}$
 [ -n "${_BRANCH}" ] || _BRANCH="$(git symbolic-ref --short --quiet HEAD || true)"
 [ -n "${_BRANCH}" ] || _BRANCH='main'
 if command -v git >/dev/null 2>&1; then
-  _URL_BASE="$(git ls-remote --get-url || true | sed 's/\.git$//')"
+  _URL_BASE="$(git ls-remote --get-url | sed 's/\.git$//' || true)"
   _URL_FULL="${_URL_BASE}/tree/${_COMMIT}"
   _TAR="${_URL_BASE}/archive/${_COMMIT}.tar.gz"
 else