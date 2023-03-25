curl/curl-for-win

commit 6d423db7362acf6dd3428fb0a10fad1dc25d3da6

_build.sh: use distinctive build workdir names for dev/debug [ci skip]

_build.sh
@@ -308,6 +308,10 @@ bld() {
       grep -a -o -E -- "${pkg}-${bldtools}" || true)"
     if [ -n "${withbuildtool}" ] && [ -f "${withbuildtool}.sh" ]; then
       pkg="${withbuildtool}"
+
+      bldtool="$(echo "${pkg}" | \
+        grep -a -o -E -- "-${bldtools}")"
+      _BLDDIR="${_BLDDIR}${bldtool}-${_CC}-${_CPU}"
     fi
 
     ${my_time} "./${pkg}.sh" "$1" "${pkgori}"

commit 7b9d0ae2bc280537d66ce6e3334e1dd59052d2bf

_build.sh: fix _BLDDIR confusion with multiple custom-make builds [ci skip]
Affected local test builds only.

Regression from 6d423db7362acf6dd3428fb0a10fad1dc25d3da6

_build.sh
@@ -307,6 +307,8 @@ bld() {
   if [ -z "${CW_BLD:-}" ] || echo " ${CW_BLD} " | grep -q -E -- " ${pkg}(-${bldtools})? "; then
     shift
 
+    export _BLDDIR="${_BLDDIR_BASE}"
+
     pkgori="${pkg}"
     [ -n "${2:-}" ] && pkg="$2"
     # allow selecting an alternate build tool
@@ -495,10 +497,10 @@ build_single_target() {
   # Setup common toolchain configuration options
 
   export _TOP; _TOP="$(pwd)"  # Must be an absolute path
+  _BLDDIR_BASE='bld'
   export _PKGDIR="${_CPU}-${_CRT}"
   _PREFIX='/usr'
   export _PP="${_PKGDIR}${_PREFIX}"
-  export _BLDDIR='bld'
   export _CC_GLOBAL=''
   export _CFLAGS_GLOBAL=''
   export _CFLAGS_GLOBAL_CMAKE=''