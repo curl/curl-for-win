curl/curl-for-win

commit 9db2ce9ffc537de49cc6a77ab238c2896d8abb47

boringssl: libcrypto strip breakage and workarounds [ci skip]
binutils strip v2.40 cannot process NASM objects inside libcrypto.a and
fails with an error, leaving the lib unchanged:
```
/usr/local/bin/x86_64-w64-mingw32-strip: x64-ucrt/usr/lib/libcrypto.a(chacha-x86-win.asm.obj): invalid operation
```

llvm-strip v16.0.0 now corrupts NASM objects in libcrypto.a, so it's
also not an option. Here's the error when running llvm-strip on it a
second time:
```
x86_64-w64-mingw32-strip: error: 'libcrypto.a': SymbolTableIndex out of range
```

Disable both for binutils strip and llvm-strip for now. This makes
libcrypto.a not reproducible.

To not leave it like this, introduce another workaround: Turns out,
binutils strip works without the error above if applied to each object
separately. Add this feature to `_libclean.sh` and use it.

I haven't verified if this truly creates a reproducible libcrypto.a
across OSes. It may also behave differently with other combinations of
LLVM and binutils versions.

boringssl.sh
@@ -110,7 +110,9 @@ _VER="$1"
   #        - produces different output across build hosts after stripping libs
   #          compiled with -ggdb.
   #        - fails to strip the `.file` record from NASM objects.
-  #        - fails to clear timestamps in NASM objects (fixed by --reproducible).
+  #          (fixed by --reproducible with nasm v2.16)
+  #        - fails to clear timestamps in NASM objects.
+  #          (fixed by --reproducible with nasm v2.15.05)
   #        Work around them by running it through binutils strip. This works for
   #        x64 and x86, but not for ARM64.
   #
@@ -137,10 +139,14 @@ _VER="$1"
   # libcrypto-ggdb-binutils-llvm.a                  2479874
   # libcrypto-ggdb-binutils-llvm-binutils.a         2488066
 
-  [ -n "${_STRIP_BINUTILS}" ] && \
-  "${_STRIP_BINUTILS}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/*.a
+  "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libssl.a
+  # FIXME: llvm-strip corrupts nasm objects as of LLVM v16.0.0
+# "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcrypto.a
 
-  "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/*.a
+  # FIXME: Use binutils strip instead, directly on objects, to avoid
+  #        binutils strip v2.40 error `invalid operation` when run on
+  #        the whole lib:
+  ../_libclean.sh --strip "${_STRIP_BINUTILS}" "${_PP}"/lib/libcrypto.a
 
   touch -c -r "${_ref}" "${_PP}"/include/openssl/*.h
   touch -c -r "${_ref}" "${_PP}"/lib/*.a

_libclean.sh
@@ -16,7 +16,9 @@ set -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
 
 while [ "${1#--*}" != "${1:-}" ]; do
   if [ "$1" = '--ar' ]; then
-    shift; AR="$1"
+    shift; AR="$1"; shift
+  elif [ "$1" = '--strip' ]; then
+    shift; strip="$1"; shift
   fi
 done
 
@@ -40,6 +42,7 @@ while [ -n "${1:-}" ]; do
         -e 's/(\.cc\.obj|\.c\.obj|\.obj)$/.o/g')"
       [ "${o}" != "${n}" ] && mv -n "${o}" "${n}"
     done
+    [ -n "${strip}" ] && "${strip}" --enable-deterministic-archives --strip-debug "${tmp}"/*
     rm "${f}"
     # shellcheck disable=SC2046
     "${AR}" crD "${f}" $(find "${tmp}" -type f | sort)

commit 753a08c3567b0c1cd5970b02152a65a5340ad7d6

boringssl.sh: strip libcrypto.a with ARM64 builds again [ci skip]
Regression from 9db2ce9ffc537de49cc6a77ab238c2896d8abb47

boringssl.sh
@@ -141,13 +141,19 @@ _VER="$1"
   # libcrypto-ggdb-binutils-llvm-binutils.a         2488066
 
   "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libssl.a
-  # FIXME: llvm-strip corrupts nasm objects as of LLVM v16.0.0
-# "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcrypto.a
 
-  # FIXME: Use binutils strip instead, directly on objects, to avoid
-  #        binutils strip v2.40 error `invalid operation` when run on
-  #        the whole lib:
-  ../_libclean.sh --strip "${_STRIP_BINUTILS}" "${_PP}"/lib/libcrypto.a
+  if [ -n "${_STRIP_BINUTILS}" ]; then
+    # FIXME: llvm-strip corrupts nasm objects as of LLVM v16.0.0
+  # "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcrypto.a
+
+    # FIXME: Use binutils strip instead, directly on objects, to avoid
+    #        binutils strip v2.40 error `invalid operation` when run on
+    #        the whole lib:
+    ../_libclean.sh --strip "${_STRIP_BINUTILS}" "${_PP}"/lib/libcrypto.a
+  else
+    # We don't yet use ASM with ARM64 builds, so it's safe to use llvm-strip:
+    "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcrypto.a
+  fi
 
   touch -c -r "${_ref}" "${_PP}"/include/openssl/*.h
   touch -c -r "${_ref}" "${_PP}"/lib/*.a