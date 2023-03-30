curl/curl-for-win

commit 0c8ae6f8c2220d121b46507c933684ae98aec30a

libssh2: add hack for dll builds with boringssl [ci skip]
`LIBS="${LIBS} -lpthread"` doesn't seem to be necessary for detecting
boringssl with libssh2 autotools builds, according to latest tests.

libssh2-autotools.sh
@@ -46,7 +46,13 @@ _VER="$1"
   if [ -n "${_OPENSSL}" ]; then
     options="${options} --with-crypto=openssl --with-libssl-prefix=${_TOP}/${_OPENSSL}/${_PP}"
     if [ "${_OPENSSL}" = 'boringssl' ]; then
-      LIBS="${LIBS} -lpthread"
+      # for DLL
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+        LDFLAGS="${LDFLAGS} -Wl,-Bdynamic,-lpthread,-Bstatic"
+      else
+        LDFLAGS="${LDFLAGS} -Wl,-Bstatic,-lpthread,-Bdynamic"
+      fi
+    fi
     elif [ "${_OPENSSL}" = 'libressl' ]; then
       LIBS="${LIBS} -lbcrypt"
     elif [ "${_OPENSSL}" = 'quictls' ] || [ "${_OPENSSL}" = 'openssl' ]; then

libssh2-cmake.sh
@@ -45,6 +45,14 @@ _VER="$1"
         LIBS="${LIBS} -lws2_32"  # to detect HAVE_EVP_AES_128_CTR
       fi
     fi
+    if [ "${_OPENSSL}" = 'boringssl' ]; then
+      # for DLL
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+        LIBS="${LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
+      else
+        LIBS="${LIBS} -lpthread"
+      fi
+    fi
     # Silence useless libssh2 warnings about missing runtime DLLs
     touch \
       "${_TOP}/${_OPENSSL}/${_PP}/crypto.dll" \

libssh2-gnumake.sh
@@ -18,6 +18,7 @@ _VER="$1"
 
   CFLAGS=''
   CPPFLAGS=''
+  LIBS=''
 
   if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
     [ "${_CPU}" = 'x64' ] && export ARCH='w64'
@@ -49,6 +50,14 @@ _VER="$1"
         CPPFLAGS="${CPPFLAGS} -DNOCRYPT"  # Avoid warnings
       fi
     fi
+    if [ "${_OPENSSL}" = 'boringssl' ]; then
+      # for DLL
+      if [ "${_TOOLCHAIN}" = 'mingw-w64' ] && [ "${_CPU}" = 'x64' ] && [ "${_CRT}" = 'ucrt' ]; then  # FIXME
+        LIBS="${LIBS} -Wl,-Bdynamic -lpthread -Wl,-Bstatic"
+      else
+        LIBS="${LIBS} -lpthread"
+      fi
+    fi
   elif [ -d ../wolfssl ]; then
     if [ "${LIBSSH2_VER_}" = '1.10.0' ]; then
       CPPFLAGS="${CPPFLAGS} -DLIBSSH2_WOLFSSL"
@@ -71,14 +80,14 @@ _VER="$1"
     export LIBSSH2_RANLIB="${RANLIB}"
     export LIBSSH2_DLL_A_SUFFIX='.dll'
     export LIBSSH2_CFLAG_EXTRAS="${_CFLAGS_GLOBAL} ${CFLAGS} ${_CPPFLAGS_GLOBAL} ${CPPFLAGS}"
-    export LIBSSH2_LDFLAG_EXTRAS="${_LDFLAGS_GLOBAL}"
+    export LIBSSH2_LDFLAG_EXTRAS="${_LDFLAGS_GLOBAL} ${LIBS}"
     export LIBSSH2_RCFLAG_EXTRAS="${_RCFLAGS_GLOBAL}"
   else
     export CC="${_CC_GLOBAL}"
     export CFLAGS="${_CFLAGS_GLOBAL} ${CFLAGS}"
     export CPPFLAGS="${_CPPFLAGS_GLOBAL} ${CPPFLAGS}"
     export LDFLAGS="${_LDFLAGS_GLOBAL}"
-    export LIBS="${_LIBS_GLOBAL}"
+    export LIBS="${_LIBS_GLOBAL} ${LIBS}"
     export RCFLAGS="${_RCFLAGS_GLOBAL}"
   fi
 

commit be4d555eb7f2b7fa4ee7dcf25141fd6f9a23a7ab

libssh2-autotools.sh: fix copy-paste error
Regression from 0c8ae6f8c2220d121b46507c933684ae98aec30a

libssh2-autotools.sh
@@ -52,7 +52,6 @@ _VER="$1"
       else
         LDFLAGS="${LDFLAGS} -Wl,-Bstatic,-lpthread,-Bdynamic"
       fi
-    fi
     elif [ "${_OPENSSL}" = 'libressl' ]; then
       LIBS="${LIBS} -lbcrypt"
     elif [ "${_OPENSSL}" = 'quictls' ] || [ "${_OPENSSL}" = 'openssl' ]; then