4bd8c7b8e448be2c186875f89b76ceac75067571
Viktor Szakats <commit@vsz.me>
2023-08-25 15:07:25 +0000

    curl-pkg.sh: fix to package all .so files with linux target [ci skip]

--- curl-pkg.sh
+++ curl-pkg.sh
@@ -108,27 +108,34 @@
       fi
     done
   )
-  cp -f -p "${_PP}"/include/curl/*.h         "${_DST}/include/curl/"
-  cp -f -p "${_PP}/bin/curl${BIN_EXT}"       "${_DST}/bin/"
-  cp -f -a "${_PP}/${DYN_DIR}"/*"${DYN_EXT}" "${_DST}/${DYN_DIR}/"
-  cp -f -p "${_PP}"/lib/*.a                  "${_DST}/lib/"
-  cp -f -p docs/*.md                         "${_DST}/docs/"
-  cp -f -p CHANGES                           "${_DST}/CHANGES.txt"
-  cp -f -p COPYING                           "${_DST}/COPYING.txt"
-  cp -f -p README                            "${_DST}/README.txt"
-  cp -f -p RELEASE-NOTES                     "${_DST}/RELEASE-NOTES.txt"
+  cp -f -p "${_PP}"/include/curl/*.h          "${_DST}/include/curl/"
+  cp -f -p "${_PP}/bin/curl${BIN_EXT}"        "${_DST}/bin/"
+  cp -f -a "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"  "${_DST}/${DYN_DIR}/"  # we must not pick up *.dll.a here
+  cp -f -p "${_PP}"/lib/*.a                   "${_DST}/lib/"
+  cp -f -p docs/*.md                          "${_DST}/docs/"
+  cp -f -p CHANGES                            "${_DST}/CHANGES.txt"
+  cp -f -p COPYING                            "${_DST}/COPYING.txt"
+  cp -f -p README                             "${_DST}/README.txt"
+  cp -f -p RELEASE-NOTES                      "${_DST}/RELEASE-NOTES.txt"
 
   if [ "${_OS}" = 'win' ]; then
-    cp -f -p "${_PP}"/bin/*.def                "${_DST}/bin/"
+    cp -f -p "${_PP}"/bin/*.def                 "${_DST}/bin/"
+  fi
+
+  if [ "${_OS}" = 'linux' ]; then
+    # To copy these files in addition to `@libcurl.so -> libcurl.so.4`:
+    #   @libcurl.so.4 -> libcurl.so.4.8.0
+    #    libcurl.so.4.8.0
+    cp -f -a "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"* "${_DST}/${DYN_DIR}/"
   fi
 
   if [ "${CW_MAP}" = '1' ]; then
-    cp -f -p "${_PP}"/bin/curl.map             "${_DST}/bin/"
-    cp -f -p "${_PP}/${DYN_DIR}"/*.map         "${_DST}/${DYN_DIR}/"
+    cp -f -p "${_PP}"/bin/curl.map              "${_DST}/bin/"
+    cp -f -p "${_PP}/${DYN_DIR}"/*.map          "${_DST}/${DYN_DIR}/"
   fi
 
   if [ -d ../cacert ]; then
-    cp -f -p scripts/mk-ca-bundle.pl           "${_DST}/"
+    cp -f -p scripts/mk-ca-bundle.pl            "${_DST}/"
   fi
 
   ../_pkg.sh "$(pwd)/${_ref}"
