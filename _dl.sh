2f355e23dd3a75ed446021989d4da5dc0519c96c
Viktor Szakats <commit@vsz.me>
2023-08-28 11:24:53 +0000

    _dl.sh: `mkdir -m` shellcheck warning fix [ci skip]
    Follow-up to c68846a684763564a0865af9af5f1a9abb67edfa

--- _dl.sh
+++ _dl.sh
@@ -5,7 +5,9 @@
 
 set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
 
-gpgdir="$(realpath .cw-gpg)"; rm -r -f "${gpgdir}"; mkdir -m 0700 -p "${gpgdir}"
+gpgdir="$(realpath .cw-gpg)"; rm -r -f "${gpgdir}"
+# shellcheck disable=SC2174
+mkdir -m 0700 -p "${gpgdir}"
 
 # NOTE: We would prefer using the canonical source for BoringSSL. But, the
 #       tarball does change for each download (the timestamps in it), so we
