95279ed85efc3cca1e459215b17a13ca4d8ff13a
Viktor Szakats <commit@vsz.me>
2023-09-02 22:27:57 +0000

    _ci-mac-homebrew.sh: silence analytics message

--- _ci-mac-homebrew.sh
+++ _ci-mac-homebrew.sh
@@ -13,6 +13,7 @@ extra=''
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_ANALYTICS_MESSAGE_OUTPUT=1
 brew update >/dev/null
 # shellcheck disable=SC2086
 # Using `|| true` to avoid failing due to preinstalled non-Homebrew
