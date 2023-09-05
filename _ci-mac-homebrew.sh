89fdf5ef2f239d648c6affa08c615ea4c0614001
Viktor Szakats <commit@vsz.me>
2023-09-05 01:12:51 +0000

    switch default compiler to apple clang for mac targets
    This allows to:
    - build for arm64e, which doesn't seem to be possible with llvm just yet
      (as of v16.0.6).
    - not waste CI time installing Homebrew packages.
    - quicker builds.
    
    You can switch back to Homebrew LLVM with `CW_CONFIG=llvm`.
    
    Internally, both cases use `_CC=llvm`, though in case of apple clang
    `_TOOLCHAIN=llvm-apple`.
    
    Also:
    - Use the native `clang` tool, instead of relying on the `gcc`
      compatibility interface.
    - Use the `llvm-` prefixed otool for binary info in `curl-pkg.sh` when
      using the non-Apple clang toolchain.
    - Show an error if `CW_CONFIG=gcc` is selected and the gcc command maps
      to the real GNU C Compiler, as opposed to the Apple clang alias
      installed by Xcode. We don't support the real (Homebrew)
      GNU C Compiler for mac targets.

--- _ci-mac-homebrew.sh
+++ _ci-mac-homebrew.sh
@@ -9,17 +9,20 @@ set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o p
 extra=''
 [[ "${CW_CONFIG:-}" = *'boringssl'* ]] && extra="${extra} go nasm"
 [[ "${CW_CONFIG:-}" = *'win'* ]] && extra="${extra} mingw-w64 osslsigncode wine-stable openssh"
+[[ "${CW_CONFIG:-}" != *'mac'* ]] && extra="${extra} llvm"
 [[ "${CW_CONFIG:-}" = *'linux'* ]] && extra="${extra} FiloSottile/musl-cross/musl-cross"
 
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
-export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_NO_ANALYTICS_MESSAGE_OUTPUT=1
-brew update >/dev/null || true
-# shellcheck disable=SC2086
-# Using `|| true` to avoid failing due to preinstalled non-Homebrew
-# python3: `Could not symlink bin/2to3`
-brew install llvm ${extra} || true
+if [ -n "${extra}" ]; then
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+  export HOMEBREW_NO_ANALYTICS=1
+  export HOMEBREW_NO_ANALYTICS_MESSAGE_OUTPUT=1
+  brew update >/dev/null || true
+  # shellcheck disable=SC2086
+  # Using `|| true` to avoid failing due to preinstalled non-Homebrew
+  # python3: `Could not symlink bin/2to3`
+  brew install ${extra} || true
+fi
 
 [[ "${CW_CONFIG:-}" = *'win'* ]] && wineboot --init
 
