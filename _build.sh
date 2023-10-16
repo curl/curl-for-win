curl/curl-for-win

commit 898aeabc9f7f27e1781a1575efb342bf9c8df5b1

_build.sh: enable arm64e for standard llvm for mac [ci skip]
Also set it in the triplet for both apple clang and llvm.

arm64e is arm64 v8.3e allowing pointer-authentication codegen for
apple-a12 CPU and newer (including M1 and newer).

Ref: https://reviews.llvm.org/D87095

For llvm this isn't reflected in `objdump --arch=all --private-headers`
`cpusubtype` output. It says `ALL` instead of `E`. So maybe this change
is not enough.

_build.sh
@@ -522,6 +522,10 @@ build_single_target() {
   [ "${_CPU}" = 'x64' ] && _machine='x86_64'
   [ "${_CPU}" = 'a64' ] && _machine='aarch64'
 
+  if [ "${_OS}" = 'mac' ] && [ "${_machine}" = 'aarch64' ] && [ "${_CC}" = 'llvm' ]; then
+    _machine='arm64e'
+  fi
+
   export _CURL_DLL_SUFFIX=''
   export _CURL_DLL_SUFFIX_NODASH=''
   if [ "${_OS}" = 'win' ]; then
@@ -825,7 +829,6 @@ build_single_target() {
   _BINUTILS_SUFFIX=''
   if [ "${_TOOLCHAIN}" = 'llvm-apple' ]; then
     _arch="${_machine}"
-    [ "${_CPU}" = 'a64' ] && _arch='arm64e'
     _CC_GLOBAL="clang${_CCSUFFIX} -arch ${_arch}"
     # supports multiple values separated by ';', e.g. 'arm64e;x86_64'
     _CMAKE_GLOBAL="${_CMAKE_GLOBAL} -DCMAKE_OSX_ARCHITECTURES=${_arch}"

commit 8dde5f4168471d73969aab8aa4534ae682bbdcc2

_build.sh: keep out arm64e designator from autotools [ci skip]
autotools doesn't recognize `arm64e`:
```
Invalid configuration `arm64e-apple-darwin': machine `arm64e-apple' not recognized
```
Ref: https://github.com/curl/curl-for-win/actions/runs/6530947363/job/17732106776#step:3:4161

So backtrack and pass this value only to `-arch` for clang-apple,
and `CMAKE_OSX_ARCHITECTURES` (we will adjust if that fails, but
so far it seems to be accepted). This also revert passing arm64e
to standard llvm which accepts this, but seems to be ignoring it
anyway (as of llvm 16/17).

Regression from 898aeabc9f7f27e1781a1575efb342bf9c8df5b1

_build.sh
@@ -523,11 +523,12 @@ build_single_target() {
   [ "${_CPU}" = 'a64' ] && _machine='aarch64'
 
   if [ "${_OS}" = 'mac' ] && [ "${_machine}" = 'aarch64' ] && [ "${_CC}" = 'llvm' ]; then
-    _machine='arm64e'
+    # llvm-apple supports multiple archs separated by ';', e.g. 'arm64e;x86_64'
+    # It also understands arm64e (vs arm64)
+    _machines='arm64e'
+  else
+    _machines="${_machine}"
   fi
-
-  # llvm-apple supports multiple archs separated by ';', e.g. 'arm64e;x86_64'
-  _machines="${_machine}"
 
   export _CURL_DLL_SUFFIX=''
   export _CURL_DLL_SUFFIX_NODASH=''