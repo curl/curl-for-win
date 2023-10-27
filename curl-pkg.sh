curl/curl-for-win

commit bcd0261b0d2d1ce5b1c403df9f25f68698fe1de4

support packaging for non-Windows builds [ci skip]

curl-pkg.sh
@@ -10,26 +10,26 @@
 
   readonly _ref='CHANGES'
 
-  "${_STRIP}" --enable-deterministic-archives --strip-all   "${_PP}"/bin/*.exe
-  "${_STRIP}" --enable-deterministic-archives --strip-all   "${_PP}"/bin/*.dll
+  "${_STRIP}" --enable-deterministic-archives --strip-all   "${_PP}/bin/curl${BIN_EXT}"
+  "${_STRIP}" --enable-deterministic-archives --strip-all   "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"
   "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcurl.a
   # LLVM strip does not support implibs, but they are deterministic by default:
   #   error: unsupported object file format
-  [ "${_LD}" = 'ld' ] && "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcurl.dll.a
+  [ "${_LD}" = 'ld' ] && [ "${_OS}" = 'win' ] && "${_STRIP}" --enable-deterministic-archives --strip-debug "${_PP}"/lib/libcurl.dll.a
 
-  ../_clean-bin.sh "${_ref}" "${_PP}"/bin/*.exe
-  ../_clean-bin.sh "${_ref}" "${_PP}"/bin/*.dll
+  ../_clean-bin.sh "${_ref}" "${_PP}/bin/curl${BIN_EXT}"
+  ../_clean-bin.sh "${_ref}" "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"
 
-  ../_sign-code.sh "${_ref}" "${_PP}"/bin/*.exe
-  ../_sign-code.sh "${_ref}" "${_PP}"/bin/*.dll
+  ../_sign-code.sh "${_ref}" "${_PP}/bin/curl${BIN_EXT}"
+  ../_sign-code.sh "${_ref}" "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"
 
-  touch -c -r "${_ref}" "${_PP}"/bin/*.exe
-  touch -c -r "${_ref}" "${_PP}"/bin/*.dll
-  touch -c -r "${_ref}" "${_PP}"/bin/*.def
+  touch -c -r "${_ref}" "${_PP}/bin/curl${BIN_EXT}"
+  touch -c -r "${_ref}" "${_PP}/${DYN_DIR}"/*"${DYN_EXT}"
   touch -c -r "${_ref}" "${_PP}"/lib/*.a
 
   if [ "${CW_MAP}" = '1' ] && [ "${_OS}" != 'mac' ]; then
     touch -c -r "${_ref}" "${_PP}"/bin/*.map
+    touch -c -r "${_ref}" "${_PP}/${DYN_DIR}"/*.map
   fi
 
   # Tests
@@ -41,22 +41,35 @@
   esac
 
   for suffix in exe dyn; do
-    TZ=UTC "${_OBJDUMP}" --all-headers "${_PP}"/bin/*."${suffix}" | grep -a -E -i "(file format|DLL Name|Time/Date)" | sort -r -f
+    {
+      if [ "${suffix}" = 'exe' ]; then
+        echo "${_PP}/bin/curl${BIN_EXT}"
+      else
+        find "${_PP}/${DYN_DIR}" -name "*${DYN_EXT}" | sort
+      fi
+    } | while read -r f; do
 
-    # Verify exported curl symbols
-    if [ "${suffix}" = 'exe' ]; then
-      "${_OBJDUMP}" --all-headers "${_PP}"/bin/*."${suffix}" | grep -a -F ' curl_' && false  # should not have any hits for statically linked curl
-    else
-      "${_OBJDUMP}" --all-headers "${_PP}"/bin/*."${suffix}" | grep -a -F ' curl_' || false  # show public libcurl APIs (in a well-defined order)
-    fi
-
-    # Dump 'DllCharacteristics' flags, e.g. HIGH_ENTROPY_VA, DYNAMIC_BASE, NX_COMPAT, GUARD_CF, TERMINAL_SERVICE_AWARE
-    "${_OBJDUMP}" --all-headers "${_PP}"/bin/*."${suffix}" | grep -a -E -o '^\s+[A-Z_]{4,}$' | sort
-    # Dump cfguard load configuration flags
-    if [ "${_CC}" = 'llvm' ]; then  # binutils readelf (as of v2.40) does not recognize this option
-      # CF_FUNCTION_TABLE_PRESENT, CF_INSTRUMENTED, CF_LONGJUMP_TABLE_PRESENT (optional)
-      "${_READELF}" --coff-load-config "${_PP}"/bin/*."${suffix}" | grep -a -E 'CF_[A-Z_]' | sort
-    fi
+      if [ "${_OS}" = 'win' ]; then
+        TZ=UTC "${_OBJDUMP}" --all-headers "${f}" | grep -a -E -i "(file format|DLL Name|Time/Date)" | sort -r -f
+        # Verify exported curl symbols
+        if [ "${suffix}" = 'exe' ]; then
+          "${_OBJDUMP}" --all-headers "${f}" | grep -a -F ' curl_' && false  # should not have any hits for statically linked curl
+        else
+          "${_OBJDUMP}" --all-headers "${f}" | grep -a -F ' curl_' || false  # show public libcurl APIs (in a well-defined order)
+        fi
+        # Dump 'DllCharacteristics' flags, e.g. HIGH_ENTROPY_VA, DYNAMIC_BASE, NX_COMPAT, GUARD_CF, TERMINAL_SERVICE_AWARE
+        "${_OBJDUMP}" --all-headers "${f}" | grep -a -E -o '^\s+[A-Z_]{4,}$' | sort
+        # Dump cfguard load configuration flags
+        if [ "${_CC}" = 'llvm' ]; then  # binutils readelf (as of v2.40) does not recognize this option
+          # CF_FUNCTION_TABLE_PRESENT, CF_INSTRUMENTED, CF_LONGJUMP_TABLE_PRESENT (optional)
+          "${_READELF}" --coff-load-config "${f}" | grep -a -E 'CF_[A-Z_]' | sort
+        fi
+      elif [ "${_OS}" = 'mac' ]; then
+        otool -L "${f}"
+      elif [ "${_OS}" = 'linux' ]; then
+        "${_READELF}" --file-header --dynamic "${f}"
+      fi
+    done
   done
 
   # Execute curl and compiled-in dependency code. This is not secure, but
@@ -67,7 +80,7 @@
   # `--version` output directly from the binary as strings, but curl creates
   # most of these strings dynamically at runtime, so this is not possible
   # (as of curl 7.83.1).
-  ${_WINE} "${_PP}"/bin/curl.exe --version | tee "curl-${_CPU}.txt"
+  ${_WINE} "${_PP}/bin/curl${BIN_EXT}" --version | tee "curl-${_CPU}.txt"
 
   # Create package
 
@@ -95,23 +108,27 @@
       fi
     done
   )
-  cp -f -p "${_PP}"/include/curl/*.h "${_DST}/include/curl/"
-  cp -f -p "${_PP}"/bin/*.exe        "${_DST}/bin/"
-  cp -f -p "${_PP}"/bin/*.dll        "${_DST}/bin/"
-  cp -f -p "${_PP}"/bin/*.def        "${_DST}/bin/"
-  cp -f -p "${_PP}"/lib/*.a          "${_DST}/lib/"
-  cp -f -p docs/*.md                 "${_DST}/docs/"
-  cp -f -p CHANGES                   "${_DST}/CHANGES.txt"
-  cp -f -p COPYING                   "${_DST}/COPYING.txt"
-  cp -f -p README                    "${_DST}/README.txt"
-  cp -f -p RELEASE-NOTES             "${_DST}/RELEASE-NOTES.txt"
+  cp -f -p "${_PP}"/include/curl/*.h         "${_DST}/include/curl/"
+  cp -f -p "${_PP}/bin/curl${BIN_EXT}"       "${_DST}/bin/"
+  cp -f -a "${_PP}/${DYN_DIR}"/*"${DYN_EXT}" "${_DST}/${DYN_DIR}/"
+  cp -f -p "${_PP}"/lib/*.a                  "${_DST}/lib/"
+  cp -f -p docs/*.md                         "${_DST}/docs/"
+  cp -f -p CHANGES                           "${_DST}/CHANGES.txt"
+  cp -f -p COPYING                           "${_DST}/COPYING.txt"
+  cp -f -p README                            "${_DST}/README.txt"
+  cp -f -p RELEASE-NOTES                     "${_DST}/RELEASE-NOTES.txt"
 
-  if [ -d ../cacert ]; then
-    cp -f -p scripts/mk-ca-bundle.pl   "${_DST}/"
+  if [ "${_OS}" = 'win' ]; then
+    cp -f -p "${_PP}"/bin/*.def                "${_DST}/bin/"
   fi
 
   if [ "${CW_MAP}" = '1' ] && [ "${_OS}" != 'mac' ]; then
-    cp -f -p "${_PP}"/bin/*.map        "${_DST}/bin/"
+    cp -f -p "${_PP}"/bin/curl.map             "${_DST}/bin/"
+    cp -f -p "${_PP}/${DYN_DIR}"/*.map         "${_DST}/${DYN_DIR}/"
+  fi
+
+  if [ -d ../cacert ]; then
+    cp -f -p scripts/mk-ca-bundle.pl           "${_DST}/"
   fi
 
   ../_pkg.sh "$(pwd)/${_ref}"

_build.sh
@@ -229,6 +229,23 @@ else
   _CRT='sys'
 fi
 
+export DYN_DIR
+export DYN_EXT
+export BIN_EXT
+if [ "${_OS}" = 'win' ]; then
+  DYN_DIR='bin'
+  DYN_EXT='.dll'
+  BIN_EXT='.exe'
+elif [ "${_OS}" = 'mac' ]; then
+  DYN_DIR='lib'
+  DYN_EXT='.dylib'
+  BIN_EXT=''
+elif [ "${_OS}" = 'linux' ]; then
+  DYN_DIR='lib'
+  DYN_EXT='.so'
+  BIN_EXT=''
+fi
+
 if [ -z "${CW_MAP:-}" ]; then
   export CW_MAP='0'
   [ "${_BRANCH#*main*}" = "${_BRANCH}" ] && CW_MAP='1'

_pkg.sh
@@ -72,7 +72,7 @@ create_pkg() {
       win) find "${_BAS}" -exec attrib +A -R '{}' \;
     esac
 
-    find "${_BAS}" -type f | sort > "${_FLS}"
+    find "${_BAS}" -type f -o -type l | sort > "${_FLS}"
 
     rm -f "${_cdo}/${_pkg}"
     case "${arch_ext}" in
@@ -115,7 +115,9 @@ if [ "${_NAM}" != "${_UNIPKG}" ]; then
   fi
 else
   create_pkg "$1" '.tar.xz'
-  create_pkg "$1" '.zip'
+  if [ "${_OS}" = 'win' ]; then
+    create_pkg "$1" '.zip'
+  fi
 fi
 
 rm -r -f "${_DST:?}"

curl-autotools.sh
@@ -370,7 +370,7 @@ _VER="$1"
 
     if [ "${CW_MAP}" = '1' ] && [ "${_OS}" != 'mac' ]; then
       if [ "${pass}" = 'shared' ]; then
-        cp -p "${_BLDDIR}-${pass}/lib/${_MAP_NAME}" "${_PP}"/bin/
+        cp -p "${_BLDDIR}-${pass}/lib/${_MAP_NAME}" "${_PP}/${DYN_DIR}/"
       else
         cp -p "${_BLDDIR}-${pass}/src/${_MAP_NAME}" "${_PP}"/bin/
       fi

curl-cmake.sh
@@ -324,7 +324,7 @@ _VER="$1"
   fi
 
   if [ "${CW_MAP}" = '1' ] && [ "${_OS}" != 'mac' ]; then
-    cp -p "${_BLDDIR}/lib/${_MAP_NAME_LIB}" "${_PP}"/bin/
+    cp -p "${_BLDDIR}/lib/${_MAP_NAME_LIB}" "${_PP}/${DYN_DIR}/"
     cp -p "${_BLDDIR}/src/${_MAP_NAME_BIN}" "${_PP}"/bin/
   fi
 

curl-gnumake.sh
@@ -270,7 +270,7 @@ _VER="$1"
 
   if [ "${CW_MAP}" = '1' ]; then
     cp -f -p ./src/*.map "${_PP}/bin/"
-    cp -f -p ./lib/*.map "${_PP}/bin/"
+    cp -f -p ./lib/*.map "${_PP}/${DYN_DIR}/"
   fi
 
   . ../curl-pkg.sh

libressl-pkg.sh
@@ -41,7 +41,7 @@
 
   # shellcheck disable=SC2043
   for bin in \
-    "${_PP}"/bin/openssl.exe \
+    "${_PP}/bin/openssl${BIN_EXT}" \
   ; do
     if [ -f "${bin}" ]; then
       file "${bin}"

commit 1d090311a2ed8e362bb3e867e6c15f0c274afa38

fix `.def` file timestamp regression [ci skip]
Regression from bcd0261b0d2d1ce5b1c403df9f25f68698fe1de4

curl-pkg.sh
@@ -29,6 +29,10 @@
 
   touch -c -r "${_ref}" "${_PP}"/lib/*.a
 
+  if [ "${_OS}" = 'win' ]; then
+    touch -c -r "${_ref}" "${_PP}"/bin/*.def
+  fi
+
   # Process map files
 
   if [ "${CW_MAP}" = '1' ]; then