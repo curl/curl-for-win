curl/curl-for-win

commit 2c7fd679f958c4d4312b55980234b5801f3cb884

curl-pkg.sh: verify project root path leak [ci skip]
Verify if our project root path ends up in the curl executable. Dump
hits and abort if there are any.

To help prevent leaking non-reproducible local data into the binaries.

curl-pkg.sh
@@ -87,6 +87,13 @@
   # (as of curl 7.83.1).
   ${_RUN_BIN} "${bin}" --version | tee "curl-${_CPU}.txt"
 
+  # Extra checks
+
+  if strings "${bin}" | grep -a -F 'curl-for-win'; then
+    echo "! Error: Our project root path is leaking into the binary: '${bin}'"
+    exit 1
+  fi
+
   # Create package
 
   _OUT="${_NAM}-${_VER}${_REVSUFFIX}${_PKGSUFFIX}"

commit 1b9ef35286cd7b5b835a2e674316677ae7905051

curl-pkg.sh: move leak check before code signing [ci skip]
Regression from 2c7fd679f958c4d4312b55980234b5801f3cb884

To avoid the leak check picking up the string in the code signature:
```
+ strings win-x64-ucrt/usr/bin/curl.exe
+ grep -a -F curl-for-win
curl-for-win Root CA 20210
curl-for-win Root CA 20210
curl-for-win Root CA 20210
curl-for-win Root CA 2021
! Error: Our project root leaking into the binary: 'win-x64-ucrt/usr/bin/curl.exe'
+ echo ! Error: Our project root leaking into the binary: 'win-x64-ucrt/usr/bin/curl.exe'
+ exit 1
```
Ref: https://ci.appveyor.com/project/curlorg/curl-for-win/builds/48435383#L10281

curl-pkg.sh
@@ -16,6 +16,15 @@
     *)       TZ=UTC stat -c '%n: %y' "${_ref}";;
   esac
 
+  bin="${_PP}/bin/curl${BIN_EXT}"
+
+  # Extra checks (do this before code signing)
+
+  if strings "${bin}" | grep -a -F 'curl-for-win'; then
+    echo "! Error: Our project root path is leaking into the binary: '${bin}'"
+    exit 1
+  fi
+
   # Process libcurl static library
 
   # shellcheck disable=SC2086
@@ -41,8 +50,6 @@
   fi
 
   # Process curl tool and libcurl shared library
-
-  bin="${_PP}/bin/curl${BIN_EXT}"
 
   for filetype in 'exe' 'dyn'; do
     {
@@ -86,13 +93,6 @@
   # most of these strings dynamically at runtime, so this is not possible
   # (as of curl 7.83.1).
   ${_RUN_BIN} "${bin}" --version | tee "curl-${_CPU}.txt"
-
-  # Extra checks
-
-  if strings "${bin}" | grep -a -F 'curl-for-win'; then
-    echo "! Error: Our project root path is leaking into the binary: '${bin}'"
-    exit 1
-  fi
 
   # Create package
 