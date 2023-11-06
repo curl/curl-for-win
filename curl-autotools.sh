45198555e7cd313a0ad4d9ba1727dfbf3cb6a693
Viktor Szakats <commit@vsz.me>
2023-11-06 10:49:21 +0000

    curl-autotools.sh: limit fix to 8.4.0 after upstream fix [ci skip]
    Fixed upstream by:
    https://github.com/curl/curl/pull/12274
    https://github.com/curl/curl/commit/7925ba431b9a099daee1fa21d36c21887f787ad5

--- curl-autotools.sh
+++ curl-autotools.sh
@@ -158,14 +158,15 @@ _VER="$1"
         if [ "${_OS}" = 'win' ]; then
           if [ "${_OPENSSL}" = 'libressl' ]; then
             CPPFLAGS="${CPPFLAGS} -DLIBRESSL_DISABLE_OVERRIDE_WINCRYPT_DEFINES_WARNING"
-            # Workaround for accidentally detecting 'arc4random' inside LibreSSL (as of
-            # v3.8.2) then failing to use it due to missing the necessary LibreSSL header,
-            # then using a non-C89 type in curl's local replacement declaration:
-            #   ../../lib/rand.c:37:1: error: unknown type name 'uint32_t'
-            #      37 | uint32_t arc4random(void);
-            #         | ^
-            # Issue: https://github.com/curl/curl/issues/12257
-            export ac_cv_func_arc4random='no'
+            if [ "${CURL_VER_}" = '8.4.0' ]; then
+              # Workaround for accidentally detecting 'arc4random' inside LibreSSL (as of
+              # v3.8.2) then failing to use it due to missing the necessary LibreSSL header,
+              # then using a non-C89 type in curl's local replacement declaration:
+              #   ../../lib/rand.c:37:1: error: unknown type name 'uint32_t'
+              #      37 | uint32_t arc4random(void);
+              #         | ^
+              export ac_cv_func_arc4random='no'
+            fi
           fi
           LIBS="${LIBS} -lbcrypt"  # for auto-detection
         fi
