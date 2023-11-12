curl/curl-for-win

commit 92a6fc1d4921516d11afa0379759a792f9983151

cacert: turn curl-ca-bundle.crt into a versioned dependency [ci skip]
- fix bumper to compare dates too.
- add cacert to the list of versioned downloads.
- add special extract logic for cacert (and skip local patch option.)
- keep remote time and save xattr for downloads (to keep cacert.pem's
  original timestamp.)
- fix packager to work without trees without a lib subdir.
- add online link to the cacert license. (Adding the license itself
  would be better, but that would need yet another versioned dependency,
  as this isn't included in the cacert download. Another option would be
  hosting it.)
- migrate cacert download from curl-pkg.sh to _dl.sh. This also adds
  hash verification.
- migrate cacert packaging from curl-pkg.sh into its own script,
  cacert.sh.
- this also means the ca-bundle will appear in the dependency list,
  along with the version.

Undoubtedly more complex than the few lines of script used originally.
Will also need more maintenance. But, now we version everything, and
we no longer download anything in the middle of the build process.

_dl.sh
@@ -37,6 +37,14 @@ cat <<EOF
     "keys": "27EDEAF22F3ABCEB50DB9A125CC908FDB71E12C2"
   },
   {
+    "name": "cacert",
+    "descending": true,
+    "url": "https://curl.se/ca/cacert-{ver}.pem",
+    "sha": ".sha256",
+    "ref_url": "https://curl.se/docs/caextract.html",
+    "ref_mask": "[0-9]{4}-[0-9]{2}-[0-9]{2}"
+  },
+  {
     "name": "gsasl",
     "url": "https://ftp.gnu.org/gnu/gsasl/gsasl-{ver}.tar.gz",
     "sig": ".sig",
@@ -192,6 +200,7 @@ EOF
 my_curl() {
   # >&2 echo "my_curl|$*|"
   curl --disable --user-agent '' --fail --silent --show-error --globoff \
+    --remote-time --xattr \
     --connect-timeout 15 --max-time 60 --retry 3 --max-redirs 10 "$@"
 }
 
@@ -309,7 +318,7 @@ check_update() {
       fi
     else
       newvern="$(printf '%s' "${newver}" | to8digit)"
-      if [ "${newvern}" -gt "${ourvern}" ]; then
+      if [[ "${newvern}" > "${ourvern}" ]]; then
         printf '%s' "${newver}"
       fi
     fi
@@ -528,9 +537,14 @@ live_xt() {
     hash="$(openssl dgst -sha256 pkg.bin)"
     echo "${hash}"
     echo "${hash}" | grep -q -a -F -- "${2:-}" || exit 1
-    rm -r -f "${pkg}"; mkdir "${pkg}"; tar --strip-components "${3:-1}" -xf pkg.bin -C "${pkg}"
+    rm -r -f "${pkg}"; mkdir "${pkg}"
+    if [ "${pkg}" = 'cacert' ]; then
+      mv pkg.bin "${pkg}/${_CACERT}"
+    else
+      tar --strip-components "${3:-1}" -xf pkg.bin -C "${pkg}"
+      [ -f "${pkg}${_patsuf}.patch" ] && dos2unix < "${pkg}${_patsuf}.patch" | patch -N -p1 -d "${pkg}"
+    fi
     rm -f pkg.bin pkg.sig
-    [ -f "${pkg}${_patsuf}.patch" ] && dos2unix < "${pkg}${_patsuf}.patch" | patch -N -p1 -d "${pkg}"
     [ -f "__${pkg}.url" ] && mv "__${pkg}.url" "${pkg}/__url__.txt"
   fi
   return 0
@@ -692,6 +706,8 @@ if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
   live_xt gsasl "${GSASL_HASH}"
 fi
 
+need_cacert=0
+
 if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
    [ "${_BRANCH#*nano*}" = "${_BRANCH}" ] && \
    [ "${_BRANCH#*micro*}" = "${_BRANCH}" ] && \
@@ -714,6 +730,7 @@ if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
     live_dl openssl-quic "${OPENSSL_QUIC_VER_}"
     live_xt openssl-quic "${OPENSSL_QUIC_HASH}"
   fi
+  need_cacert=1
 fi
 
 if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
@@ -722,6 +739,7 @@ if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
   if [ "${_BRANCH#*wolfssh*}" != "${_BRANCH}" ]; then
     live_dl wolfssh "${WOLFSSH_VER_}"
     live_xt wolfssh "${WOLFSSH_HASH}"
+    need_cacert=1
   elif [ "${_BRANCH#*libssh*}" != "${_BRANCH}" ]; then
     # shellcheck disable=SC2153
     live_dl libssh "${LIBSSH_VER_}"
@@ -739,6 +757,11 @@ if [ "${_BRANCH#*pico*}" = "${_BRANCH}" ] && \
     fi
     live_xt libssh2 "${LIBSSH2_HASH}"
   fi
+fi
+
+if [ "${need_cacert}" = '1' ]; then
+  live_dl cacert "${CACERT_VER_}"
+  live_xt cacert "${CACERT_HASH}"
 fi
 
 if [ "${_BRANCH#*dev*}" != "${_BRANCH}" ]; then

_build.sh
@@ -165,6 +165,8 @@ else
   _TAR="${_URL_BASE}/archive/refs/heads/${_BRANCH}.tar.gz"
 fi
 
+export _CACERT='cacert.pem'
+
 [ -n "${CW_CCSUFFIX:-}" ] || CW_CCSUFFIX=''
 
 export _CC='clang'
@@ -754,6 +756,7 @@ build_single_target() {
   bld wolfssh           "${WOLFSSH_VER_}"
   bld libssh             "${LIBSSH_VER_}"
   bld libssh2           "${LIBSSH2_VER_}"
+  bld cacert             "${CACERT_VER_}"
   bld curl                 "${CURL_VER_}"
 
   # Unified, per-target package: Build

_bumper.sh
@@ -46,11 +46,3 @@ image="$(curl --disable --user-agent '' --silent --fail --show-error \
   | grep -a -o -E 'Ubuntu[0-9]{4}' | sort | tail -1)"
 
 echo; echo "image: ${image}"
-
-# Find out the latest CA certificate bundle release
-
-image="$(curl --disable --user-agent '' --silent --fail --show-error \
-  'https://curl.se/docs/caextract.html' \
-  | grep -a -o -E '[0-9]{4}-[0-9]{2}-[0-9]{2}' | sort | tail -1)"
-
-echo; echo "CACERT_VER='${image}'"

_pkg.sh
@@ -31,7 +31,7 @@ if [ "${_NAM}" != "${_UNIPKG}" ]; then
       mkdir -p "${unipkg}/dep/${_NAM}"
       rsync --archive --update "${_DST}/docs" "${unipkg}/dep/${_NAM}"
     fi
-    rsync --archive --update "${_DST}/lib" "${unipkg}"
+    [ -d "${_DST}/lib" ]      && rsync --archive --update "${_DST}/lib" "${unipkg}"
     if [ "${_NAM}" = 'curl' ]; then
       cp -f -p "${_DST}"/*.* "${unipkg}"
       rsync --archive --update "${_DST}/docs" "${unipkg}"

_versions.sh
@@ -5,10 +5,10 @@
 
 # NOTE: Bump nghttp3 and ngtcp2 together with curl.
 
-export CACERT_VER=''  # '2022-10-11'
-
 export CURL_VER_='7.85.0'
 export CURL_HASH=88b54a6d4b9a48cb4d873c7056dcba997ddd5b7be5a2d537a4acb55c20b04be6
+export CACERT_VER_='2022-10-11'
+export CACERT_HASH=2cff03f9efdaf52626bd1b451d700605dc1ea000c5da56bd0fc59f8f43071040
 export BROTLI_VER_='1.0.9'
 export BROTLI_HASH=f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46
 export CARES_VER_='1.17.2'

cacert.sh
@@ -1,0 +1,42 @@
+#!/bin/sh
+
+# Copyright 2014-present Viktor Szakats. See LICENSE.md
+# SPDX-License-Identifier: MIT
+
+# shellcheck disable=SC3040
+set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
+
+export _NAM _VER _OUT _BAS _DST
+
+_NAM="$(basename "$0" | cut -f 1 -d '.' | sed 's/-m32//')"
+_VER="$1"
+
+(
+  cd "${_NAM}" || exit 0
+
+  # Make steps for determinism
+
+  readonly _ref="${_CACERT}"
+
+  # Create package
+
+  _OUT="${_NAM}-${_VER}${_REVSUFFIX}${_PKGSUFFIX}"
+  _BAS="${_NAM}-${_VER}${_PKGSUFFIX}"
+  _DST="$(mktemp -d)/${_BAS}"
+
+  mkdir -p "${_DST}/bin"
+
+  # To avoid depending on yet another unversioned download (or vendoring
+  # the license), add link to it instead:
+  _fn="${_DST}/LICENSE.url"
+  cat <<EOF > "${_fn}"
+[InternetShortcut]
+URL=https://www.mozilla.org/media/MPL/2.0/index.txt
+EOF
+  unix2dos --quiet --keepdate "${_fn}"
+  touch -c -r "${_ref}" "${_fn}"
+
+  cp -f -p "${_CACERT}" "${_DST}/bin/curl-ca-bundle.crt"
+
+  ../_pkg.sh "$(pwd)/${_ref}"
+)

curl-pkg.sh
@@ -6,27 +6,6 @@
 # curl pre-packaging, shared between build systems.
 
 {
-  if [ -n "${_OPENSSL}" ] || [ -d ../wolfssl ]; then
-    # Download CA bundle
-    calocal='../ca-bundle.crt'
-    if [ ! -f "${calocal}" ]; then
-
-      if [ -n "${CACERT_VER}" ]; then
-        caremote="cacert-${CACERT_VER}.pem"
-      else
-         # CAVEAT: Unversioned build-time download. It can break reproducibility.
-        caremote='cacert.pem'
-      fi
-
-      curl --disable --user-agent '' --fail --silent --show-error \
-        --remote-time --xattr \
-        --output "${calocal}" \
-        "https://curl.se/ca/${caremote}"
-    fi
-
-    openssl dgst -sha256 "${calocal}"
-  fi
-
   # Make steps for determinism
 
   readonly _ref='CHANGES'
@@ -111,9 +90,8 @@
   cp -f -p README                    "${_DST}/README.txt"
   cp -f -p RELEASE-NOTES             "${_DST}/RELEASE-NOTES.txt"
 
-  if [ -n "${_OPENSSL}" ] || [ -d ../wolfssl ]; then
+  if [ -d ../cacert ]; then
     cp -f -p scripts/mk-ca-bundle.pl   "${_DST}/"
-    cp -f -p "${calocal}"              "${_DST}/bin/curl-ca-bundle.crt"
   fi
 
   if [ "${CW_MAP}" = '1' ]; then

commit a604c34878b7ceab7d6e8069f3bafe960d87862c

_dl.sh: fix to include cacert with wolfssl not wolfssh [ci skip]
Regression from 92a6fc1d4921516d11afa0379759a792f9983151

_dl.sh
@@ -714,17 +714,18 @@ if [[ ! "${_CONFIG}" =~ (zero|bldtst|pico|nano) ]]; then
   fi
 fi
 
+need_cacert=0
+
 if [[ "${_CONFIG}" = *'big'* ]]; then
   _DEPS+=' libidn2 libunistring libiconv libpsl gsasl'
 fi
 if [[ "${_CONFIG}" = *'wolfssl'* ]]; then
   _DEPS+=' wolfssl'
+  need_cacert=1
 fi
 if [[ "${_CONFIG}" = *'mbedtls'* ]]; then
   _DEPS+=' mbedtls'
 fi
-
-need_cacert=0
 
 if [[ ! "${_CONFIG}" =~ (zero|bldtst) ]]; then
   if [ "${_OS}" != 'win' ] || \
@@ -748,7 +749,6 @@ fi
 if [[ ! "${_CONFIG}" =~ (zero|bldtst|pico|nano|micro) ]]; then
   if   [[ "${_CONFIG}" = *'wolfssh'* ]]; then
     _DEPS+=' wolfssh'
-    need_cacert=1
   elif [[ "${_CONFIG}" = *'libssh'* ]]; then
     _DEPS+=' libssh1'
   else