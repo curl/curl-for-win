curl/curl-for-win

commit 772ed975a9577b24d3a7df75754ace46c89714e5

_build.sh: fix to use `llvm-ar` for mac with llvm [ci skip]
Instead of the Xcode `ar` which doesn't handle e.g. ThinLTO objects.

Fixes missing symbols when linking libs to curl:
```
ld64.lld: error: undefined symbol: OpenSSL_add_all_digests
>>> referenced by /tmp/lto.tmp:(symbol curl_easy_init+0xd6)
>>> referenced by /tmp/lto.tmp:(symbol curl_global_init+0xd4)
>>> referenced by /tmp/lto.tmp:(symbol curl_global_init_mem+0xeb)
>>> referenced 13 more times
```
Ref: https://github.com/curl/curl-for-win/actions/runs/7131041425/job/19418771861#step:3:11956

_build.sh
@@ -1340,12 +1340,12 @@ build_single_target() {
     chmod +x "${AR_NORMALIZE}"
   fi
 
-  if [ "${_OS}" = 'win' ] && [ "${_HOST}" = 'mac' ]; then
+  if [ "${_HOST}" = 'mac' ] && [ "${_TOOLCHAIN}" != 'llvm-apple' ]; then
     if [ "${_TOOLCHAIN}" = 'llvm-mingw' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${CW_LLVM_MINGW_PATH}/bin/${AR}"
-    elif [ "${_CC}" = 'llvm' ]; then
+    elif [ "${_OS}" = 'mac' ] && [ "${_CC}" = 'llvm' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${_MAC_LLVM_PATH}/${AR}"
-    else
+    elif [ "${_OS}" = 'win' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${_SYSROOT}/bin/${AR}"
     fi
   fi

commit b5c4f60157d7bd9d84b426e9d99fe9265f4bc3be

_build.sh: fix mac -> Homebrew llvm regression [ci skip]
Regression from 772ed975a9577b24d3a7df75754ace46c89714e5

_build.sh
@@ -1365,7 +1365,7 @@ build_single_target() {
   if [ "${_HOST}" = 'mac' ] && [ "${_TOOLCHAIN}" != 'llvm-apple' ]; then
     if [ "${_TOOLCHAIN}" = 'llvm-mingw' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${CW_LLVM_MINGW_PATH}/bin/${AR}"
-    elif [ "${_OS}" = 'mac' ] && [ "${_CC}" = 'llvm' ]; then
+    elif [ "${_CC}" = 'llvm' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${_MAC_LLVM_PATH}/${AR}"
     elif [ "${_OS}" = 'win' ]; then
       _CMAKE_GLOBAL+=" -DCMAKE_AR=${_SYSROOT}/bin/${AR}"