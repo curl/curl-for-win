f32347fcfa4df1f2d7ae55fe53a2c1b0160ea0b7
Viktor Szakats <commit@vsz.me>
2023-12-17 17:11:34 +0000

    _dl.sh: minor option format tidy-up [ci skip]

--- _dl.sh
+++ _dl.sh
@@ -569,7 +569,7 @@ live_xt() {
     if [ "${pkg}" = 'cacert' ]; then
       mv pkg.bin "${pkg}/${_CACERT}"
     else
-      tar --strip-components "${3:-1}" -xf pkg.bin --directory="${pkg}"
+      tar --strip-components="${3:-1}" -xf pkg.bin --directory="${pkg}"
       [ -f "${pkg}${_patsuf}.patch" ] && patch --forward --strip=1 --directory="${pkg}" < "${pkg}${_patsuf}.patch"
     fi
     rm -f pkg.bin pkg.sig
