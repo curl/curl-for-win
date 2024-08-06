af16869c8b87f53ee9ebb0f29aec13efb4dc6afe
Viktor Szakats <commit@vsz.me>
2024-08-06 13:30:13 +0200

    curl.sh: CAcert support expected in 8.10.0 [ci skip]
    https://github.com/curl/curl/commit/8a3740bc8e558b9a9d4a652b74cf27a0961d7010
    https://github.com/curl/curl/pull/14059

--- curl.sh
+++ curl.sh
@@ -375,7 +375,7 @@ _VER="$1"
   # Pending https://github.com/curl/curl/pull/14059
   if [[ "${_CONFIG}" != *'nocurltool'* ]] && \
      [[ "${_DEPS}" = *'cacert'* ]] && \
-     grep -a -q 'CURL_CA_EMBED' './CMakeLists.txt'; then  # Expected in 8.10.0
+     [ "${CURL_VER_}" != '8.9.1' ]; then
     options+=" -DCURL_CA_EMBED=${_TOP}/cacert/${_CACERT}"
   fi
 
