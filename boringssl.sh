80961225cf0c05d0ac5ae0b2b71881c3ffb58c68
Viktor Szakats <commit@vsz.me>
2024-10-06 18:46:26 +0200

    boringssl.sh: add macOS requirement [ci skip]

--- boringssl.sh
+++ boringssl.sh
@@ -3,6 +3,8 @@
 # Copyright (C) Viktor Szakats. See LICENSE.md
 # SPDX-License-Identifier: MIT
 
+# Requires macOS 10.13
+
 # FIXME (upstream):
 # - x64 mingw-w64 pthread ucrt static linking bug -> requires llvm-mingw
 # - as of 4fe29ebc hacks are needed to avoid build issues. grep for the hash
