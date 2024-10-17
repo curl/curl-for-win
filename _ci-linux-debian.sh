b0109bba988d330a374ae743b1f9f65ba28035f2
Viktor Szakats <commit@vsz.me>
2024-10-17 18:51:33 +0200

    fixup

--- _ci-linux-debian.sh
+++ _ci-linux-debian.sh
@@ -122,7 +122,7 @@ if [ -n "${dl}" ]; then
   find -L 'my-pkg/usr/lib/clang' | sort
 fi
 
-find /usr/riscv64-linux-gnu/lib
-find /usr/lib/gcc-cross/riscv64-linux-gnu
+find /usr/riscv64-linux-gnu/lib || true
+find /usr/lib/gcc-cross/riscv64-linux-gnu || true
 
 ./_build.sh
