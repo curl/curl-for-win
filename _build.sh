0426de747a94721d2b4430c784df8096443c0e9f
Viktor Szakats <commit@vsz.me>
2024-11-07 06:17:18 +0100

    _build.sh: blind try for debian musl missing `<memory>` C++ header

--- _build.sh
+++ _build.sh
@@ -1117,18 +1117,22 @@ build_single_target() {
         _CXXFLAGS_GLOBAL+=" -I${tmp}/include/c++/${_TRIPLET}"
         _CXXFLAGS_GLOBAL+=" -I${tmp}/include/c++/backward"
       fi
-    elif [ "${_HOST}" = 'linux' ] && [ "${_OS}" = 'linux' ] && [ "${unamem}" != "${_machine}" ] && [ "${_CRT}" != 'musl' ]; then
-      _CFLAGS_GLOBAL+=" -isystem /usr/${_TRIPLETSH}/include"
-      _LDFLAGS_GLOBAL+=" -L/usr/${_TRIPLETSH}/lib"
-      if [ "${_CCRT}" = 'libgcc' ]; then
-        # https://packages.debian.org/testing/all/libgcc-13-dev-arm64-cross/filelist
-        # /usr/lib/gcc-cross/aarch64-linux-gnu/13/
-        tmp="$(find "/usr/lib/gcc-cross/${_TRIPLETSH}" -mindepth 1 -maxdepth 1 -type d | head -n 1 || true)"
-        if [ -z "${tmp}" ]; then
-          >&2 echo '! Error: Failed to detect gcc-cross env root.'
-          exit 1
+    elif [ "${_HOST}" = 'linux' ] && [ "${_OS}" = 'linux' ] && [ "${unamem}" != "${_machine}" ] && [ "${_CC}" = 'llvm' ]; then
+      if [ "${_CRT}" != 'musl' ]; then
+        _CFLAGS_GLOBAL+=" -isystem /usr/${_TRIPLETSH}/include"
+        _LDFLAGS_GLOBAL+=" -L/usr/${_TRIPLETSH}/lib"
+        if [ "${_CCRT}" = 'libgcc' ]; then
+          # https://packages.debian.org/testing/all/libgcc-13-dev-arm64-cross/filelist
+          # /usr/lib/gcc-cross/aarch64-linux-gnu/13/
+          tmp="$(find "/usr/lib/gcc-cross/${_TRIPLETSH}" -mindepth 1 -maxdepth 1 -type d | head -n 1 || true)"
+          if [ -z "${tmp}" ]; then
+            >&2 echo '! Error: Failed to detect gcc-cross env root.'
+            exit 1
+          fi
+          _LDFLAGS_GLOBAL+=" -L${tmp}"
         fi
-        _LDFLAGS_GLOBAL+=" -L${tmp}"
+      fi
+      if [ "${_DISTRO}" = 'debian' ]; then
         # https://packages.debian.org/testing/all/libstdc++-13-dev-arm64-cross/filelist
         # /usr/aarch64-linux-gnu/include/c++/13/
         tmp="$(find "/usr/${_TRIPLETSH}/include/c++" -mindepth 1 -maxdepth 1 -type d | head -n 1 || true)"
