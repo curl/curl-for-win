23337be6cf1b7b177a65e447738f35df85995ce5
Viktor Szakats <commit@vsz.me>
2024-11-14 18:22:58 +0100

    cacert.sh: stop distributing `curl-ca-bundle.crt` [ci skip]
    Starting with curl 8.12.0.
    
    The CA bundle is embedded into the curl binary since 8.10.0_1.
    
    If you need it as a disk file (e.g. for libcurl), you can extract it
    using this command:
    ```
    curl.exe --dump-ca-embed
    ```

--- cacert.sh
+++ cacert.sh
@@ -53,8 +53,9 @@ EOF
   fi
   touch -c -r "${_ref}" "${_fn}"
 
-  # TODO: Omit external CA bundle from distro?
-  cp -f -p "${_CACERT}" "${_DST}/bin/curl-ca-bundle.crt"
+  if [ "${CURL_VER_}" != '8.12.0' ]; then
+    cp -f -p "${_CACERT}" "${_DST}/bin/curl-ca-bundle.crt"
+  fi
 
   ../_pkg.sh "$(pwd)/${_ref}"
 )
