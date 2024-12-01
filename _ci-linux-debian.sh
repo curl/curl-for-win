curl/curl-for-win

commit fc001e2e5f34042417f9707ea29253ac21129b5e

_ci-linux-debian.sh: use `--no-install-suggests --no-install-recommends` [ci skip]
- install ca-certificates manually.
- install libclang-rt-19-dev manually (and any 16+ version)
  Necessary for debian-testing, not necessary for bookworm.
  https://github.com/curl/curl-for-win/actions/runs/11470272182/job/31919128418

It's not obvious on the job run times, but these two options greatly reduce
the amount of packages installed.

before: https://github.com/curl/curl-for-win/actions/runs/11449835781/job/31856166788
after:  https://github.com/curl/curl-for-win/actions/runs/11470394944/job/31919485534

linux-glibc-debian-bookworm-llvm:
0 upgraded, 215 newly installed, 0 to remove and 0 not upgraded.
Need to get 316 MB of archives.
After this operation, 1639 MB of additional disk space will be used.

0 upgraded, 134 newly installed, 0 to remove and 0 not upgraded.
Need to get 210 MB of archives.
After this operation, 1084 MB of additional disk space will be used.

linux-glibc-debian-bookworm-gcc:
0 upgraded, 215 newly installed, 0 to remove and 0 not upgraded.
Need to get 258 MB of archives.
After this operation, 1140 MB of additional disk space will be used.

0 upgraded, 135 newly installed, 0 to remove and 0 not upgraded.
Need to get 226 MB of archives.
After this operation, 1038 MB of additional disk space will be used.

linux-musl-debian-testing-gcc:
3 upgraded, 239 newly installed, 0 to remove and 5 not upgraded.
Need to get 365 MB of archives.
After this operation, 1687 MB of additional disk space will be used.

1 upgraded, 172 newly installed, 0 to remove and 8 not upgraded.
Need to get 342 MB of archives.
After this operation, 1610 MB of additional disk space will be used.

linux-musl-debian-testing-llvm:
3 upgraded, 271 newly installed, 0 to remove and 5 not upgraded.
Need to get 458 MB of archives.
After this operation, 2320 MB of additional disk space will be used.

1 upgraded, 179 newly installed, 0 to remove and 8 not upgraded.
Need to get 369 MB of archives.

_ci-linux-debian.sh
@@ -16,6 +16,9 @@ dl=''
 if [[ "${CW_CONFIG:-}" != *'gcc'* ]]; then
   [ -n "${CW_CCSUFFIX:-}" ] || export CW_CCSUFFIX='-19'
   extra+=" llvm${CW_CCSUFFIX} clang${CW_CCSUFFIX} lld${CW_CCSUFFIX}"
+  if [ "${CW_CCSUFFIX}" != '-15' ]; then
+    extra+=" libclang-rt${CW_CCSUFFIX}-dev"
+  fi
 fi
 
 [[ "${CW_CONFIG:-}" = *'boringssl'* ]] && extra+=' golang'
@@ -49,7 +52,7 @@ elif [[ "${CW_CONFIG:-}" = *'linux'* ]]; then
     [[ "${CW_CONFIG:-}" = *'r64'* ]] && extra+=" gcc${CW_GCCSUFFIX}-riscv64-linux-gnu g++${CW_GCCSUFFIX}-riscv64-linux-gnu"
   else
     # These packages do not install due to dependency requirements.
-    # We download unpack them manually as a workaround.
+    # We download and unpack them manually as a workaround.
     if [ "${CW_CCSUFFIX}" = '-15' ]; then
       # ./my-pkg/usr/lib/clang/15/lib
       # ./my-pkg/usr/lib/llvm-15/lib/clang/15.0.6/lib/linux/libclang_rt.builtins-aarch64.a
@@ -102,8 +105,8 @@ fi
 
 apt-get --option Dpkg::Use-Pty=0 --yes update
 # shellcheck disable=SC2086
-apt-get --option Dpkg::Use-Pty=0 --yes install \
-  curl git gpg gpg-agent rsync python3-pefile make cmake ninja-build \
+apt-get --option Dpkg::Use-Pty=0 --yes install --no-install-suggests --no-install-recommends \
+  curl ca-certificates git gpg gpg-agent rsync python3-pefile make cmake ninja-build \
   libssl-dev zlib1g-dev \
   zip xz-utils time jq secure-delete ${extra}
 

commit 781bee2bd3cd55c779264d0435728708192cccff

_ci-linux-debian.sh: re-add lost wine package [ci skip]
This in turn caused losing the -V summar from https://curl.se/windows.

Regression from fc001e2e5f34042417f9707ea29253ac21129b5e

_ci-linux-debian.sh
@@ -24,7 +24,7 @@ fi
 [[ "${CW_CONFIG:-}" = *'boringssl'* ]] && extra+=' golang'
 
 if [[ "${CW_CONFIG:-}" = *'win'* ]]; then
-  extra+=' mingw-w64 wine64'
+  extra+=' mingw-w64 wine64 wine'
   # https://tracker.debian.org/pkg/osslsigncode
   extra+=' osslsigncode'
   if [[ "${CW_CONFIG:-}" = *'boringssl'* ]] || [[ "${CW_CONFIG:-}" = *'awslc'* ]]; then