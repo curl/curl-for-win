eeb42e02e9a958bc4c619884185d70e02005838c
Viktor Szakats <commit@vsz.me>
2025-02-14 16:04:46 +0100

    prefer `CMAKE_INSTALL_PREFIX` over `--prefix`

--- ngtcp2.sh
+++ ngtcp2.sh
@@ -60,11 +60,12 @@ _VER="$1"
     -DENABLE_STATIC_LIB=ON \
     -DENABLE_SHARED_LIB=OFF \
     -DBUILD_TESTING=OFF \
+    -DCMAKE_INSTALL_PREFIX="${PWD}/${_PPS}" \
     -DCMAKE_C_FLAGS="${_CFLAGS_GLOBAL_CMAKE} ${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${CPPFLAGS} ${_LDFLAGS_GLOBAL} ${LDFLAGS} ${LIBS}" \
     -DCMAKE_CXX_FLAGS="${_CFLAGS_GLOBAL_CMAKE} ${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${CPPFLAGS} ${_LDFLAGS_GLOBAL} ${LDFLAGS} ${LIBS} ${_CXXFLAGS_GLOBAL} ${_LDFLAGS_CXX_GLOBAL}"
 
   cmake --build "${_BLDDIR}"
-  cmake --install "${_BLDDIR}" --prefix "${_PPS}"
+  cmake --install "${_BLDDIR}"
 
   # Delete .pc files
   rm -r -f "${_PPS}"/lib/pkgconfig
