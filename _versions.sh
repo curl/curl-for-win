8d8d8066f41c3265ad1755b20fb9665d912bae97
Viktor Szakats <commit@vsz.me>
2025-05-22 13:13:29 +0200

    debian:testing-20250520-slim [ci skip]

--- _versions.sh
+++ _versions.sh
@@ -3,7 +3,7 @@
 # Copyright (C) Viktor Szakats. See LICENSE.md
 # SPDX-License-Identifier: MIT
 
-export DOCKER_IMAGE='debian:testing-20250428-slim'
+export DOCKER_IMAGE='debian:testing-20250520-slim'
 export DOCKER_CONTENT_TRUST='1'
 
 export CURL_VER_='8.13.0'
