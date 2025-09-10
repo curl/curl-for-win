4c649221acae9b18c5b158550b5d10277b087089
Viktor Szakats <commit@vsz.me>
2025-09-11 01:52:21 +0200

    zstd: patch build to stop requiring C++ [ci skip]
    Apply upstream patches (not yet released) to add support:
    https://github.com/facebook/zstd/pull/4357
    https://github.com/facebook/zstd/commit/769723aee2540aaff8951ac432a1babed358aa71
    https://github.com/facebook/zstd/commit/472acf5d83e0f059c0d5938dea6556e21385a71c
    https://github.com/facebook/zstd/commit/9a6fe9a428751b82be76cdf010cf89aae64897ce

--- _ci-linux-debian.sh
+++ _ci-linux-debian.sh
@@ -25,7 +25,7 @@ fi
 [[ "${CW_CONFIG:-}" = *'boringssl'* ]] && extra+=' golang'
 
 if [[ "${CW_CONFIG:-}" = *'win'* ]]; then
-  # g++ necessary for stdc++ in aws-lc/boringssl/zstd
+  # g++ necessary for stdc++ in aws-lc/boringssl
   extra+=' gcc-mingw-w64-x86-64-win32 g++-mingw-w64-x86-64-win32 wine64 wine'
   # https://tracker.debian.org/pkg/osslsigncode
   extra+=' osslsigncode'
