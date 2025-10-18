c22bb2a7f5ffa96725282f3c265258a5c4abd5c0
Viktor Szakats <commit@vsz.me>
2025-10-18 12:10:12 +0200

    _ci-*.sh: install ssh client (w/o server) and only when necessary [ci skip]
    Before:
    ```
    5 upgraded, 150 newly installed, 0 to remove and 7 not upgraded.
    Need to get 197 MB of archives.
    After this operation, 917 MB of additional disk space will be used.
    ```
    https://github.com/curl/curl-for-win/actions/runs/18614179870/job/53076446551#step:3:207
    
    After:
    ```
    3 upgraded, 134 newly installed, 0 to remove and 9 not upgraded.
    Need to get 189 MB of archives.
    After this operation, 889 MB of additional disk space will be used.
    ```
    https://github.com/curl/curl-for-win/actions/runs/18614290279/job/53076711155#step:3:193

--- _ci-linux-debian.sh
+++ _ci-linux-debian.sh
@@ -31,6 +31,7 @@ if [[ "${CW_CONFIG:-}" = *'win'* ]]; then
     [[ "${CW_CONFIG:-}" = *'x86'* ]] && extra+=' g++-mingw-w64-i686-win32'
   fi
   extra+=' python3-pip python3-venv'
+  [ -n "${DEPLOY_GPG_PASS:+1}" ] && extra+=' openssh-client'
 elif [[ "${CW_CONFIG:-}" = *'linux'* ]]; then
   [ -n "${CW_GCCSUFFIX:-}" ] || CW_GCCSUFFIX='-14'
 
@@ -112,7 +113,7 @@ fi
 ${sudo} apt-get --option Dpkg::Use-Pty=0 --yes update
 # shellcheck disable=SC2086
 ${sudo} apt-get --option Dpkg::Use-Pty=0 --yes install --no-install-suggests --no-install-recommends \
-  curl ca-certificates git gpg gpg-agent patch ssh rsync python3 make cmake ninja-build \
+  curl ca-certificates git gpg gpg-agent patch rsync python3 make cmake ninja-build \
   zip xz-utils time jq secure-delete ${extra}
 
 if [ -n "${dl}" ]; then
