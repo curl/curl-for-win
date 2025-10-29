curl/curl-for-win

commit e7a1232f3478a85f1d8e57f75703421e2f958812

llvm-mingw 20250826 with LLVM 21.1.0 [ci skip]
https://github.com/mstorsjo/llvm-mingw/releases/tag/20250826

_versions.sh
@@ -53,11 +53,11 @@ export ZLIB_VER_='1.3.1'
 export ZLIB_HASH=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
 export ZSTD_VER_='1.5.7'
 export ZSTD_HASH=eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3
-export LLVM_MINGW_LINUX_AARCH64_VER_='20250709'
-export LLVM_MINGW_LINUX_AARCH64_HASH=7db379b2af59a2a45f002a32893c9461222939f4ddbb8bcef7d2b4a986761287
-export LLVM_MINGW_LINUX_X86_64_VER_='20250709'
-export LLVM_MINGW_LINUX_X86_64_HASH=60cafae6474c7411174cff1d4ba21a8e46cadbaeb05a1bace306add301628337
-export LLVM_MINGW_MAC_VER_='20250709'
-export LLVM_MINGW_MAC_HASH=e0707a0a91a363e9adc896475b4915cd68fe70bb4391eec1860343b2b58846f0
-export LLVM_MINGW_WIN_VER_='20250709'
-export LLVM_MINGW_WIN_HASH=82babcd6aae4dc3606e8e0471d816989c384b4bf86a139f184a8b4a1b2c2758d
+export LLVM_MINGW_LINUX_AARCH64_VER_='20250826'
+export LLVM_MINGW_LINUX_AARCH64_HASH=79fa6786e95a6552620a95d1dae047ad1a88c391ed90e3a22bff481d22476a63
+export LLVM_MINGW_LINUX_X86_64_VER_='20250826'
+export LLVM_MINGW_LINUX_X86_64_HASH=764306a271b75b01fd0e86873e28d081373c2b4eeb1efecab30e9455728740ca
+export LLVM_MINGW_MAC_VER_='20250826'
+export LLVM_MINGW_MAC_HASH=937e842bdd3a84da0f32bd6aa665156906c270126151209b1ec238b0f3b3ab78
+export LLVM_MINGW_WIN_VER_='20250826'
+export LLVM_MINGW_WIN_HASH=51c347bf5dead6c4383ff16fa9baee362dacceb70ecda9448d2289c8e158af61

commit 01e53fe9852a3c14c6c1bfeff9623b90f9980faa

llvm-mingw downgrade to 20.1.8 to fix broken Windows ARM64 binaries [ci skip]
llvm-mingw 21 creating broken LibreSSL ARM64 binaries, crashing with
errorlevel -1073740791 aka 0xC0000409 (STATUS_STACK_BUFFER_OVERRUN),
as tested with the current latest llvm-mingw 21.1.4. The crash has no
display, pop-up, it just quits with this error code. This error code
indicates a "fail fast exception", not an actual buffer overrun,
according to Raymond Chen:
https://devblogs.microsoft.com/oldnewthing/20190108-00/?p=100655

curl is crashing silently when touching SSL code.

It isn't LibreSSL-specific, and happens with OpenSSL 3.5.2 as well.

Disabling CFGuard by omitting `-mguard=cf` options makes the crash
disappear. If this causes just not catching an error, or disabling
a broken CFGuard, I can't tell.

Ref: https://gist.github.com/alvinhochun/a65e4177e2b34d551d7ecb02b55a4b0a

curl-for-win official binaries affected: 8.15.0_6 to 8.16.0_12

Running a basic curl transfer test would be nice to catch this early,
but running ARM64 binaries isn't possible under WINE.

Regression from e7a1232f3478a85f1d8e57f75703421e2f958812

_versions.sh
@@ -55,11 +55,11 @@ export ZLIB_VER_='1.3.1'
 export ZLIB_HASH=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
 export ZSTD_VER_='1.5.7'
 export ZSTD_HASH=eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3
-export LLVM_MINGW_LINUX_AARCH64_VER_='20251021'
-export LLVM_MINGW_LINUX_AARCH64_HASH=50fa37549333a68558eea03dcd919df48a6a8b19798147b863e3b921dfd69a85
-export LLVM_MINGW_LINUX_X86_64_VER_='20251021'
-export LLVM_MINGW_LINUX_X86_64_HASH=7b7f3acb6e735a9fabfaa3c8ff551e9937a5f2f298c2cd12796b357f7d1fb6a8
-export LLVM_MINGW_MAC_VER_='20251021'
-export LLVM_MINGW_MAC_HASH=5bb182b7439430988ae1c25533d5c5fd86d5b66a30f54c35474f58ee16e5a109
-export LLVM_MINGW_WIN_VER_='20251021'
-export LLVM_MINGW_WIN_HASH=0a0ce4733b54de8c4858ecfc2a08447de57728dccf9fbf7fcba3ce7e0d0c6af8
+export LLVM_MINGW_LINUX_AARCH64_VER_='20250709'
+export LLVM_MINGW_LINUX_AARCH64_HASH=7db379b2af59a2a45f002a32893c9461222939f4ddbb8bcef7d2b4a986761287
+export LLVM_MINGW_LINUX_X86_64_VER_='20250709'
+export LLVM_MINGW_LINUX_X86_64_HASH=60cafae6474c7411174cff1d4ba21a8e46cadbaeb05a1bace306add301628337
+export LLVM_MINGW_MAC_VER_='20250709'
+export LLVM_MINGW_MAC_HASH=e0707a0a91a363e9adc896475b4915cd68fe70bb4391eec1860343b2b58846f0
+export LLVM_MINGW_WIN_VER_='20250709'
+export LLVM_MINGW_WIN_HASH=82babcd6aae4dc3606e8e0471d816989c384b4bf86a139f184a8b4a1b2c2758d