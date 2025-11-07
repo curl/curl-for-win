a8cf5683c63ad3b56dc4abd1a27db7494dd8bf35
Viktor Szakats <commit@vsz.me>
2025-11-07 12:50:20 +0100

    curl.sh: optimized the batch [ci skip]

--- curl.sh
+++ curl.sh
@@ -600,13 +600,13 @@ _VER="$1"
         {
           printf "%s\r\n" ':: EXPERIMENTAL'
           printf "%s\r\n" '@set bb_dir=%~dp0'
-          printf "%s\r\n" '@set bb_exe=busybox.exe'
-          printf "%s\r\n" '@if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set bb_exe=busybox64.exe'
-          printf "%s\r\n" '@if "%PROCESSOR_ARCHITECTURE%" == "ARM64" set bb_exe=busybox64a.exe'
-          printf "%s\r\n" '@for %%P in (%PATH:;=;%) do @dir /b "%%~P\busybox.exe" >nul 2>&1 && (set bb_dir=%%~P\& set bb_exe=busybox.exe)'
-          printf "%s\r\n" '@if not exist "%bb_dir%%bb_exe%" curl -fsSO https://frippery.org/files/busybox/%bb_exe%'
+          printf "%s\r\n" '@set bb=busybox'
+          printf "%s\r\n" '@if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set bb=busybox64'
+          printf "%s\r\n" '@if "%PROCESSOR_ARCHITECTURE%" == "ARM64" set bb=busybox64a'
+          printf "%s\r\n" '@for %%P in (%PATH:;=;%) do @dir /b "%%~P\busybox.exe" >nul 2>&1 && (set bb_dir=%%~P\& set bb=busybox)'
+          printf "%s\r\n" '@if not exist "%bb_dir%%bb%.exe" curl -fsSO https://frippery.org/files/busybox/%bb%.exe'
           printf "%s\r\n" '@set PATH=%~dp0;%PATH'
-          printf "%s\r\n" '@"%bb_dir%%bb_exe%" sh "%~dp0wcurl" %*'
+          printf "%s\r\n" '@"%bb_dir%%bb%.exe" sh "%~dp0wcurl" %*'
         } > "${_DST}"/bin/wcurl.bat
       fi
     fi
