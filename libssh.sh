8c63198d459c1178c6fb3ff218d231841b62472a
Viktor Szakats <commit@vsz.me>
2026-02-10 17:37:53 +0100

    curl.sh: cleanup quotes [ci skip]

--- libssh.sh
+++ libssh.sh
@@ -6,6 +6,54 @@
 # WARNING: libssh uses hard-coded world-writable paths (/etc/..., ~/.ssh/) to
 #          read its configuration from, making it vulnerable to attacks on
 #          Windows. Do not use this component till there is a fix for these.
+#          0.12.0 update:
+#            https://gitlab.com/libssh/libssh-mirror/-/commit/6a7f19ec3486698bde3169161edf01ca11cca55f
+#          This received a mitigation where the unixy hard-coded paths are
+#          replaced with Windowsy ones by default, pointing to 'PROGRAMDATA'
+#          env if set, or the hard-coded 'C:/ProgramData/ssh' otherwise. This
+#          solution still potentially leaves systems/users vulnerable:
+#          - libssh seems to support Windows older than XP. The concept of
+#            ProgramData came with Vista, leaving earlier versions vulnerable.
+#          - The build-time value of 'PROGRAMDATA' env works as expected only
+#            when the built binary is run on the same machine as it was built
+#            on. Typically on Windows, this is not the case. Individual
+#            machines can have this directory customized or the OS installed
+#            on a different drive.
+#          - Same issue with the fallback value, which may be correct for
+#            many or most systems, but not for all.
+#            To fix both, this directory would need to be determined at
+#            runtime, not build-time, via 'SHGetKnownFolderPath()'. The
+#            fallback value would need to be something that works better on
+#            supported systems, e.g. via 'WINDIR' or the Win32 API equivalent.
+#            (or perhaps refuse to load data from the disk in such case.)
+#            FWIW libssh already uses 'SHGetSpecialFolderPathA' to load the
+#            user's home directory.
+#          - There is no CMake option to directly customize 'GLOBAL_CONF_DIR',
+#            as are for its two derivatives 'GLOBAL_BIND_CONFIG' and
+#            'GLOBAL_CLIENT_CONFIG'.
+#          - After this patch, (cross-)builders have to clear 'PROGRAMDATA'
+#            env to avoid being accidentally or maliciously set to something
+#            undesired. I would have preferred an explicit CMake option to
+#            control/override this directory at build-time.
+#          - Setting 'WITH_HERMETIC_USR' for the CMake build creates
+#            a vulnerable configuration on Windows, via
+#            'USR_GLOBAL_CLIENT_CONFIG'. Also via 'USR_GLOBAL_CONF_DIR', but
+#            this latter config value is set, and then left unused.
+#          - As per documentation ProgramData allows modification by the user
+#            who created the file first. This is not necessarily the semantics
+#            expected for SSH global configuration on a multi-user machine.
+#            Unless perhaps this user was a dedicated administrator.
+#            Otherwise a malicious application running under the local user
+#            can still alter the libssh global configuration.
+#          - included config files are still loaded from a hard-coded
+#            '/etc/ssh', via 'ssh_config_make_absolute()'.
+#          - other parts of the code still default to '~/.ssh' for the user
+#            configuration, which is later not expanded to the home directory
+#            in 'ssh_path_expand_tilde()' since
+#            da9b2e25f6233a419495933822446adf5736cdef.
+#          - it would be nice to have a way to fully prevent loading any
+#            global configuration from disk, making settings 'hermetic' and
+#            determined solely at runtime by the user application.
 
 # shellcheck disable=SC3040,SC2039
 set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
@@ -94,6 +142,7 @@ _VER="$1"
 
   if [ "${_OS}" = 'win' ]; then
     _my_prefix='C:/Windows/System32/ssh'
+    export PROGRAMDATA="${_my_prefix}"  # to set 'GLOBAL_CONF_DIR' for 'ssh_known_hosts' (as of v0.12.0)
   else
     _my_prefix='/etc/ssh'
   fi
