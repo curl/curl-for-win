e54f7f878fa0e1a9e877270092f189eed380acc2
Viktor Szakats <commit@vsz.me>
2026-02-10 20:21:26 +0100

    libssh: bump to v0.12.0 expand Windows vulnerability notes [ci skip]
    Refs:
    https://mastodon.social/@cryptomilk/116046580608606511
    https://www.libssh.org/2026/02/10/libssh-0-12-0-and-0-11-4-security-releases/
    https://www.libssh.org/security/advisories/CVE-2025-14821.txt

--- libssh.sh
+++ libssh.sh
@@ -3,9 +3,85 @@
 # Copyright (C) Viktor Szakats. See LICENSE.md
 # SPDX-License-Identifier: MIT
 
-# WARNING: libssh uses hard-coded world-writable paths (/etc/..., ~/.ssh/) to
-#          read its configuration from, making it vulnerable to attacks on
-#          Windows. Do not use this component till there is a fix for these.
+# WARNING: libssh uses hard-coded world-writable paths (e.g. /etc/...) to read
+#          its security critical configuration from, making it vulnerable to
+#          attacks on Windows.
+#          Do not use this component till there is a fix for these.
+#
+#          v0.12.0 update:
+#            https://gitlab.com/libssh/libssh-mirror/-/commit/6a7f19ec3486698bde3169161edf01ca11cca55f
+#          This issue received a mitigation where the unixy hard-coded paths
+#          are replaced with Windowsy ones by default, pointing to 'PROGRAMDATA'
+#          env if set, or the hard-coded 'C:/ProgramData/ssh' otherwise. This
+#          still potentially leaves systems/users vulnerable:
+#
+#          - libssh seems to support Windows older than XP. The concept of
+#            ProgramData came with Vista, leaving earlier versions vulnerable.
+#            (it does not affect curl-for-win, as it needs Vista or newer)
+#
+#          - The build-time value of 'PROGRAMDATA' env works as expected only
+#            when the built binary is run on the same machine as it was built
+#            on. Typically on Windows this is rarely the case. Individual
+#            machines can have this directory customized or the OS installed
+#            on a different drive. Windows has no guaranteed protected
+#            directory across versions, configurations and installations.
+#
+#          - Same issue with the fallback value, which may be correct for
+#            many/most systems, but not for all.
+#            To fix both, this directory would need to be determined at
+#            runtime, not build-time, via 'SHGetKnownFolderPath()'. The
+#            fallback value would need to be something that works on all
+#            supported systems, e.g. via 'WINDIR' or the Win32 API equivalent.
+#            (or perhaps refuse to load config data from disk in such case.)
+#            Similar to how libssh uses 'SHGetSpecialFolderPathA()' to figure
+#            out the user home directory:
+#            https://gitlab.com/libssh/libssh-mirror/-/blob/f13a8d7ced240456d9002460bfc243c3097e77e5/src/misc.c#L117
+#
+#          - There is no CMake option to directly customize 'GLOBAL_CONF_DIR',
+#            as exists for its two derivatives 'GLOBAL_BIND_CONFIG' and
+#            'GLOBAL_CLIENT_CONFIG'. It means, to keep them synced, one must
+#            set 'PROGRAMDATA' env explicitly, aligned with the other two CMake
+#            configuration settings.
+#
+#          - After this patch, (cross-)builders have to clear/set 'PROGRAMDATA'
+#            env to avoid being accidentally or maliciously set to something
+#            undesired. IMO an explicit CMake option could be useful to
+#            control/override this directory at build-time.
+#
+#          - Setting 'WITH_HERMETIC_USR' for the CMake build creates
+#            a world-writable, vulnerable configuration on Windows by appending
+#            the Windows absolute ProgramData path to '/usr',
+#            via 'USR_GLOBAL_CLIENT_CONFIG' and 'USR_GLOBAL_BIND_CONFIG'. Also
+#            via 'USR_GLOBAL_CONF_DIR', but even though this config value is set,
+#            it does not seem to be used afterwards:
+#            https://gitlab.com/libssh/libssh-mirror/-/blob/f13a8d7ced240456d9002460bfc243c3097e77e5/DefineOptions.cmake#L90-92
+#            https://gitlab.com/libssh/libssh-mirror/-/blob/f13a8d7ced240456d9002460bfc243c3097e77e5/DefineOptions.cmake#L98-100
+#
+#          - Included (recursive) config files seem to be loaded from
+#            a hard-coded '/etc/ssh' path, via 'ssh_config_make_absolute()':
+#            https://gitlab.com/libssh/libssh-mirror/-/blob/f13a8d7ced240456d9002460bfc243c3097e77e5/src/config.c#L651-665
+#
+#          - I would find it useful to have a build-time option to fully prevent
+#            loading global configuration from disk, making settings 'hermetic'
+#            and determined solely at runtime by the user application. This may
+#            be useful for non-Windows too.
+#
+#          - Documentation says ProgramData allows modification by the user
+#            who created the file first (or any user on the machine?). This is
+#            not necessarily the semantics expected for SSH global configuration
+#            on multi-user machines.
+#            Unless perhaps this user was a dedicated administrator, which is
+#            not guaranteed and not enforceable from a library. Otherwise
+#            a malicious application running under the local user can freely
+#            alter the libssh global configuration.
+#            In case the config files were created by a different user on the
+#            same machine, that user may keep controlling global settings for
+#            other users. A disk location with security properties similar to
+#            the HKLM registry hive may be a better fit, perhaps
+#            'FOLDERID_System' or 'FOLDERID_ProgramFiles'.
+#            Refs:
+#            https://learn.microsoft.com/windows/win32/shell/knownfolderid
+#            https://learn.microsoft.com/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-folderlocations-programdata
 
 # shellcheck disable=SC3040,SC2039
 set -o xtrace -o errexit -o nounset; [ -n "${BASH:-}${ZSH_NAME:-}" ] && set -o pipefail
@@ -96,6 +172,7 @@ _VER="$1"
 
   if [ "${_OS}" = 'win' ]; then
     _my_prefix='C:/Windows/System32/ssh'
+    export PROGRAMDATA="${_my_prefix}"  # to set 'GLOBAL_CONF_DIR' for 'ssh_known_hosts' (as of v0.12.0)
   else
     _my_prefix='/etc/ssh'
   fi
