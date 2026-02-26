8d2f6ea3b4fdcf1b121a5fbda11ecc4b3e21bb08
Viktor Szakats <commit@vsz.me>
2026-02-26 18:58:48 +0100

    _sign-pkg-cosign.sh: add comment about signature selection option [ci skip]

--- _sign-pkg-cosign.sh
+++ _sign-pkg-cosign.sh
@@ -14,6 +14,12 @@ if [ -n "${COSIGN_KEY:-}" ] && \
    [ -n "${COSIGN_KEY_PASS:+1}" ]; then
   file="$1"
   echo "Package signing with cosign: '${file}'"
+  # TODO: use `--signing-algorithm=ecdsa-sha2-512-nistp521` (with possibly
+  #       a newer/better algo) option in the future, when it makes sense.
+  #       https://blog.trailofbits.com/2026/01/29/building-cryptographic-agility-into-sigstore/
+  #       https://github.com/sigstore/cosign/pull/3497/files
+  #       https://github.com/sigstore/cosign/commit/8e7e0571a0ccad90a883da3676562eb6e1fe5ab4
+  #       Supported by cosign v3.0.3+
   tr -d '\n' <<EOF | \
   cosign sign-blob --yes --key="${COSIGN_KEY}" --new-bundle-format=true --bundle="${file}".sigstore "${file}"
 ${COSIGN_KEY_PASS}
