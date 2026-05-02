6b0b0e5811f20f6c3940d850faa28c42940726e3
Viktor Szakats <commit@vsz.me>
2026-05-02 08:13:46 +0200

    8.20.0_2 with `libcrypto.a` `strndup` fix

--- _versions.sh
+++ _versions.sh
@@ -13,7 +13,7 @@ export CURL_HASH=63fe2dc148ba0ceae89922ef838f7e5c946272c2e78b7c59fab4b79d3ce2b89
 # Create revision string
 # NOTE: Set _REV to 1 after bumping CURL_VER_, then increment for each
 #       CI rebuild via `main` branch push (e.g. after bumping a dependency).
-export _REV="${CW_REVISION:-1}"
+export _REV="${CW_REVISION:-2}"
 
 export TRURL_VER_='0.16.1'
 export TRURL_HASH=aac947d4fb421a58abc19a3771e87942cd4721b8f855c433478c94c11a8203ba
