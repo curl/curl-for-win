6f1faa2db8ce013553fb9de7be3cb001e5d3d071
Viktor Szakats <commit@vsz.me>
2026-09-09 11:19:15 +0200

    _build.sh: bump to age 1.3.2 [ci skip]
    Ref: https://github.com/FiloSottile/age/releases/tag/v1.3.2

--- _build.sh
+++ _build.sh
@@ -455,8 +455,8 @@ if [ "${_HOST}" = 'linux' ] && \
     "$(printf '%s' "${age_ver_str}" | cut -d '.' -f 1)" \
     "$(printf '%s' "${age_ver_str}" | cut -d '.' -f 2)")"
   if [ "${age_ver}" -lt 0130 ]; then  # debian:trixie
-    AGE_VERSION=1.3.1
-    AGE_SHA256=bdc69c09cbdd6cf8b1f333d372a1f58247b3a33146406333e30c0f26e8f51377
+    AGE_VERSION=1.3.2
+    AGE_SHA256=cbe24006683f8eb669266162894b9a522a1af52f2665fbc63a4bb032ed26ac10
     curl --disable --fail --silent --show-error --connect-timeout 15 --max-time 60 --retry 3 --retry-connrefused \
       --location --proto-redir =https "https://github.com/FiloSottile/age/releases/download/v${AGE_VERSION}/age-v${AGE_VERSION}-linux-amd64.tar.gz" --output pkg.bin
     sha256sum pkg.bin | tee /dev/stderr | grep -qwF -- "${AGE_SHA256}" && tar -xf pkg.bin && rm -f pkg.bin
