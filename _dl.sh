831d48af36eca22b16cc20be688818751de27bb5
Viktor Szakats <commit@vsz.me>
2026-09-10 01:41:09 +0200

    _dl.sh: reuse the date in the `*_VER_` variable, replacing a GitHub API call [ci skip]
    This currently affects to `certdata` package.
    
    Minor downside: the hours, minutes, seconds are not maintained present
    in the `*_VER_` value, and therefore are replaced with '00:00:00'. Not a
    practical issue because certdata gets updated once every couple of
    months. Also the exact revision is shown via the Git commit hash, if it
    ever gets multiple updates on the same day.
    
    Reproducibility is still ensured after this patch. But, it's a one-time
    breakage compared to previous builds which used the full timestamp.
    
    After this patch, `api.github.com` is only used once by curl-for-win;
    when detecting certdata and psl version bumps. This may also be replaced
    by using the Atom feed and parsing XML, if necessary. The benefits are
    small, because the Atom feed is just as much GitHub-specific as its API,
    and also less convenient to parse.
    
    Follow-up to 2cca01215f8ea6eb01047f03472293ca0227247b

--- _dl.sh
+++ _dl.sh
@@ -640,16 +640,10 @@ live_dl() {
         url="https://github.com/${slug}/archive/${commit}.tar.gz"
         redir='redir'
       else
-        # Use the GitHub API to find out the timestamp of the downloaded file
-        sha=''
-        [ -n "${commit:-}" ] && sha="&sha=${commit}"
         # Tweak the received date format to be accepted also by busybox (on Alpine).
         # https://busybox.net/downloads/BusyBox.html#date
-        dcommit="$(my_curl --user-agent 'curl' "https://api.github.com/repos/${slug}/commits?path=${path}${sha}" \
-          --retry-all-errors --retry 10 \
-          --header 'X-GitHub-Api-Version: 2022-11-28' \
-          | jq --raw-output '.[0].commit.committer.date' | sed 's/^null$//' | tr 'T' ' ' | tr -d 'Z')"  # 'YYYY-MM-DDThh:mm:ssZ' -> 'YYYY-MM-DD hh:mm:ss'
-        echo "! Detected file timestamp: |${dcommit}|"
+        dcommit="${ver} 00:00:00"  # YYYY-MM-DD hh:mm:ss
+        echo "! Using file timestamp: |${dcommit}|"
       fi
     fi
 
