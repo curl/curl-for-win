f0dc15030e8ffd8c39cc06586c7810bf33ed5e24
Viktor Szakats <commit@vsz.me>
2026-09-26 16:56:27 +0200

    try sth 4c

--- zlib.sh
+++ zlib.sh
@@ -28,6 +28,10 @@ _VER="$1"
     options+=' -DZLIB_BUILD_SHARED=OFF'
   fi
 
+  echo '----------------------ZZZ'
+  find /usr/lib/gcc -type -d
+  echo '----------------------'
+
   # shellcheck disable=SC2086
   cmake -B "${_BLDDIR}" ${_CMAKE_GLOBAL} ${options} \
     -DCMAKE_C_FLAGS="${_CFLAGS_GLOBAL_CMAKE} ${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${_LDFLAGS_GLOBAL} -v" || true
