4a25a403ed5fb7c639efe8b2ae132fdb22c08956
Viktor Szakats <commit@vsz.me>
2026-09-26 13:47:19 +0200

    try sth 3 gcc_s

--- zlib.sh
+++ zlib.sh
@@ -30,7 +30,7 @@ _VER="$1"
 
   # shellcheck disable=SC2086
   cmake -B "${_BLDDIR}" ${_CMAKE_GLOBAL} ${options} \
-    -DCMAKE_C_FLAGS="${_CFLAGS_GLOBAL_CMAKE} ${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${_LDFLAGS_GLOBAL}"
+    -DCMAKE_C_FLAGS="${_CFLAGS_GLOBAL_CMAKE} ${_CFLAGS_GLOBAL} ${_CPPFLAGS_GLOBAL} ${_LDFLAGS_GLOBAL} -v"
 
   cmake --build "${_BLDDIR}"
   cmake --install "${_BLDDIR}" --prefix "${_PP}"
